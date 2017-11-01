a59b28ace5e606de0592fdabb3fcf58b22f738da
Pavel Shirshov <pavel.shirshov@teckro.com>
2017-11-01 17:06:00 +0000

    test args fixed

--- sbt-izumi/src/main/scala/org/bitbucket/pshirshov/izumi/sbt/TestingPlugin.scala
+++ sbt-izumi/src/main/scala/org/bitbucket/pshirshov/izumi/sbt/TestingPlugin.scala
@@ -5,7 +5,7 @@ import sbt._
 object TestingPlugin extends AutoPlugin {
 
   override lazy val globalSettings = Seq(
-    Keys.testOptions in Test += Tests.Argument("-oD -eD")
+    Keys.testOptions in Test ++= Seq(Tests.Argument("-oD"), Tests.Argument("-eD"))
   )
 
   override def projectSettings = Seq(
