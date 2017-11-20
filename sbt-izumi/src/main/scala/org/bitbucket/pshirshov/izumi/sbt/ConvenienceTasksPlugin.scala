e94a56967cc12cfa62c2faa93f2a4c7dac93dc13
pshirshov <pshirshov@gmail.com>
2017-11-20 19:21:52 +0000

    module templates

--- sbt-izumi/src/main/scala/org/bitbucket/pshirshov/izumi/sbt/ConvenienceTasksPlugin.scala
+++ sbt-izumi/src/main/scala/org/bitbucket/pshirshov/izumi/sbt/ConvenienceTasksPlugin.scala
@@ -14,13 +14,15 @@ object ConvenienceTasksPlugin extends AutoPlugin {
     val addVersionSuffix = inputKey[Unit]("Add a suffix into version defined in version file")
     val preserveTargets = inputKey[Unit]("Preserve 'target' directories")
     val rmDirs = inputKey[Unit]("Recursively remove directories with a name provided")
+    val newModule = inputKey[Unit]("Create new empty module layout in current directory")
+    val newStub = inputKey[Unit]("Copy stub from stubs/stub into current directory")
   }
 
   import Keys._
 
   override def projectSettings = Seq(
     addVersionSuffix := {
-      val suffix: String = (token(Space) ~> token(StringBasic, "name"))
+      val suffix: String = (token(Space) ~> token(StringBasic, "suffix"))
         .parsed
         .replace('/', '_')
         .replace('.', '_')
@@ -35,7 +37,7 @@ object ConvenienceTasksPlugin extends AutoPlugin {
       IO.write(file("version.sbt"), s"""version in ThisBuild := "$newVersion"""")
     }
     , preserveTargets := {
-      val suffix: String = (token(Space) ~> token(StringBasic, "name"))
+      val suffix: String = (token(Space) ~> token(StringBasic, "suffix"))
         .parsed
 
       val name = "target"
@@ -58,5 +60,55 @@ object ConvenienceTasksPlugin extends AutoPlugin {
       val dirs = (file(".") ** (DirectoryFilter && new ExactFilter(name))).get
       IO.delete(dirs)
     }
+    , newModule := {
+      val name: String = (token(Space) ~> token(StringBasic, "moduleName"))
+        .parsed
+
+      mkDefaultModule(name)
+    }
+    , newStub := {
+      val args = spaceDelimited("<args>").parsed
+      val moduleName = args.head
+      val stubId = args.tail.headOption.getOrElse("default")
+      mkModule(moduleName, stubId)
+    }
   )
+
+  private def mkModule(name: String, stubId: String): Unit = {
+    val base = file(".").toPath
+    val stub = base.resolve("stubs").resolve(stubId).toFile
+    if (!stub.exists()) {
+      throw new IllegalArgumentException(s"Directory $stub does not exist!")
+    }
+    val target = base.resolve(name).toFile
+    if (target.exists()) {
+      throw new IllegalArgumentException(s"Directory $target already exists!")
+    }
+    target.mkdirs()
+    logger.info(s"Copying stub $stub => $target")
+    IO.copyDirectory(stub, target)
+  }
+
+  private def mkDefaultModule(name: String): Unit = {
+    val stub = Seq(
+      "src/main/scala"
+      , "src/main/java"
+      , "src/test/java"
+      , "src/test/scala"
+    )
+    val base = file(".").toPath.resolve(name)
+    if (base.toFile.exists()) {
+      throw new IllegalArgumentException(s"Directory $base already exists!")
+    }
+
+    stub.foreach {
+      n =>
+        val dir = base.resolve(n)
+        val dirOk = dir.toFile.mkdirs()
+        val fileOk = dir.resolve(".keep").toFile.createNewFile()
+        if (!dirOk || !fileOk) {
+          throw new IllegalArgumentException(s"IO failed on $dir")
+        }
+    }
+  }
 }
