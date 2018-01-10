b9db2bb4ba5f63059d2eb9468aa735b225379b93
pshirshov <pshirshov@gmail.com>
2018-01-10 18:41:13 +0000

    immutable logic

--- lib/di/src/main/scala/org/bitbucket/pshirshov/izumi/di/BasicInjector.scala
+++ lib/di/src/main/scala/org/bitbucket/pshirshov/izumi/di/BasicInjector.scala
@@ -9,8 +9,11 @@ import org.bitbucket.pshirshov.izumi.di.plan.{Association, DIPlan, ImmutablePlan
 trait PlanningConflict
 
 object PlanningConflict {
+
   case class NoConflict(newOp: Op) extends PlanningConflict
+
   case class Conflict(newOp: Op, existingOp: Op) extends PlanningConflict
+
 }
 
 class BasicInjector extends Injector {
@@ -35,7 +38,7 @@ class BasicInjector extends Injector {
         val nextPlan = extendPlan(currentPlan, toPull, toProvision)
 
 
-        val next = new ImmutablePlan(nextPlan.collect { case Leave(op) => op})
+        val next = new ImmutablePlan(nextPlan.collect { case Leave(op) => op })
 
         System.err.println("-" * 120)
         System.err.println(s"> ${nextPlan}")
@@ -43,16 +46,58 @@ class BasicInjector extends Injector {
         assertSanity(next.getPlan)
         next
     }
-
   }
-
 
 
   private def extendPlan(currentPlan: DIPlan, toPull: Seq[Op], toProvision: Seq[Op]) = {
     import PlanningConflict._
-    val current = currentPlan.getPlan.map(Leave)
-    val nextPlan = scala.collection.mutable.ArrayBuffer[PlanTransformation](current: _*)
+    val withConflicts = findConflicts(currentPlan, toProvision)
 
+    val currentPlanBlock = currentPlan.getPlan.map(Leave)
+    val insertAt  = withConflicts
+      .map {
+        case Conflict(_, existingOp) =>
+          currentPlanBlock.indexOf(Leave(existingOp))
+
+        case _: NoConflict =>
+          currentPlanBlock.length
+      }
+      .min
+
+    val (before, after) = currentPlanBlock.splitAt(insertAt)
+
+    val transformations = withConflicts.map {
+      case NoConflict(newOp) =>
+        Leave(newOp)
+
+        // TODO: here we may check for circular deps
+      case Conflict(newOp, existing: PullDependency) =>
+        Replace(existing, newOp)
+
+      case Conflict(newOp, existing) if newOp == existing =>
+        Duplicated(newOp)
+
+      case Conflict(newOp, existing) =>
+        UnsolvableConflict(newOp, existing)
+    }
+
+    val replacements = transformations.collect { case t: Replace => Leave(t.op) }.toSet
+
+    val beforeReplaced = before.filterNot(replacements.contains)
+    val afterReplaced = after.filterNot(replacements.contains)
+    val transformationsWithoutReplacements = transformations.map {
+      case t: Replace => Leave(t.replacement)
+      case v => v
+    }
+
+    toPull.map(Leave) ++
+      beforeReplaced ++
+      transformationsWithoutReplacements ++
+      afterReplaced
+  }
+
+  private def findConflicts(currentPlan: DIPlan, toProvision: Seq[Op]): Seq[PlanningConflict] = {
+    import PlanningConflict._
     val currentUnwrapped = currentPlan.getPlan.toSet
 
     val withConflicts = toProvision.map {
@@ -65,41 +110,8 @@ class BasicInjector extends Injector {
           case _ =>
             NoConflict(newOp)
         }
-
     }
-
-
-    val justConflicts = withConflicts.collect { case c: Conflict => c }
-
-    val allIndexes = justConflicts.map(c => nextPlan.indexOf(Leave(c.existingOp)))
-    val insertAt = (allIndexes :+ nextPlan.length).min
-
-    nextPlan.insertAll(insertAt, toProvision.map(Leave))
-
-    val transformations = justConflicts.map {
-      case Conflict(newOp, existing) if newOp == existing =>
-        Duplicated(newOp)
-
-      case Conflict(newOp, existing: PullDependency) =>
-        Replace(newOp, existing)
-
-      case Conflict(newOp, existing) =>
-        UnsolvableConflict(newOp, existing)
-    }
-
-    nextPlan --= transformations.collect {
-      case Replace(leave, replace) =>
-        Leave(replace)
-
-    }
-
-    nextPlan ++= transformations.collect {
-      case u: UnsolvableConflict => u
-      case d: Duplicated => d
-    }
-
-    // all the toPull keys are not in current plan
-    toPull.map(Leave) ++ nextPlan
+    withConflicts
   }
 
   private def provisioning(target: DIKey, impl: Symb, deps: Seq[Association]): Seq[Op] = {
