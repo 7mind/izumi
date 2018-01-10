aa67cfbb8ebf96790771039afd54c67035ec5192
pshirshov <pshirshov@gmail.com>
2018-01-10 22:49:06 +0000

    NOPs and comments. Model revisited

--- lib/di/src/main/scala/org/bitbucket/pshirshov/izumi/di/model/plan/DodgyPlan.scala
+++ lib/di/src/main/scala/org/bitbucket/pshirshov/izumi/di/model/plan/DodgyPlan.scala
@@ -1,6 +1,6 @@
 package org.bitbucket.pshirshov.izumi.di.model.plan
 
-import org.bitbucket.pshirshov.izumi.di.model.plan.DodgyOp.Statement
+import org.bitbucket.pshirshov.izumi.di.model.plan.DodgyOp.{Nop, Statement}
 
 /**
   * ***********,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,***./((%%&&&%%##((//////((((((((((((((((((((((///(((((((((//////(((
@@ -119,6 +119,8 @@ case class DodgyPlan(steps: Seq[DodgyOp]) {
   override def toString: String = steps.collect {
     case Statement(op) =>
       op.format
+    case Nop(message) =>
+      message
     case v =>
       v.toString
   }.mkString("\n")
