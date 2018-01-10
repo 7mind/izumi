3fd988b69f4b92afc432d3d27d072f02ade26075
pshirshov <pshirshov@gmail.com>
2018-01-10 16:58:49 +0000

    sanity

--- lib/di/src/main/scala/org/bitbucket/pshirshov/izumi/di/model/DIKey.scala
+++ lib/di/src/main/scala/org/bitbucket/pshirshov/izumi/di/model/DIKey.scala
@@ -1,14 +1,19 @@
 package org.bitbucket.pshirshov.izumi.di.model
 
-import org.bitbucket.pshirshov.izumi.di.Symb
+import org.bitbucket.pshirshov.izumi.di.{Symb, Tag}
+import scala.reflect.runtime.universe._
 
 sealed trait DIKey {
   def symbol: Symb
 }
 
 object DIKey {
+
   case class TypeKey(symbol: Symb) extends DIKey
+
   case class IdKey[InstanceId](symbol: Symb, id: InstanceId) extends DIKey
+
+  def get[K: Tag]: TypeKey = TypeKey(typeTag[K].tpe.typeSymbol)
 }
 
 
