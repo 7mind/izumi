adf1e5086645e502ded1a5f56215fd594fc9bd51
pshirshov <pshirshov@gmail.com>
2018-01-11 16:40:40 +0000

    decopypasted

--- lib/di/src/main/scala/org/bitbucket/pshirshov/izumi/di/planning/WithPlanAnalysis.scala
+++ lib/di/src/main/scala/org/bitbucket/pshirshov/izumi/di/planning/WithPlanAnalysis.scala
@@ -2,37 +2,25 @@ package org.bitbucket.pshirshov.izumi.di.planning
 
 import org.bitbucket.pshirshov.izumi.di.model.DIKey
 import org.bitbucket.pshirshov.izumi.di.model.plan.DodgyOp.Statement
-import org.bitbucket.pshirshov.izumi.di.model.plan.{DodgyPlan, ReadyPlan}
+import org.bitbucket.pshirshov.izumi.di.model.plan.{DodgyPlan, ExecutableOp, ReadyPlan}
 import org.bitbucket.pshirshov.izumi.di.model.plan.ExecutableOp.DependentOp
 
 import scala.collection.mutable
 
+case class FdwRefTable(dependencies: Map[DIKey, Set[DIKey]], dependants: Map[DIKey, Set[DIKey]])
+
 trait WithPlanAnalysis {
   protected def computeFwdRefTable(plan: DodgyPlan): FdwRefTable = {
-    // TODO: make it immu
-    val dependencies = plan.steps.foldLeft(new mutable.HashMap[DIKey, mutable.Set[DIKey]]) {
-      case (acc, Statement(op: DependentOp)) =>
-        val forwardRefs = op.deps.map(_.wireWith).filterNot(acc.contains).toSet
-        acc.getOrElseUpdate(op.target, mutable.Set.empty) ++= forwardRefs
-        acc
-
-      case (acc, Statement(op)) =>
-        acc.getOrElseUpdate(op.target, mutable.Set.empty)
-        acc
-
-      case (acc, _) =>
-        acc
-    }
-      .filter(_._2.nonEmpty)
-      .mapValues(_.toSet).toMap
-
-    val dependants = reverseReftable(dependencies)
-    FdwRefTable(dependencies, dependants)
+    computeFwdRefTable(plan.steps.collect { case Statement(op) => op }.toStream)
   }
 
   protected def computeFwdRefTable(plan: ReadyPlan): FdwRefTable = {
+    computeFwdRefTable(plan.getPlan.toStream)
+  }
+
+  protected def computeFwdRefTable(plan: Stream[ExecutableOp]): FdwRefTable = {
     // TODO: make it immu
-    val dependencies = plan.getPlan.foldLeft(new mutable.HashMap[DIKey, mutable.Set[DIKey]]) {
+    val dependencies = plan.foldLeft(new mutable.HashMap[DIKey, mutable.Set[DIKey]]) {
       case (acc, op: DependentOp) =>
         val forwardRefs = op.deps.map(_.wireWith).filterNot(acc.contains).toSet
         acc.getOrElseUpdate(op.target, mutable.Set.empty) ++= forwardRefs
