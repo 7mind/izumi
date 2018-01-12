68cc18ba93d070481d21f41a6ce66e485895cbab
pshirshov <pshirshov@gmail.com>
2018-01-12 00:03:13 +0000

    refactorings

--- lib/di/src/main/scala/org/bitbucket/pshirshov/izumi/di/DefaultBootstrapContext.scala
+++ lib/di/src/main/scala/org/bitbucket/pshirshov/izumi/di/DefaultBootstrapContext.scala
@@ -1,24 +1,36 @@
 package org.bitbucket.pshirshov.izumi.di
 
 import org.bitbucket.pshirshov.izumi.di.model.DIKey
-import org.bitbucket.pshirshov.izumi.di.planning.{ForwardingRefResolver, ForwardingRefResolverDefaultImpl, PlanResolver, PlanResolverDefaultImpl}
+import org.bitbucket.pshirshov.izumi.di.planning._
 import org.bitbucket.pshirshov.izumi.di.reflection.{DependencyKeyProvider, DependencyKeyProviderDefaultImpl, ReflectionProvider, ReflectionProviderDefaultImpl}
 
 
-
-object DefaultBootstrapContext extends Locator {
+trait DefaultBootstrapContext extends Locator {
   override def parent: Option[Locator] = None
+
   private val planResolverDefaultImpl = new PlanResolverDefaultImpl()
   private val forwardingRefResolverDefaultImpl = new ForwardingRefResolverDefaultImpl()
   private val dependencyKeyProviderDefaultImpl = new DependencyKeyProviderDefaultImpl()
   private val reflectionProviderDefaultImpl = new ReflectionProviderDefaultImpl(dependencyKeyProviderDefaultImpl)
+  private val factoryOfFactories = new TheFactoryOfAllTheFactoriesDefaultImpl()
+  private val planner = new DefaultPlannerImpl(
+    planResolverDefaultImpl
+    , forwardingRefResolverDefaultImpl
+    , reflectionProviderDefaultImpl
+  )
 
-  private val defaultImpls = Map[DIKey, AnyRef](
+  protected def defaultImpls: Map[DIKey, AnyRef] = Map[DIKey, AnyRef](
     DIKey.get[PlanResolver] -> planResolverDefaultImpl
     , DIKey.get[ForwardingRefResolver] -> forwardingRefResolverDefaultImpl
     , DIKey.get[DependencyKeyProvider] -> dependencyKeyProviderDefaultImpl
+    , DIKey.get[TheFactoryOfAllTheFactories] -> factoryOfFactories
     , DIKey.get[ReflectionProvider] -> reflectionProviderDefaultImpl
+    , DIKey.get[Planner] -> planner
   )
 
   def lookup(key: DIKey): Option[AnyRef] = defaultImpls.get(key)
+}
+
+object DefaultBootstrapContext {
+  final val instance = new DefaultBootstrapContext {}
 }
