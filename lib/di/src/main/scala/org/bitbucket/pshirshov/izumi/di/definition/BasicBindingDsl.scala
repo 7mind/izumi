8a45c3a2ec1fc8964ad1fe14c9f84700f31c3c20
pshirshov <pshirshov@gmail.com>
2018-01-12 00:39:51 +0000

    simple binding dsl

---
+++ lib/di/src/main/scala/org/bitbucket/pshirshov/izumi/di/definition/BasicBindingDsl.scala
@@ -0,0 +1,24 @@
+package org.bitbucket.pshirshov.izumi.di.definition
+
+import org.bitbucket.pshirshov.izumi.di.Tag
+import org.bitbucket.pshirshov.izumi.di.definition.Def.SingletonBinding
+import org.bitbucket.pshirshov.izumi.di.model.DIKey
+import scala.reflect.runtime.universe._
+
+object BasicBindingDsl {
+  val start: Seq[Def] = Seq.empty
+
+  def symbolDef[T: Tag]: ImplDef = ImplDef.TypeImpl(typeTag[T].tpe.typeSymbol)
+
+  implicit class BindingSupport(bindings: Seq[Def]) {
+    def add[T:Tag]: BindingSupport = {
+      BindingSupport(bindings :+ SingletonBinding(DIKey.get[T], symbolDef[T]))
+    }
+
+    def add[T:Tag, I <: T:Tag]: BindingSupport = {
+      BindingSupport(bindings :+ SingletonBinding(DIKey.get[T], symbolDef[I]))
+    }
+
+    def finish: Seq[Def] = bindings
+  }
+}
