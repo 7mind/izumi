9b294b176c4387eed22e31ade4dfc1b64b72bdfd
pshirshov <pshirshov@gmail.com>
2018-01-12 19:29:05 +0000

    cleanups

--- lib/di/src/main/scala/org/bitbucket/pshirshov/izumi/di/model/Formattable.scala
+++ lib/di/src/main/scala/org/bitbucket/pshirshov/izumi/di/model/Formattable.scala
@@ -1,8 +1,6 @@
 package org.bitbucket.pshirshov.izumi.di.model
 
-// traits: typeTag[T2].tpe.decls.head.getClass
-// classes: typeTag[C3].tpe.decls.last.info.paramLists
-// names are available: typeTag[C3].tpe.decls.last.info.paramLists.head.head.name
+
 trait Formattable {
   def format: String
 }
