9183764d20db9ef7acdeaad18aff634a2214989e
pshirshov <pshirshov@gmail.com>
2018-01-13 01:47:21 +0000

    decluttering

--- lib/di/src/main/scala/org/bitbucket/pshirshov/izumi/di/model/plan/PlanningOp.scala
+++ lib/di/src/main/scala/org/bitbucket/pshirshov/izumi/di/model/plan/PlanningOp.scala
@@ -5,8 +5,6 @@ sealed trait PlanningOp
 object PlanningOp {
   case class Put(op: ExecutableOp) extends PlanningOp
 
-//  case class Replace(op: ExecutableOp, replacement: ExecutableOp) extends PlanningOp
-
   case class SolveUnsolvable(op: ExecutableOp, existing: DodgyOp) extends PlanningOp
 
   case class SolveRedefinition(op: ExecutableOp) extends PlanningOp
