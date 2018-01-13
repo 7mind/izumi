ba95c0d1be709b424ffe086befd2b81fef94405a
pshirshov <pshirshov@gmail.com>
2018-01-13 01:59:26 +0000

    more cleanups

--- lib/di/src/main/scala/org/bitbucket/pshirshov/izumi/di/model/plan/Provisioning.scala
+++ lib/di/src/main/scala/org/bitbucket/pshirshov/izumi/di/model/plan/Provisioning.scala
@@ -1,10 +1,16 @@
 package org.bitbucket.pshirshov.izumi.di.model.plan
 
+import org.bitbucket.pshirshov.izumi.di.definition.ImplDef
+import org.bitbucket.pshirshov.izumi.di.model.plan.ExecutableOp.InstantiationOp
+
 sealed trait Provisioning[G, B] {
   @inline def map[G1](f: G => G1): Provisioning[G1, B]
 }
 
 object Provisioning {
+
+  type StepProvisioning = Provisioning[NextOps, Seq[ImplDef]]
+  type InstanceProvisioning = Provisioning[Seq[InstantiationOp], Seq[ImplDef]]
 
   case class Possible[G, B](possible: G) extends Provisioning[G, B] {
     @inline override def map[G1](f: G => G1): Provisioning[G1, B] = Possible(f(possible))
---
+++ lib/di/src/main/scala/org/bitbucket/pshirshov/izumi/di/planning/PlanningObserverDefaultImpl.scala
@@ -0,0 +1,22 @@
+package org.bitbucket.pshirshov.izumi.di.planning
+
+import org.bitbucket.pshirshov.izumi.di.model.plan.{DodgyPlan, FinalPlan}
+
+class PlanningObserverDefaultImpl extends PlanningObsever {
+
+  override def onFinalPlan(finalPlan: FinalPlan): Unit = {
+    System.err.println("=" * 60 + " Final Plan " + "=" * 60)
+    System.err.println(s"$finalPlan")
+    System.err.println("\n")
+  }
+
+  override def onSuccessfulStep(next: DodgyPlan): Unit = {
+//    System.err.println("-" * 60 + " Next Plan " + "-" * 60)
+//    System.err.println(next)
+  }
+
+  override def onFailedStep(next: DodgyPlan): Unit = {
+//    System.err.println("-" * 60 + " Next Plan (failed) " + "-" * 60)
+//    System.err.println(next)
+  }
+}
