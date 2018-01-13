e187fc0fe1461d4107ddc961d78ea0594179c040
pshirshov <pshirshov@gmail.com>
2018-01-13 15:47:58 +0000

    ordering logic significantly simplified

--- lib/di/src/main/scala/org/bitbucket/pshirshov/izumi/di/planning/PlanResolverDefaultImpl.scala
+++ lib/di/src/main/scala/org/bitbucket/pshirshov/izumi/di/planning/PlanResolverDefaultImpl.scala
@@ -2,7 +2,6 @@ package org.bitbucket.pshirshov.izumi.di.planning
 
 import org.bitbucket.pshirshov.izumi.di.definition.ContextDefinition
 import org.bitbucket.pshirshov.izumi.di.model.exceptions.UntranslatablePlanException
-import org.bitbucket.pshirshov.izumi.di.model.plan.DodgyOp.{Nop, Statement}
 import org.bitbucket.pshirshov.izumi.di.model.plan.{DodgyPlan, FinalPlan, FinalPlanImmutableImpl}
 
 
@@ -10,13 +9,13 @@ import org.bitbucket.pshirshov.izumi.di.model.plan.{DodgyPlan, FinalPlan, FinalP
 
 class PlanResolverDefaultImpl(planningObsever: PlanningObsever) extends PlanResolver {
   override def resolve(plan: DodgyPlan, definition: ContextDefinition): FinalPlan = {
-    val (goodSteps, badSteps) = plan.flatten.filterNot(_.isInstanceOf[Nop]).partition(_.isInstanceOf[Statement])
+    val issues = plan.issues
 
-    if (badSteps.nonEmpty) {
-      throw new UntranslatablePlanException(s"Cannot translate untranslatable (with default policy, feel free to jerk at this point): $badSteps", badSteps)
+    if (issues.nonEmpty) {
+      throw new UntranslatablePlanException(s"Cannot translate untranslatable (with default policy, feel free to jerk at this point): $issues", issues)
     }
 
-    val ops = goodSteps.collect { case op: Statement => op.op }
+    val ops = plan.statements
     val finalPlan = new FinalPlanImmutableImpl(ops, definition)
     planningObsever.onFinalPlan(finalPlan)
     finalPlan
