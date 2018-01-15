4536e165157f75d60f93cc00bb61cb23acf04018
pshirshov <pshirshov@gmail.com>
2018-01-15 00:43:51 +0000

    sbt plugin api simplified

---
+++ sbt-izumi/src/main/scala/org/bitbucket/pshirshov/izumi/sbt/model/Repositories.scala
@@ -0,0 +1,14 @@
+package org.bitbucket.pshirshov.izumi.sbt.model
+
+import sbt.librarymanagement.Resolver
+
+
+trait Repositories {
+  def get(filter: Repo => Boolean): Seq[Resolver]
+
+  def get(snapshots: Boolean): Seq[Resolver] = {
+    get(r => !r.isSnaphot || r.isSnaphot == snapshots)
+  }
+}
+
+
