719848b5b913010203244ccd18cb9200d3e30100
pshirshov <pshirshov@gmail.com>
2018-01-24 16:39:40 +0000

    cleanups

--- distage/distage-core/src/main/scala/org/bitbucket/pshirshov/izumi/distage/provisioning/cglib/CgLibFactoryMethodInterceptor.scala
+++ distage/distage-core/src/main/scala/org/bitbucket/pshirshov/izumi/distage/provisioning/cglib/CgLibFactoryMethodInterceptor.scala
@@ -2,14 +2,14 @@ package org.bitbucket.pshirshov.izumi.distage.provisioning.cglib
 
 import java.lang.reflect.Method
 
-import net.sf.cglib.proxy.{MethodInterceptor, MethodProxy}
+import net.sf.cglib.proxy.MethodProxy
 import org.bitbucket.pshirshov.izumi.distage.model.DIKey
-import org.bitbucket.pshirshov.izumi.distage.model.exceptions.{DIException, MissingRefException, UnsupportedWiringException}
+import org.bitbucket.pshirshov.izumi.distage.model.exceptions.{DIException, UnsupportedWiringException}
 import org.bitbucket.pshirshov.izumi.distage.model.plan.ExecutableOp.WiringOp
 import org.bitbucket.pshirshov.izumi.distage.model.plan.{Association, ExecutableOp, UnaryWiring, Wiring}
-import org.bitbucket.pshirshov.izumi.distage.provisioning.{OpResult, ProvisioningContext}
 import org.bitbucket.pshirshov.izumi.distage.provisioning.OpResult.{NewImport, NewInstance}
 import org.bitbucket.pshirshov.izumi.distage.provisioning.strategies.JustExecutor
+import org.bitbucket.pshirshov.izumi.distage.provisioning.{OpResult, ProvisioningContext}
 
 protected[distage] class CgLibFactoryMethodInterceptor
 (
@@ -18,10 +18,9 @@ protected[distage] class CgLibFactoryMethodInterceptor
   , context: JustExecutor
   , provisioningContext: ProvisioningContext
   , f: WiringOp.InstantiateFactory
-) extends MethodInterceptor {
+) extends CgLibTraitMethodInterceptor(dependencyMethodIndex, provisioningContext) {
 
   override def intercept(o: scala.Any, method: Method, objects: Array[AnyRef], methodProxy: MethodProxy): AnyRef = {
-    //premature optimization, all our methods are parameterless
     if (factoryMethodIndex.contains(method)) {
       factoryMethodIndex(method).wireWith match {
         case w: UnaryWiring.Constructor =>
@@ -37,18 +36,8 @@ protected[distage] class CgLibFactoryMethodInterceptor
         case w =>
           throw new UnsupportedWiringException(s"Wiring unsupported: $w", f.wiring.factoryType)
       }
-    } else if(dependencyMethodIndex.contains(method)) {
-      // TODO: copypasted from trait interceptor
-      val wireWith = dependencyMethodIndex(method).wireWith
-      provisioningContext.fetchKey(wireWith) match {
-        case Some(v) =>
-          v.asInstanceOf[AnyRef]
-
-        case None =>
-          throw new MissingRefException(s"Cannot return $wireWith from ${method.getName}, it's not available in the context o_O", Set(wireWith), None)
-      }
     } else {
-      CglibTools.invokeExistingMethod(o, method, objects)
+      super.intercept(o, method, objects, methodProxy)
     }
   }
 
