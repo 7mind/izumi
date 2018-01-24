cf33f069ad542141019d50cb8e2753fac6ef0f27
pshirshov <pshirshov@gmail.com>
2018-01-24 15:04:45 +0000

    TODO link

--- distage/distage-core/src/main/scala/org/bitbucket/pshirshov/izumi/distage/provisioning/strategies/TraitTools.scala
+++ distage/distage-core/src/main/scala/org/bitbucket/pshirshov/izumi/distage/provisioning/strategies/TraitTools.scala
@@ -32,7 +32,7 @@ object TraitTools {
           }
         } catch {
           case e: AbstractMethodError =>
-            throw new TraitInitializationFailedException(s"TODO: Failed to initialize trait $instanceType. Probably it contains fields (val or var) though fields are not supported yet", instanceType, e)
+            throw new TraitInitializationFailedException(s"TODO: Failed to initialize trait $instanceType. Probably it contains fields (val or var) though fields are not supported yet, see https://github.com/pshirshov/izumi-r2/issues/26", instanceType, e)
 
           case e: Throwable =>
             throw new TraitInitializationFailedException(s"Failed to initialize trait $instanceType. It may be an issue with the trait, framework bug or trait instantiator implemetation lim", instanceType, e)
