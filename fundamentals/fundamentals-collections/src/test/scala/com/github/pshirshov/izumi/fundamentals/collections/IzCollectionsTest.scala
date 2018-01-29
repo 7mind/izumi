f0834a778ca24d9182cc9e490c58c091751b40fa
pshirshov <pshirshov@gmail.com>
2018-01-29 00:50:24 +0000

    distinctBy test

--- fundamentals/fundamentals-collections/src/test/scala/com/github/pshirshov/izumi/fundamentals/collections/IzCollectionsTest.scala
+++ fundamentals/fundamentals-collections/src/test/scala/com/github/pshirshov/izumi/fundamentals/collections/IzCollectionsTest.scala
@@ -13,6 +13,10 @@ class IzCollectionsTest extends WordSpec {
       assert(List("a" -> 1, "a" -> 2).toMultimap == Map("a" -> Set(1, 2)))
       assert(List("a" -> 1, "a" -> 2).toMutableMultimap == mutable.Map("a" -> mutable.Set(1, 2)))
     }
+
+    "support .distinctBy operation" in {
+      assert(1.to(20).distinctBy(_ % 5) == 1.to(5))
+    }
   }
 
   
