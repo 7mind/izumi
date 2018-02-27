12c24f00d9db659a95c958d6aa77ca9858f05556
dnaumenko <dm.naumenko@gmail.com>
2018-02-27 12:33:02 +0200

    introduce a new type for response in wire protocol

--- idealingua/idealingua-model/src/main/scala/com/github/pshirshov/izumi/idealingua/model/runtime/transport/MultiplexingWireTransport.scala
+++ idealingua/idealingua-model/src/main/scala/com/github/pshirshov/izumi/idealingua/model/runtime/transport/MultiplexingWireTransport.scala
@@ -4,12 +4,12 @@ import com.github.pshirshov.izumi.idealingua.model.runtime.model.IDLService
 
 import scala.util.Try
 
-trait MultiplexingWireTransport[OnWire] {
-  def protocol: WireProtocol[OnWire]
+trait MultiplexingWireTransport[InWire, OutWire] {
+  def protocol: WireProtocol[InWire, OutWire]
 
   def services: Seq[AbstractTransport[IDLService]]
 
-  def process(request: OnWire): Try[OnWire] = {
+  def process(request: InWire): Try[OutWire] = {
     Try(protocol.decode(request))
       .map(r => (r, services.filter(_.inAcceptable(r)).head)) // by design we have one service per input class
       .map(rs => rs._2.processUnsafe(rs._1))
--- idealingua/idealingua-model/src/main/scala/com/github/pshirshov/izumi/idealingua/model/runtime/transport/MultiplexingWireTransportDefaultImpl.scala
+++ idealingua/idealingua-model/src/main/scala/com/github/pshirshov/izumi/idealingua/model/runtime/transport/MultiplexingWireTransportDefaultImpl.scala
@@ -2,9 +2,9 @@ package com.github.pshirshov.izumi.idealingua.model.runtime.transport
 
 import com.github.pshirshov.izumi.idealingua.model.runtime.model.IDLService
 
-class MultiplexingWireTransportDefaultImpl[OnWire]
+class MultiplexingWireTransportDefaultImpl[InWire, OutWire]
 (
-  val protocol: WireProtocol[OnWire]
+  val protocol: WireProtocol[InWire, OutWire]
   , override val services: Seq[AbstractTransport[IDLService]]
-) extends MultiplexingWireTransport[OnWire] {
+) extends MultiplexingWireTransport[InWire, OutWire] {
 }
