5b6eb04c576ecffc1ccb586d646a9492994d62ff
pshirshov <pshirshov@gmail.com>
2018-03-14 20:01:41 +0000

    IL parser
    IL comments
    
    IL comments
    
    IL comments
    
    IL comments
    
    IL comments
    
    working il comments

--- idealingua/idealingua-core/src/test/scala/com/github/pshirshov/izumi/idealingua/ILParserTest.scala
+++ idealingua/idealingua-core/src/test/scala/com/github/pshirshov/izumi/idealingua/ILParserTest.scala
@@ -36,50 +36,87 @@ class ILParserTest extends WordSpec {
       assertParses(new ILParser().dtoBlock, "data Data {}")
       assertParses(new ILParser().idBlock, "id Id {}")
       assertParses(new ILParser().serviceBlock, "service Service {}")
+      assertParses(new ILParser().SepLineOpt, "// test")
+      assertParses(new ILParser().SepLineOpt,
+        """// test
+          |/*test*/
+          | /* test/**/*/
+        """.stripMargin)
 
-
-      val domaindef =
-        """domain x.y.z
-          |
-          |import x.domain
-          |import x.y.domain
-          |
-          |include "b/c/d.model"
-          |include "a.model"
-          |
-          |alias x = str
-          |alias B = str
-          |
-          |enum MyEnum {X Y Zz}
-          |
-          |mixin Mixin {
-          | + Mixin
-          | a: B
-          | c: x#Y
-          |}
-          |
-          |data Data {
-          |+ Mixin
-          |+Mixin
-          |}
-          |
-          |id Id {
-          |  a: B
-          |  b: map[str, set[x#Y]]
-          |}
-          |service Service {
-          | def testMethod(Mixin1, c.d#Mixin2, x.y#Mixin3, x.y.z#Mixin4, z#Mixin5): (Mixin1, a.b#Mixin3)
-          |}
-          |""".stripMargin
-
-      assertDomainParses(domaindef)
 
       val domaindef1 =
         """domain x.y.z
           |
           |alias x = y
+          |/**/
           |""".stripMargin
       assertDomainParses(domaindef1)
+
+      val domaindef =
+        """domain x.y.z
+          |/*test*/
+          |import x.domain/*test*/
+          |import x.y.domain
+          |
+          |include "b/c/d.model"/*test*/
+          |include "a.model"
+          |
+          |alias x = str
+          |/*test*/
+          |alias B = str/*test*/
+          |
+          |enum MyEnum {X Y Zz}
+          |/*test*/
+          |mixin Mixin {/*test*/
+          | + Mixin
+          | a: B
+          | /*test*/
+          | c: x#Y
+          |}
+          |
+          |data Data {
+          |// test
+          |+ Mixin//test
+          |// test
+          |+Mixin
+          |/**/
+          |}
+          |
+          |id Id0 {
+          |}
+          |
+          |id Id1 {
+          |
+          |}
+          |
+          |id Id2 {
+          |//test
+          |/**/
+          |}
+          |
+          |
+          |id/*test*/Id/*test*/{
+          |  a:/*test*/B/*test*/
+          |  b: map[str, set[x#Y]]
+          | /*test*/
+          | // test
+          |}
+          |service Service {
+          | /*test*/
+          | /*test*/
+          | // test
+          | def/*test*/testMethod(Mixin1, c.d#Mixin2, x.y#Mixin3, x.y.z#Mixin4, z#Mixin5): (Mixin1, a.b#Mixin3)
+          | // test
+          | /*test*/
+          | def testMethod1(Mixin1,/*test*/c.d#Mixin2, x.y#Mixin3, x.y.z#Mixin4, z#Mixin5): (Mixin1, a.b#Mixin3)
+          |}
+          |
+          |// test
+          |""".stripMargin
+
+      assertDomainParses(domaindef)
+
+
     }
   }
 }
