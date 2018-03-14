edcb7275e9639aadb780278cd57c0c2aadc47b9e
Dmitry Naumenko <dm.naumenko@gmail.com>
2018-03-14 16:06:54 +0200

    fix casting (#99)

--- idealingua/idealingua-model/src/main/scala/com/github/pshirshov/izumi/idealingua/model/runtime/transport/MultiplexingWireTransport.scala
+++ idealingua/idealingua-model/src/main/scala/com/github/pshirshov/izumi/idealingua/model/runtime/transport/MultiplexingWireTransport.scala
@@ -12,7 +12,7 @@ trait MultiplexingWireTransport[InWire, Response, OutWire] {
   def process(request: InWire): Try[OutWire] = {
     Try(protocol.decode(request))
       .map(r => (r, services.filter(_.inAcceptable(r)).head)) // by design we have one service per input class
-      .map(rs => rs._2.processUnsafe(rs._1))
+      .map(rs => rs._2.processUnsafe(rs._1).asInstanceOf[Response])
       .map(protocol.encode)
   }
 }
