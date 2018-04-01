9770de19cac77ecf12346bda19207cbcff5d2889
pshirshov <pshirshov@gmail.com>
2018-04-01 23:55:00 +0100

    new service syntax

---
+++ idealingua/idealingua-core/src/main/scala/com/github/pshirshov/izumi/idealingua/il/parser/model/AlgebraicType.scala
@@ -0,0 +1,5 @@
+package com.github.pshirshov.izumi.idealingua.il.parser.model
+
+import com.github.pshirshov.izumi.idealingua.model.il.parsing.ILAstParsed
+
+case class AlgebraicType(alternatives: ILAstParsed.Types)
