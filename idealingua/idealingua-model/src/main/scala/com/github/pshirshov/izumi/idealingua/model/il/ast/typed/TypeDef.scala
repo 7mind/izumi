83d4bcd33664cb2b14e081c6752b0a5c2c27ecee
pshirshov <pshirshov@gmail.com>
2018-04-02 13:49:18 +0100

    package structure

---
+++ idealingua/idealingua-model/src/main/scala/com/github/pshirshov/izumi/idealingua/model/il/ast/typed/TypeDef.scala
@@ -0,0 +1,45 @@
+package com.github.pshirshov.izumi.idealingua.model.il.ast.typed
+
+import com.github.pshirshov.izumi.idealingua.model.common.TypeId._
+import com.github.pshirshov.izumi.idealingua.model.common._
+
+
+sealed trait TypeDef {
+  def id: TypeId
+}
+
+object TypeDef {
+
+  case class Alias(id: AliasId, target: TypeId) extends TypeDef
+
+  case class Enumeration(id: EnumId, members: List[String]) extends TypeDef
+
+  case class Adt(id: AdtId, alternatives: TypeList) extends TypeDef
+
+  case class Identifier(id: IdentifierId, fields: PrimitiveTuple) extends TypeDef
+
+  sealed trait WithStructure extends TypeDef {
+    def id: StructureId
+
+    def struct: Structure
+  }
+
+  case class Interface(id: InterfaceId, struct: Structure) extends WithStructure
+
+  case class DTO(id: DTOId, struct: Structure) extends WithStructure
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
