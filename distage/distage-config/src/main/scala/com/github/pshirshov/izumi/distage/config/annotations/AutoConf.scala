23a18bde738ba56b7f3df24ae57c9fe420c2a878
Kaidax <kaidaxofficial@gmail.com>
2018-05-17 22:56:18 +0100

    Detailed documentation for @AutoConf, @Conf and @ConfPath (#172)

--- distage/distage-config/src/main/scala/com/github/pshirshov/izumi/distage/config/annotations/AutoConf.scala
+++ distage/distage-config/src/main/scala/com/github/pshirshov/izumi/distage/config/annotations/AutoConf.scala
@@ -1,16 +1,73 @@
 package com.github.pshirshov.izumi.distage.config.annotations
 
 /**
-  * This annotation tells config resolution mechanism to use all the context information
-  * to resolve config entry, namely:
-  * - Config class name
-  * - Binding class name for the binding requiring the config value
-  * - Binding name for the binding requiring the config value
+  * Inject parameter from configuration.
   *
-  * So, altogether config path structure for autoconf entry would be
+  * The config path is derived automatically from the context.
   *
-  * {binding_type|fq_binding_type}.{config_type|fq_config_type}.{binding_name?|%}
+  * For example, given a class:
+  *
+  *   class Service(@AutoConf hostPort: HostPort)
+  *
+  * where:
+  *
+  *   case class HostPort(host: String, port: Int)
+  *
+  * you'll be able to define configuration like this:
+  *
+  *   Service {
+  *     HostPort {
+  *       host = "example.com"
+  *       port = 8080
+  *     }
+  *   }
+  *
+  * If the requesting class is bound as named binding, you can disambiguate
+  * the config path to uniquely configure that class:
+  *
+  *   new ModuleDef {
+  *     make[Service].named("namedService")
+  *   }
+  *
+  * Config:
+  *
+  *   Service {
+  *     namedService {
+  *       HostPort {
+  *         host = "example.com"
+  *         port = 8080
+  *       }
+  *     }
+  *   }
+  *
+  * If a class requests several values of the same type from config, you can
+  * disambiguate the config path using parameter names:
+  *
+  *   class Service(@AutoConf param1: HostPort, @AutoConf param2: HostPort)
+  *
+  * Config:
+  *
+  *   Service {
+  *     HostPort {
+  *       param1 {
+  *         host = "example.com"
+  *         port = 8080
+  *       }
+  *
+  *       param2 {
+  *         host = "google.com"
+  *         port = 80
+  *       }
+  *     }
+  *   }
+  *
+  * Specifically, the path is defined by segments:
+  *
+  *   {binding_type}.{binding_id_key?}.{config_type}.{parameter_name?}
+  *
+  * Where segments marked with ? at the end are optional.
   *
   * Use this annotation when you need to inject one config section into <i>one</i> component
+  *
   */
 final class AutoConf extends scala.annotation.StaticAnnotation
