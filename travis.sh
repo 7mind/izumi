994ca33e17cab26bf7836cad65c81510837a749a
pshirshov <pshirshov@gmail.com>
2018-05-21 15:11:27 +0100

    build

--- travis.sh
+++ travis.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -xe
 
+function csbt {
+sbt ++$TRAVIS_SCALA_VERSION -jvm-opts ./.sbtopts.travis $*
+}
+
 function versionate {
   if [[ "$TRAVIS_BRANCH" != "master" &&  "$TRAVIS_BRANCH" != "develop" && ! ( "$TRAVIS_TAG" =~ ^v.*$ ) ]] ; then
     echo "Setting version suffix to $TRAVIS_BRANCH"
-    sbt ++$TRAVIS_SCALA_VERSION "addVersionSuffix $TRAVIS_BRANCH"
+    csbt "addVersionSuffix $TRAVIS_BRANCH"
   else
     echo "No version suffix required"
   fi
@@ -11,22 +15,22 @@ function versionate {
 
 function coverage {
   echo "COVERAGE..."
-  sbt clean coverage test coverageReport || exit 1
+  csbt clean coverage test coverageReport || exit 1
   bash <(curl -s https://codecov.io/bash)
 }
 
 function scripted {
   echo "SCRIPTED..."
-  sbt clean "scripted sbt-izumi-plugins/*" || exit 1
+  csbt clean "scripted sbt-izumi-plugins/*" || exit 1
 }
 
 function deploy {
   if [[ -f .secrets/credentials.sonatype-nexus.properties ]] ; then
     echo "PUBLISH..."
-    sbt ++$TRAVIS_SCALA_VERSION +clean +publishSigned || exit 1
+    csbt +clean +publishSigned || exit 1
 
     if [[ "$TRAVIS_TAG" =~ ^v.*$ ]] ; then
-        sbt ++$TRAVIS_SCALA_VERSION sonatypeRelease || exit 1
+        csbt sonatypeRelease || exit 1
     fi
   fi
 }
