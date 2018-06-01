ccf844bc35edfaf6e7415f70358d4da50120f9fc
pshirshov <pshirshov@gmail.com>
2018-06-01 11:55:22 +0100

    variance info, cli issues fixed

--- idealingua/idealingua-model/src/main/scala/com/github/pshirshov/izumi/idealingua/model/common/ExtendedField.scala
+++ idealingua/idealingua-model/src/main/scala/com/github/pshirshov/izumi/idealingua/model/common/ExtendedField.scala
@@ -1,9 +1,10 @@
 package com.github.pshirshov.izumi.idealingua.model.common
 
 import com.github.pshirshov.izumi.idealingua.model.il.ast.typed
+import com.github.pshirshov.izumi.idealingua.model.il.ast.typed.Field
 
 
-final case class FieldDef(definedBy: TypeId, usedBy: TypeId, distance: Int)
+final case class FieldDef(definedBy: TypeId, usedBy: TypeId, distance: Int, variance: List[Field] = List.empty)
 
 final case class ExtendedField(field: typed.Field, defn: FieldDef) {
   override def toString: TypeName = s"${defn.usedBy}#$field <- ${defn.distance} - ${defn.definedBy}"
