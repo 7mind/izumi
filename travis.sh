79c37e7453f1e92b29cdb9a94402332100ed6cda
pshirshov <pshirshov@gmail.com>
2018-06-17 00:10:20 +0100

    language versions

--- travis.sh
+++ travis.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -xe
 
+export GOPATH=$HOME/gopath
+export PATH=$HOME/gopath/bin:$PATH
+
 function block_open {
     echo -en "travis_fold:start:$1\\r"
 }
@@ -74,9 +77,6 @@ function deploy {
   publish
   site
 }
-
-go version
-tsc -version
 
 PARAMS=()
 SOFT=0
