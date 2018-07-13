8c93cefc6629f3133bae03888f3548c5b0b00731
pshirshov <pshirshov@gmail.com>
2018-07-14 00:45:37 +0100

    #264: timestamp check in idl plugin / working

--- sbt/sbt-idealingua/src/main/scala/com/github/pshirshov/izumi/sbt/IdealinguaPlugin.scala
+++ sbt/sbt-idealingua/src/main/scala/com/github/pshirshov/izumi/sbt/IdealinguaPlugin.scala
@@ -1,6 +1,7 @@
 package com.github.pshirshov.izumi.sbt
 
 import java.nio.file.Path
+import java.security.MessageDigest
 
 import com.github.pshirshov.izumi.fundamentals.platform.files.IzFiles
 import com.github.pshirshov.izumi.fundamentals.platform.time.IzTime
@@ -19,10 +20,11 @@ import sbt.Keys.{sourceGenerators, watchSources, _}
 import sbt._
 import sbt.internal.util.ConsoleLogger
 import sbt.plugins._
+import scalacache.MD5
 
 object IdealinguaPlugin extends AutoPlugin {
 
-  final case class Scope(source: Path, target: Path)
+  final case class Scope(source: Path, buildTarget: Path, target: Path)
 
   sealed trait Mode
 
@@ -78,7 +80,7 @@ object IdealinguaPlugin extends AutoPlugin {
       val izumiSrcDir = src.resolve("main/izumi")
 
 
-      val scope = Scope(izumiSrcDir, srcManaged)
+      val scope = Scope(izumiSrcDir, target.value.toPath, srcManaged)
 
       val (scalaTargets, nonScalaTargets) = compilationTargets.value.partition(i => i.options.language == IDLLanguage.Scala)
 
@@ -87,16 +89,19 @@ object IdealinguaPlugin extends AutoPlugin {
 
       nonScalaTargets.foreach {
         t =>
-          val nonScalaScope = Scope(izumiSrcDir, (resManaged.toFile / s"${t.options.language}").toPath)
+          val nonScalaScope = Scope(izumiSrcDir, target.value.toPath, (resManaged.toFile / s"${t.options.language}").toPath)
           compileSources(nonScalaScope, Seq(t), depClasspath)
       }
 
       val files = scala_result.flatMap {
         case (_, Some(result)) =>
           result.invokation.flatMap(_._2.paths)
-        case ((_, scope), _) if scope.target.toFile.exists() =>
-          IzFiles.walk(scope.target.toFile)
-        case ((_, scope), _) if !scope.target.toFile.exists() =>
+        case ((_, s), _) if s.target.toFile.exists() =>
+          val existing = IzFiles.walk(scope.target.toFile).filterNot(_.toFile.isDirectory)
+          logger.info(s"Compiler didn't return a result, target ${s.target} exists. Reusing ${existing.size} files there...")
+          existing
+        case ((_, s), _) if !s.target.toFile.exists() =>
+          logger.info(s"Compiler didn't return a result, target ${s.target} does not exist. What the fuck? Okay, let's return nothing :/")
           Seq.empty
       }
 
@@ -111,7 +116,6 @@ object IdealinguaPlugin extends AutoPlugin {
         f =>
           val relative = idlbase.toPath.relativize(f.toPath)
           val targetPath = ((resourceManaged in Compile).value / "idealingua").toPath.resolve(relative).toFile
-
           f -> targetPath
       }
       IO.copy(mapped, CopyOptions().withOverwrite(true))
@@ -137,22 +141,24 @@ object IdealinguaPlugin extends AutoPlugin {
         case (a, t) =>
           val targetDir = target.value / "idealingua" / s"${a.name}-${a.classifier.get}-$versionValue-$scalaVersionValue"
 
-          val scope = Scope(src.resolve("main/izumi"), targetDir.toPath)
+          val scope = Scope(src.resolve("main/izumi"), target.value.toPath, targetDir.toPath)
 
-          val zipFile = targetDir / s"${a.name}-${a.classifier.get}-$versionValue.zip"
+          val zipFile = targetDir / s"${a.name}-${a.classifier.get}-$versionValue.zip.source"
 
           val result = doCompile(scope, t, (dependencyClasspath in Compile).value)
 
           result match {
             case Some(r) =>
+              logger.info(s"Have new compilation result, copying ${r.sources.toFile} info ${zipFile} for artifact $a")
               IO.copyDirectory(r.sources.toFile, zipFile)
-              //IO.copy(r.sources.toFile, zipFile)
-              //IO.zip(result.map(r => (r, scope.target.relativize(r.toPath).toString)), zipFile)
               Seq(a -> zipFile)
+
             case None =>
               if (zipFile.exists()) {
+                logger.info(s"Compiler didn't return a result, target $zipFile exists, reusing...")
                 Seq(a -> zipFile)
               } else {
+                logger.info(s"Compiler didn't return a result, target $zipFile does not exist. What the fuck? Okay, let's return nothing :/")
                 Seq.empty
               }
           }
@@ -179,23 +185,23 @@ object IdealinguaPlugin extends AutoPlugin {
   }
 
   private def doCompile(scope: Scope, invokation: Invokation, classpath: Classpath): Option[IDLCompiler.Result] = {
-
     val cp = classpath.map(_.data)
     val target = scope.target
     logger.debug(s"""Loading models from $scope...""")
 
-    val tsCache = target.resolve("izumi.timestamp")
+    val digest = MD5.messageDigest.clone().asInstanceOf[MessageDigest]
+    digest.reset()
+    val hash = digest.digest(scope.toString.getBytes).map("%02X".format(_)).mkString
+
+    val tsCache = scope.buildTarget.resolve(s"izumi-$hash.timestamp")
 
     val srcLastModified = IzFiles.getLastModified(scope.source.toFile)
     val targetLastModified = IzFiles.getLastModified(tsCache.toFile)
 
-    logger.info(s"""$srcLastModified, ${scope.source}""")
-    logger.info(s"""$targetLastModified, ${scope.target}""")
-
     val isNew = srcLastModified.flatMap(src => targetLastModified.map(tgt => (src, tgt)))
 
-    if (isNew.exists({case (src, tgt) => src.isAfter(tgt)}) || isNew.isEmpty) {
-      IO.write(tsCache.toFile, IzTime.isoNow)
+    if (isNew.exists({ case (src, tgt) => src.isAfter(tgt) }) || isNew.isEmpty) {
+      // TODO: maybe it's unsafe to destroy the whole directory?..
       val toCompile = new LocalModelLoader(scope.source, cp).load()
       if (toCompile.nonEmpty) {
         logger.info(s"""Going to compile the following models: ${toCompile.map(_.domain.id).mkString(",")}""")
@@ -211,9 +217,10 @@ object IdealinguaPlugin extends AutoPlugin {
           logger.debug(s"Model $id produced ${s.paths.size} source files...")
       }
 
+      IO.write(tsCache.toFile, IzTime.isoNow)
       Some(result)
     } else {
-      logger.info(s"""Output timestamp is okay, not recompiling ${scope.source}""")
+      logger.info(s"""Output timestamp is okay, not going to recompile ${scope.source} : ts=$tsCache""")
       None
     }
   }
