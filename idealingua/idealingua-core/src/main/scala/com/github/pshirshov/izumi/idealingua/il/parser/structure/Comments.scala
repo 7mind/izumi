2efb505d61e479eb756fe746ad37d753d8c1cd9a
pshirshov <pshirshov@gmail.com>
2018-07-31 20:35:07 +0100

    #303 /**/ cornercase disambiguated

--- idealingua/idealingua-core/src/main/scala/com/github/pshirshov/izumi/idealingua/il/parser/structure/Comments.scala
+++ idealingua/idealingua-core/src/main/scala/com/github/pshirshov/izumi/idealingua/il/parser/structure/Comments.scala
@@ -7,14 +7,14 @@ trait Comments
 
   final lazy val DocComment = {
     val Chunk = P(CharsWhile(c => c != '\n' && c != '\r').rep.!)
-    P("/*" ~ (!"*/" ~ "*" ~ Chunk).rep(1, sep = NLC ~ sep.wss) ~ NLC ~ sep.wss ~ "*/").map {
+    P(!"/**/" ~ "/*" ~ (!"*/" ~ "*" ~ Chunk).rep(1, sep = NLC ~ sep.wss) ~ NLC ~ sep.wss ~ "*/").map {
       s => s.mkString("\n")
     }
   }
 
   final lazy val MultilineComment: P0 = {
     val CommentChunk = P(CharsWhile(c => c != '/' && c != '*') | MultilineComment | !"*/" ~ AnyChar)
-    P(!"/**" ~ "/*" ~ CommentChunk.rep ~ "*/").rep(1)
+    P((!"/**" ~ "/*" ~ CommentChunk.rep ~ "*/") | "/**/").rep(1)
   }
 
   final lazy val ShortComment = P("//" ~ (CharsWhile(c => c != '\n' && c != '\r', min = 0) ~ NLC))
