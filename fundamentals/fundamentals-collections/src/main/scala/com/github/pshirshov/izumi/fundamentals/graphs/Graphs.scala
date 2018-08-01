00b2bc852ec881b1ae1e2b07df2e1309b6c358c9
pshirshov <pshirshov@gmail.com>
2018-08-02 00:30:50 +0100

    typos

--- fundamentals/fundamentals-collections/src/main/scala/com/github/pshirshov/izumi/fundamentals/graphs/Graphs.scala
+++ fundamentals/fundamentals-collections/src/main/scala/com/github/pshirshov/izumi/fundamentals/graphs/Graphs.scala
@@ -2,7 +2,7 @@ package com.github.pshirshov.izumi.fundamentals.graphs
 
 
 trait Graphs {
-  def toposort: Toposort
+  val toposort: Toposort = new Toposort {}
 }
 
 
--- fundamentals/fundamentals-collections/src/main/scala/com/github/pshirshov/izumi/fundamentals/graphs/package.scala
+++ fundamentals/fundamentals-collections/src/main/scala/com/github/pshirshov/izumi/fundamentals/graphs/package.scala
@@ -1,7 +1,4 @@
 package com.github.pshirshov.izumi.fundamentals
 
 package object graphs extends Graphs {
-  private object Toposort extends Toposort
-
-  override def toposort: Toposort = Toposort
 }
