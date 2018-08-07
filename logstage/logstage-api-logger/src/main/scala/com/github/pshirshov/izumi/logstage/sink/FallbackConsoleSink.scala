b8d3019f7e3d63dfc54d17ffc906cbd765db006e
ratoshniuk <mratoshniuk@gmail.com>
2018-08-07 13:56:05 +0300

    console sink mapper implemented

--- logstage/logstage-api-logger/src/main/scala/com/github/pshirshov/izumi/logstage/sink/FallbackConsoleSink.scala
+++ logstage/logstage-api-logger/src/main/scala/com/github/pshirshov/izumi/logstage/sink/FallbackConsoleSink.scala
@@ -4,9 +4,20 @@ import com.github.pshirshov.izumi.fundamentals.platform.console.TrivialLogger
 import com.github.pshirshov.izumi.logstage.api.Log
 import com.github.pshirshov.izumi.logstage.api.logger.LogSink
 import com.github.pshirshov.izumi.logstage.api.rendering.RenderingPolicy
+import com.github.pshirshov.izumi.logstage.config.codecs.LogSinkCodec.LogSinkMapper
+import com.typesafe.config.Config
 
 class FallbackConsoleSink(policy: RenderingPolicy, trivialLogger: TrivialLogger) extends LogSink {
   override def flush(e: Log.Entry): Unit = {
     trivialLogger.log(policy.render(e))
   }
 }
+
+//object ConsoleSink {
+//  val configInstanceMapper : LogSinkMapper[ConsoleSink] = new LogSinkMapper[ConsoleSink] {
+//    override def instantiate(config: Config, renderingPolicy: RenderingPolicy): ConsoleSink = {
+//      Quirks.discard(config)
+//      new ConsoleSink(renderingPolicy)
+//    }
+//  }
+//}
