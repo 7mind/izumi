738850b721566371ff7ddab3fee17ffeb83ba4e2
pshirshov <pshirshov@gmail.com>
2018-08-11 14:54:14 +0100

    zio nattran

--- idealingua/idealingua-runtime-rpc-scala/src/main/scala/com/github/pshirshov/izumi/idealingua/runtime/rpc/IRTZioResult.scala
+++ idealingua/idealingua-runtime-rpc-scala/src/main/scala/com/github/pshirshov/izumi/idealingua/runtime/rpc/IRTZioResult.scala
@@ -1,12 +1,21 @@
 package com.github.pshirshov.izumi.idealingua.runtime.rpc
 
+import cats.arrow.FunctionK
 import scalaz.zio.IO
 
+import scala.language.higherKinds
 
-trait IRTZioResult extends IRTResult[IO] {
+trait IRTZioableResult[R[_, _]] extends IRTResult[R] {
+  implicit def conv[E]: FunctionK[R[E, ?], IO[E, ?]]
+}
+
+
+trait IRTZioResult extends IRTZioableResult[IO] {
   def choice[L, R](v: => Either[L, R]): Or[L, R] = IO.fromEither(v)
 
   def just[R](v: => R): Just[R] = IO.point(v)
+
+  override implicit def conv[E]: FunctionK[IO[E, ?], IO[E, ?]] = FunctionK.id
 }
 
 
