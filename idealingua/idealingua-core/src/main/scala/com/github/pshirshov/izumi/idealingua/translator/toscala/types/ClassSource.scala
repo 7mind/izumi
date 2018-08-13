5397fe634a99c76069558f896caa58f1134a2747
pshirshov <pshirshov@gmail.com>
2018-08-13 21:56:26 +0100

    codestyle: odd {} removed

--- idealingua/idealingua-core/src/main/scala/com/github/pshirshov/izumi/idealingua/translator/toscala/types/ClassSource.scala
+++ idealingua/idealingua-core/src/main/scala/com/github/pshirshov/izumi/idealingua/translator/toscala/types/ClassSource.scala
@@ -2,21 +2,11 @@ package com.github.pshirshov.izumi.idealingua.translator.toscala.types
 
 import com.github.pshirshov.izumi.idealingua.model.il.ast.typed.TypeDef._
 
-sealed trait ClassSource {
-//  def nonEphemeral: TypeId
-}
+sealed trait ClassSource
 
 object ClassSource {
-  final case class CsDTO(dto: DTO) extends ClassSource {
-//    override def nonEphemeral: TypeId = dto.id
-  }
-  final case class CsInterface(i: Interface) extends ClassSource {
-//    override def nonEphemeral: TypeId = i.id
-  }
-  final case class CsMethodInput(sc: ServiceContext, smp: ServiceMethodProduct) extends ClassSource {
-//    override def nonEphemeral: TypeId = smp.Input.inputIdWrapped
-  }
-  final case class CsMethodOutput(sc: ServiceContext, smp: ServiceMethodProduct) extends ClassSource {
-//    override def nonEphemeral: TypeId = smp.Input.outputIdWrapped
-  }
+  final case class CsDTO(dto: DTO) extends ClassSource
+  final case class CsInterface(i: Interface) extends ClassSource
+  final case class CsMethodInput(sc: ServiceContext, smp: ServiceMethodProduct) extends ClassSource
+  final case class CsMethodOutput(sc: ServiceContext, smp: ServiceMethodProduct) extends ClassSource
 }
