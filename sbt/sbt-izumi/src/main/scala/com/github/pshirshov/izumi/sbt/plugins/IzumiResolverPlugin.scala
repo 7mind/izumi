036c11251f52417d574718eec1eeb9de091272a4
pshirshov <pshirshov@gmail.com>
2018-08-13 23:27:29 +0100

    configurable resolver options

--- sbt/sbt-izumi/src/main/scala/com/github/pshirshov/izumi/sbt/plugins/IzumiResolverPlugin.scala
+++ sbt/sbt-izumi/src/main/scala/com/github/pshirshov/izumi/sbt/plugins/IzumiResolverPlugin.scala
@@ -4,15 +4,16 @@ import sbt.Keys._
 import sbt._
 
 object IzumiResolverPlugin extends AutoPlugin {
+  import IzumiPropertiesPlugin.autoImport._
 
   override lazy val globalSettings = Seq(
-    aggregate in update := false,
+    aggregate in update := sys.props.getBoolean("build.update.aggregate", default = true),
     updateOptions := updateOptions
       .value
-      .withInterProjectFirst(true)
-      .withCachedResolution(true)
-      .withGigahorse(true)
-      .withLatestSnapshots(false)
+      .withInterProjectFirst(sys.props.getBoolean("build.update.inter-project-first", default = true))
+      .withCachedResolution(sys.props.getBoolean("build.update.cached-resolution", default = true))
+      .withGigahorse(sys.props.getBoolean("build.update.gigahorse", default = true))
+      .withLatestSnapshots(sys.props.getBoolean("build.update.latest-snapshots", default = false))
     ,
   )
 }
