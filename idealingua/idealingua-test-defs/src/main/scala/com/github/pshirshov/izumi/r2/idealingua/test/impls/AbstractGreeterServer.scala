db4c8c93e832a70e7134f8bc879dc4da8ee638e8
pshirshov <pshirshov@gmail.com>
2018-08-20 13:19:40 +0100

    wip

--- idealingua/idealingua-test-defs/src/main/scala/com/github/pshirshov/izumi/r2/idealingua/test/impls/AbstractGreeterServer.scala
+++ idealingua/idealingua-test-defs/src/main/scala/com/github/pshirshov/izumi/r2/idealingua/test/impls/AbstractGreeterServer.scala
@@ -4,8 +4,6 @@ import cats.MonadError
 import cats.data.EitherT
 import com.github.pshirshov.izumi.idealingua.runtime.rpc._
 import com.github.pshirshov.izumi.r2.idealingua.test.generated._
-import scalaz.zio.IO
-import IRTResultTransZio._
 
 import scala.language.higherKinds
 
@@ -39,50 +37,23 @@ object AbstractGreeterServer1 {
 
 }
 
-//trait AbstractGreeterServer2[R[_, _], C]
-//  extends GreeterServiceServer[R, C] {
-//  import R._
-//
-//  protected implicit def ME[E]: MonadError[Or[E, ?], E]
-//
-//  override def greet(ctx: C, name: String, surname: String): Just[String] = just {
-//    s"Hi, $name $surname!"
-//  }
-//
-//  override def sayhi(ctx: C): Just[String] = just {
-//    "Hi!"
-//  }
-//
-//  override def alternative(ctx: C): Or[Long, String] = choice {
-//    /*
-//    ME[Long].raiseError(45)
-//    ME[Long].pure("test")
-//    */
-//    Right("value")
-//  }
-//
-//  override def nothing(ctx: C): Or[Nothing, String] = just {
-//    ""
-//  }
-//}
-//
-//object AbstractGreeterServer2 {
-//
-//  class Impl[C] extends AbstractGreeterServer2[EitherT[cats.effect.IO, ?, ?], C] {
-//
-//    override def stop[V](v: => Throwable): Just[V] = ME[Nothing].point(throw v)
-//
-//    protected def ME[E]: MonadError[Or[E, ?], E] = implicitly
-//
-//    def choice[E, V](v: => Either[E, V]): Or[E, V] = v match {
-//      case Right(r) =>
-//        ME[E].pure(r)
-//
-//      case Left(l) =>
-//        ME[E].raiseError(l)
-//    }
-//
-//    def just[V](v: => V): Just[V] = ME[Nothing].pure(v)
-//  }
-//
-//}
+object AbstractGreeterServer2 {
+
+  implicit object EitherTResult extends IRTResult[EitherT[cats.effect.IO, ?, ?]] {
+    def ME[E]: MonadError[Or[E, ?], E] = implicitly
+
+    def stop[V](v: => Throwable): Just[V] = ME[Nothing].point(throw v)
+
+    def choice[E, V](v: => Either[E, V]): Or[E, V] = v match {
+      case Right(r) =>
+        ME[E].pure(r)
+
+      case Left(l) =>
+        ME[E].raiseError(l)
+    }
+
+    def just[V](v: => V): Just[V] = ME[Nothing].pure(v)
+  }
+
+  class Impl[C] extends AbstractGreeterServer1[EitherT[cats.effect.IO, ?, ?], C]
+}
