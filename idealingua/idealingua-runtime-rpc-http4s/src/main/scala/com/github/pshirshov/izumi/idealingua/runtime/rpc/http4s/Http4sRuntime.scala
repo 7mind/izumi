558bdb08cfdde3639f1f995a8ec9738cd71eb31d
pshirshov <pshirshov@gmail.com>
2018-08-20 17:27:11 +0100

    wip: more polymorphism

--- idealingua/idealingua-runtime-rpc-http4s/src/main/scala/com/github/pshirshov/izumi/idealingua/runtime/rpc/http4s/Http4sRuntime.scala
+++ idealingua/idealingua-runtime-rpc-http4s/src/main/scala/com/github/pshirshov/izumi/idealingua/runtime/rpc/http4s/Http4sRuntime.scala
@@ -23,15 +23,16 @@ class Http4sRuntime[ZIO[+ _, + _] : IRTResultTransZio, CaIO[+ _] : ConcurrentEff
 
   override type BIO[+E, +V] = ZIO[E, V]
 
-  override protected val BIO: IRTResultTransZio[BIO] = implicitly
-
   override type CIO[+T] = CaIO[T]
 
-  override protected val CIO: ConcurrentEffect[CIO] = implicitly[ConcurrentEffect[CIO]]
-  override protected val CIOT: Timer[CIO] = implicitly[Timer[CIO]]
+  override protected val BIO: IRTResultTransZio[BIO] = implicitly
+
+  override protected val CIO: ConcurrentEffect[CIO] = implicitly
+
+  override protected val CIOT: Timer[CIO] = implicitly
+
+  override protected val CIORunner: CIORunner[CaIO] = implicitly
 
   override protected val dsl: Http4sDsl[CIO] = Http4sDsl.apply[CIO]
-
-  override protected def CIORunner: CIORunner[CaIO] = implicitly
 
 }
