8a9bba7b717dfab1597c8ce9e312ce0a180a5d4c
pshirshov <pshirshov@gmail.com>
2018-08-22 14:17:59 +0100

    docker build

---
+++ docker.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+export SOURCEDIR=$(pwd)
+export CNAME="izumi-build-$(date +%s)"
+export IMAGE="septimalmind/izumi-env:latest"
+export RESOURCEDIR="$( cd "$(dirname "$0")" ; pwd -P )"
+export HOMEDIR="$( cd ~ ; pwd -P )"
+
+docker pull $IMAGE
+
+docker run --rm --name $CNAME \
+    -e TRAVIS_SCALA_VERSION=$TRAVIS_SCALA_VERSION \
+    -e TRAVIS_BRANCH=$TRAVIS_BRANCH \
+    -e TRAVIS_TAG=$TRAVIS_TAG \
+    -e TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST \
+    --volume $SSH_AUTH_SOCK:/ssh-agent --env SSH_AUTH_SOCK=/ssh-agent \
+    --volume "${SOURCEDIR}":/work:z \
+    --volume "${HOMEDIR}":/root:z \
+    --volume $(cat $SOURCEDIR/.git/objects/info/alternates):/work/.git/objects:z \
+    $IMAGE bash -xe travis.sh $1
