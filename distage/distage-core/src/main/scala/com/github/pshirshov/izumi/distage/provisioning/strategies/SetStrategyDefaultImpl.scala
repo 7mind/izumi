3596794988b6078e61e627a9e6cc902b7a40d6a0
pshirshov <pshirshov@gmail.com>
2018-09-29 15:56:51 +0100

    #276 safety checks for set strategy, set merging logic

--- distage/distage-core/src/main/scala/com/github/pshirshov/izumi/distage/provisioning/strategies/SetStrategyDefaultImpl.scala
+++ distage/distage-core/src/main/scala/com/github/pshirshov/izumi/distage/provisioning/strategies/SetStrategyDefaultImpl.scala
@@ -1,28 +1,53 @@
 package com.github.pshirshov.izumi.distage.provisioning.strategies
 
-import com.github.pshirshov.izumi.distage.model.exceptions.IncompatibleTypesException
+import com.github.pshirshov.izumi.distage.model.exceptions.{IncompatibleTypesException, MissingRefException, SanityCheckFailedException}
 import com.github.pshirshov.izumi.distage.model.plan.ExecutableOp.CreateSet
 import com.github.pshirshov.izumi.distage.model.provisioning.strategies.SetStrategy
 import com.github.pshirshov.izumi.distage.model.provisioning.{OpResult, ProvisioningKeyProvider}
-import com.github.pshirshov.izumi.distage.model.reflection.universe.RuntimeDIUniverse
+import com.github.pshirshov.izumi.distage.model.reflection.universe.RuntimeDIUniverse._
 
 class SetStrategyDefaultImpl extends SetStrategy {
   def makeSet(context: ProvisioningKeyProvider, op: CreateSet): Seq[OpResult.NewInstance] = {
-    import op._
     // target is guaranteed to be a Set
-    val scalaCollectionSetType = RuntimeDIUniverse.SafeType.get[collection.Set[_]]
+    val scalaCollectionSetType = SafeType.get[collection.Set[_]]
     val erasure = scalaCollectionSetType.tpe.typeSymbol
 
-    if (!tpe.tpe.baseClasses.contains(erasure)) {
+    if (!op.tpe.tpe.baseClasses.contains(erasure)) {
       throw new IncompatibleTypesException("Tried to create make a Set with a non-Set type! " +
-        s"For $target expected $tpe to be a sub-class of $scalaCollectionSetType, but it isn't!"
+        s"For ${op.target} expected ${op.tpe} to be a sub-class of $scalaCollectionSetType, but it isn't!"
         , scalaCollectionSetType
-        , tpe)
+        , op.tpe)
     }
 
-    val newSet = op.members.map(m => context.fetchKey(m).get)
+    val keyType = op.tpe.tpe.typeArgs match {
+      case head :: Nil =>
+        SafeType.apply(head)
+      case _ =>
+        throw new IncompatibleTypesException("Expected to be a set type but has no parameters", scalaCollectionSetType, op.tpe)
+    }
 
-    Seq(OpResult.NewInstance(target, newSet))
+    val badKeys = op.members.filterNot {
+      key =>
+        (key.tpe weak_<:< keyType) || (key.tpe weak_<:< op.tpe)
+    }
+
+    if (badKeys.nonEmpty) {
+      throw new SanityCheckFailedException(s"Target set ${op.target} expects ${op.tpe} but got members $badKeys")
+    }
+
+    val fetched = op.members
+      .map(m => (m, context.fetchKey(m)))
+
+    val newSet = fetched.flatMap {
+      case (m, Some(value)) if m.tpe weak_<:< op.tpe => // in case member type <:< set type we just add
+        value.asInstanceOf[collection.Set[Any]]
+      case (_, Some(value)) =>
+        Set(value)
+      case (m, None) =>
+        throw new MissingRefException(s"Failed to fetch set element $m", Set(m), None)
+    }
+
+    Seq(OpResult.NewInstance(op.target, newSet))
   }
 }
 
