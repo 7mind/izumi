9021d350392f938da4550f9fd1a2837807a5588c
Kai <kaidaxofficial@gmail.com>
2018-09-30 12:06:54 +0100

    Remove ModuleDef, PluginDef, BootstrapModuleDef instances

--- distage/distage-model/src/main/scala/com/github/pshirshov/izumi/distage/model/definition/BootstrapModuleDef.scala
+++ distage/distage-model/src/main/scala/com/github/pshirshov/izumi/distage/model/definition/BootstrapModuleDef.scala
@@ -3,10 +3,3 @@ package com.github.pshirshov.izumi.distage.model.definition
 import com.github.pshirshov.izumi.distage.model.definition.dsl.ModuleDefDSL
 
 trait BootstrapModuleDef extends BootstrapModule with ModuleDefDSL
-
-object BootstrapModuleDef {
-  implicit val bootstrapModuleDefApi: ModuleMake[BootstrapModuleDef] = b =>
-    new BootstrapModuleDef {
-      override val bindings: Set[Binding] = b
-    }
-}
