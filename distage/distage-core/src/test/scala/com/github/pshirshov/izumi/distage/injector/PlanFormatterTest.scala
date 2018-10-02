348e26ab6e0ffdd6746b509dbd3b041848d83ad4
dnaumenko <dm.naumenko@gmail.com>
2018-10-02 20:42:22 +0300

    test fix

--- distage/distage-core/src/test/scala/com/github/pshirshov/izumi/distage/injector/PlanFormatterTest.scala
+++ distage/distage-core/src/test/scala/com/github/pshirshov/izumi/distage/injector/PlanFormatterTest.scala
@@ -2,11 +2,11 @@ package com.github.pshirshov.izumi.distage.injector
 
 import com.github.pshirshov.izumi.distage.fixtures.BasicCases.BasicCase1.{Impl1, JustTrait}
 import com.github.pshirshov.izumi.distage.model.plan.PlanFormatter
-import distage.{Injector, ModuleDef}
+import distage.ModuleDef
 import org.scalatest.WordSpec
 
-class PlanTest extends WordSpec with MkInjector {
-  "OrderedPlan has an human-readable toString impl" in {
+class PlanFormatterTest extends WordSpec with MkInjector {
+  "PlanFormatterTest should produce short class names if it's unique in plan" in {
     val injector = mkInjector()
     val plan = injector.plan(new ModuleDef {
       many[JustTrait]
@@ -14,7 +14,6 @@ class PlanTest extends WordSpec with MkInjector {
     })
 
     val formatted = new PlanFormatter().format(plan)
-    println(formatted)
     assert(!formatted.contains(classOf[Impl1].getName))
   }
 }
