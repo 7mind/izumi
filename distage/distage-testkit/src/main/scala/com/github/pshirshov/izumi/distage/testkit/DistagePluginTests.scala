8920d030371ccd4fcc300fc5abea10e4cc4673d5
Kai <kaidaxofficial@gmail.com>
2018-10-03 20:40:08 +0100

    float up merge strategy

--- distage/distage-testkit/src/main/scala/com/github/pshirshov/izumi/distage/testkit/DistagePluginTests.scala
+++ distage/distage-testkit/src/main/scala/com/github/pshirshov/izumi/distage/testkit/DistagePluginTests.scala
@@ -1,28 +1,29 @@
 package com.github.pshirshov.izumi.distage.testkit
 
 import com.github.pshirshov.izumi.distage.model.definition.ModuleBase
-import com.github.pshirshov.izumi.distage.plugins.PluginBase
+import com.github.pshirshov.izumi.distage.plugins.{LoadedPlugins, PluginBase}
 import com.github.pshirshov.izumi.distage.plugins.load.PluginLoaderDefaultImpl
 import com.github.pshirshov.izumi.distage.plugins.load.PluginLoaderDefaultImpl.PluginConfig
-import com.github.pshirshov.izumi.distage.plugins.merge.ConfigurablePluginMergeStrategy
+import com.github.pshirshov.izumi.distage.plugins.merge.{ConfigurablePluginMergeStrategy, PluginMergeStrategy}
 import com.github.pshirshov.izumi.distage.plugins.merge.ConfigurablePluginMergeStrategy.PluginMergeConfig
 import com.github.pshirshov.izumi.fundamentals.tags.TagExpr
 
 trait DistagePluginTests extends DistageTests {
-  lazy val loadedPlugins: Seq[PluginBase] = {
+  protected lazy val loadedPlugins: Seq[PluginBase] = {
     new PluginLoaderDefaultImpl(
       PluginConfig(debug = false, pluginPackages, Seq.empty)
     ).load()
   }
 
-  protected def makeBindings: ModuleBase = {
-    val mergeStrategy = new ConfigurablePluginMergeStrategy(PluginMergeConfig(
+  protected def mergeStrategy: PluginMergeStrategy[LoadedPlugins] =
+    new ConfigurablePluginMergeStrategy(PluginMergeConfig(
       disabledTags
       , Set.empty
       , Set.empty
       , Map.empty
     ))
 
+  protected def makeBindings: ModuleBase = {
     val primaryModule = mergeStrategy.merge(loadedPlugins).definition
     primaryModule
   }
