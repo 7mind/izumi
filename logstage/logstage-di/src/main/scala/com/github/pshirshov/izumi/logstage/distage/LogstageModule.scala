75064785e54542f450de40da9f9777946962cb2d
Kai <kaidaxofficial@gmail.com>
2018-10-07 00:56:34 +0100

    revert erroneous delete

--- logstage/logstage-di/src/main/scala/com/github/pshirshov/izumi/logstage/distage/LogstageModule.scala
+++ logstage/logstage-di/src/main/scala/com/github/pshirshov/izumi/logstage/distage/LogstageModule.scala
@@ -8,5 +8,9 @@ import com.github.pshirshov.izumi.logstage.api.Log.CustomContext
 import com.github.pshirshov.izumi.logstage.api.logger.LogRouter
 
 class LogstageModule(router: LogRouter) extends BootstrapModuleDef {
-
+  make[LogRouter].from(router)
+  make[CustomContext].from(CustomContext.empty)
+  make[IzLogger]
+  make[PlanningObserver].from[PlanningObserverLoggingImpl]
+  make[LoggerHook].from[LoggerHookLoggingImpl]
 }
