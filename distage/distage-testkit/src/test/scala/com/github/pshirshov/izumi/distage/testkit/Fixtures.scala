e3375fbf5bc228c7bf9427fe1c6d6270407fabc4
Kai <kaidaxofficial@gmail.com>
2018-10-15 23:27:02 +0100

    add test for testkit closing resources in order

--- distage/distage-testkit/src/test/scala/com/github/pshirshov/izumi/distage/testkit/Fixtures.scala
+++ distage/distage-testkit/src/test/scala/com/github/pshirshov/izumi/distage/testkit/Fixtures.scala
@@ -3,8 +3,22 @@ package com.github.pshirshov.izumi.distage.testkit
 import com.github.pshirshov.izumi.distage.config.annotations.ConfPath
 import com.github.pshirshov.izumi.distage.plugins.PluginDef
 
-class TestService1 extends AutoCloseable {
-  override def close(): Unit = {}
+import scala.collection.mutable
+
+class InitCounter {
+  val closedCloseables: mutable.ArrayBuffer[AutoCloseable] = mutable.ArrayBuffer()
+}
+
+class TestResource1(counter: InitCounter) extends AutoCloseable {
+  override def close(): Unit = counter.closedCloseables += this
+}
+
+class TestResource2(val testResource1: TestResource1, counter: InitCounter) extends AutoCloseable {
+  override def close(): Unit = counter.closedCloseables += this
+}
+
+class TestService1(val testResource2: TestResource2, counter: InitCounter) extends AutoCloseable {
+  override def close(): Unit = counter.closedCloseables += this
 }
 
 case class TestConfig(x: Int, y: Int)
@@ -18,4 +32,7 @@ class TestService2(
 class TestPlugin extends PluginDef {
   make[TestService1]
   make[TestService2]
+  make[TestResource1]
+  make[TestResource2]
+  make[InitCounter]
 }
--- distage/distage-testkit/src/test/scala/com/github/pshirshov/izumi/distage/testkit/TestkitTest.scala
+++ distage/distage-testkit/src/test/scala/com/github/pshirshov/izumi/distage/testkit/TestkitTest.scala
@@ -2,15 +2,26 @@ package com.github.pshirshov.izumi.distage.testkit
 
 import com.github.pshirshov.izumi.distage.model.Locator.LocatorRef
 
-
 class TestkitTest extends DistagePluginSpec {
   "testkit" must {
     "load plugins" in di {
       (service: TestService1, locatorRef: LocatorRef) =>
         assert(locatorRef.get.instances.exists(_.value == service))
         assert(!locatorRef.get.instances.exists(_.value.isInstanceOf[TestService2]))
-        assert(locatorRef.get.get[Set[AutoCloseable]].size == 1)
+        assert(locatorRef.get.get[Set[AutoCloseable]].size == 3)
         assert(locatorRef.get.parent.get.get[Set[AutoCloseable]].isEmpty)
+    }
+
+    "close resources in correct order" in {
+      var ref: LocatorRef = null
+
+      di {
+        (_: TestService1, locatorRef: LocatorRef) =>
+          ref = locatorRef
+      }
+
+      val ctx = ref.get
+      assert(ctx.get[InitCounter].closedCloseables == Seq(ctx.get[TestService1], ctx.get[TestResource2], ctx.get[TestResource1]))
     }
   }
 }
