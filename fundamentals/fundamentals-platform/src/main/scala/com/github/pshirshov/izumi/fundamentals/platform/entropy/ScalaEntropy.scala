d916a07f808a52ab6303b5d1a277c01b59b90c80
pshirshov <pshirshov@gmail.com>
2018-10-17 15:14:12 +0100

    entropy source abstraction

---
+++ fundamentals/fundamentals-platform/src/main/scala/com/github/pshirshov/izumi/fundamentals/platform/entropy/ScalaEntropy.scala
@@ -0,0 +1,26 @@
+package com.github.pshirshov.izumi.fundamentals.platform.entropy
+
+import scala.collection.generic.CanBuildFrom
+
+trait ScalaEntropy extends Entropy {
+  protected def random: scala.util.Random
+  def shuffle[T, CC[X] <: TraversableOnce[X]](xs: CC[T])(implicit bf: CanBuildFrom[CC[T], T, CC[T]]): CC[T] = {
+    random.shuffle(xs)(bf)
+  }
+
+  override def nextInt(max: Int): Int = random.nextInt(max)
+
+  override def nextBoolean(): Boolean = random.nextBoolean()
+
+  override def nextBytes(bytes: Array[Byte]): Unit = random.nextBytes(bytes)
+
+  override def nextDouble(): Double = random.nextDouble()
+
+  override def nextFloat(): Float = random.nextFloat()
+
+  override def nextGaussian(): Double = random.nextGaussian()
+
+  override def nextLong(): Long = random.nextLong()
+
+  override def nextInt(): Int = random.nextInt()
+}
