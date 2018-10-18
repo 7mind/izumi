b2b1f3ba0ae76807fab4b2e08b403bc09ff8c5ab
Kai <kaidaxofficial@gmail.com>
2018-10-19 00:52:05 +0100

    rename keys to requiredKeys

--- distage/distage-app/src/main/scala/com/github/pshirshov/izumi/distage/app/plugin/macros/StaticPluginCheckerMacro.scala
+++ distage/distage-app/src/main/scala/com/github/pshirshov/izumi/distage/app/plugin/macros/StaticPluginCheckerMacro.scala
@@ -151,7 +151,7 @@ object StaticPluginCheckerMacro {
 
     val finalPlan = injector.plan(module).locateImports(bootstrap)
     val imports = finalPlan.unresolvedImports.left.getOrElse(Seq.empty).filter {
-      case i if moduleRequirements.fold(false)(_.keys contains i.target) => false
+      case i if moduleRequirements.fold(false)(_.requiredKeys contains i.target) => false
       case _ => true
     }
 
@@ -163,7 +163,7 @@ object StaticPluginCheckerMacro {
            |    ${imports.mkString("\n    ")}
            |
            |  Module requirements were:
-           |    ${moduleRequirements.fold(Set.empty[DIKey])(_.keys).mkString("\n    ")}
+           |    ${moduleRequirements.fold(Set.empty[DIKey])(_.requiredKeys).mkString("\n    ")}
            |
            |  Plan was:
            |${finalPlan.render(CompactPlanFormatter.OrderedPlanFormatter)}
