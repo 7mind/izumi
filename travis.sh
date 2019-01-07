8aa8f36a6db74538dec35bdec9f4119f79e382f9
Pavel Shirshov <pshirshov@gmail.com>
2019-01-07 01:25:14 +0000

    preliminary irt publishing

--- travis.sh
+++ travis.sh
@@ -75,6 +75,10 @@ function publish {
   fi
 
   echo "PUBLISH..."
+
+  export IZUMI_VERSION=$(cat version.sbt | awk 'match($0, /"(.+)"/, arr) { print arr[1] }' | sed -En "s/SNAPSHOT/build."${TRAVIS_BUILD_NUMBER}"/p")
+  ./idealingua/idealingua-runtime-rpc-typescript/src/npmjs/publish.sh
+
   csbt clean package publishSigned || exit 1
 
   if [[ "$TRAVIS_TAG" =~ ^v.*$ ]] ; then
