0cc97716e6abe0c39739ac98722be519da02138a
Kai <kaidaxofficial@gmail.com>
2019-01-08 15:32:33 +0000

    rename LogF -> LogIO, Log2 -> LogBIO

--- logstage/logstage-core/src/main/scala/com/github/pshirshov/izumi/logstage/macros/LogFMacros.scala
+++ logstage/logstage-core/src/main/scala/com/github/pshirshov/izumi/logstage/macros/LogFMacros.scala
@@ -2,32 +2,32 @@ package com.github.pshirshov.izumi.logstage.macros
 
 import com.github.pshirshov.izumi.fundamentals.reflection.CodePositionMaterializer.getEnclosingPosition
 import com.github.pshirshov.izumi.logstage.macros.LogMessageMacro.logMessageMacro
-import logstage.{Log, LogF}
+import logstage.{Log, LogIO}
 
 import scala.reflect.macros.blackbox
 
 object LogFMacros {
-  def scTraceMacro[F[_]](c: blackbox.Context { type PrefixType = LogF[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
+  def scTraceMacro[F[_]](c: blackbox.Context { type PrefixType = LogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
     c.universe.reify(c.prefix.splice.log(Log.Level.Trace)(logMessageMacro(c)(message).splice)(getEnclosingPosition(c).splice))
   }
 
-  def scDebugMacro[F[_]](c: blackbox.Context { type PrefixType = LogF[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
+  def scDebugMacro[F[_]](c: blackbox.Context { type PrefixType = LogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
     c.universe.reify(c.prefix.splice.log(Log.Level.Debug)(logMessageMacro(c)(message).splice)(getEnclosingPosition(c).splice))
   }
 
-  def scInfoMacro[F[_]](c: blackbox.Context { type PrefixType = LogF[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
+  def scInfoMacro[F[_]](c: blackbox.Context { type PrefixType = LogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
     c.universe.reify(c.prefix.splice.log(Log.Level.Info)(logMessageMacro(c)(message).splice)(getEnclosingPosition(c).splice))
   }
 
-  def scWarnMacro[F[_]](c: blackbox.Context { type PrefixType = LogF[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
+  def scWarnMacro[F[_]](c: blackbox.Context { type PrefixType = LogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
     c.universe.reify(c.prefix.splice.log(Log.Level.Warn)(logMessageMacro(c)(message).splice)(getEnclosingPosition(c).splice))
   }
 
-  def scErrorMacro[F[_]](c: blackbox.Context { type PrefixType = LogF[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
+  def scErrorMacro[F[_]](c: blackbox.Context { type PrefixType = LogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
     c.universe.reify(c.prefix.splice.log(Log.Level.Error)(logMessageMacro(c)(message).splice)(getEnclosingPosition(c).splice))
   }
 
-  def scCritMacro[F[_]](c: blackbox.Context { type PrefixType = LogF[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
+  def scCritMacro[F[_]](c: blackbox.Context { type PrefixType = LogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
     c.universe.reify(c.prefix.splice.log(Log.Level.Crit)(logMessageMacro(c)(message).splice)(getEnclosingPosition(c).splice))
   }
 }
--- logstage/logstage-core/src/main/scala/logstage/UnsafeLogIO.scala
+++ logstage/logstage-core/src/main/scala/logstage/UnsafeLogIO.scala
@@ -3,16 +3,16 @@ package logstage
 import com.github.pshirshov.izumi.functional.mono.SyncSafe
 import com.github.pshirshov.izumi.logstage.api.Log.{Entry, LoggerId}
 
-trait UnsafeLogF[+F[_]] {
+trait UnsafeLogIO[+F[_]] {
   def unsafeLog(entry: Entry): F[Unit]
   def acceptable(loggerId: LoggerId, logLevel: Level): F[Boolean]
 }
 
-object UnsafeLogF {
-  def apply[F[_]: UnsafeLogF]: UnsafeLogF[F] = implicitly
+object UnsafeLogIO {
+  def apply[F[_]: UnsafeLogIO]: UnsafeLogIO[F] = implicitly
 
-  def fromLogger[F[_]: SyncSafe](logger: IzLogger): UnsafeLogF[F] = {
-    new UnsafeLogF[F] {
+  def fromLogger[F[_]: SyncSafe](logger: IzLogger): UnsafeLogIO[F] = {
+    new UnsafeLogIO[F] {
       override def unsafeLog(entry: Entry): F[Unit] = {
         SyncSafe[F].syncSafe(logger.unsafeLog(entry))
       }
