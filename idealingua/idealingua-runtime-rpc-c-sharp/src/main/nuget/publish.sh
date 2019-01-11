2fe7600ab1de2cb21ec70ae81d5f0de0fc64c27b
Pavel Shirshov <pshirshov@gmail.com>
2019-01-11 13:18:34 +0000

    nuget publish/verbosity=detailed

--- idealingua/idealingua-runtime-rpc-c-sharp/src/main/nuget/publish.sh
+++ idealingua/idealingua-runtime-rpc-c-sharp/src/main/nuget/publish.sh
@@ -5,16 +5,19 @@ export THISDIR="$( cd "$(dirname "$0")" ; pwd -P )"
 pushd .
 cd $THISDIR
 
-cat irt.nuspec | sed 's/0.0.1-UNSET/'${IZUMI_VERSION}'/g' > irt.tmp.nuspec
+rm -rf *.nupkg
 
-nuget pack irt.tmp.nuspec
-rm irt.tmp.nuspec
+NUSPEC=irt.tmp.nuspec
+cat irt.nuspec | sed 's/0.0.1-UNSET/'${IZUMI_VERSION}'/g' > $NUSPEC
+cat $NUSPEC
+nuget pack $NUSPEC
+rm $NUSPEC
 
 nuget setapikey $NUGET_TOKEN
 
 for TRG in $(find . -name '*.nupkg' -type f -print)
 do
-    nuget push $TRG -Source https://www.nuget.org
+    nuget push $TRG -Source https://www.nuget.org -Verbosity detailed
 done
 
 popd
