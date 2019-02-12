7df9c03d8a45ca3e8f3d166325c2e28014ba9116
Pavel Shirshov <pshirshov@gmail.com>
2019-02-12 14:07:48 +0000

    circular dependency breaker limited to actual loop members

--- fundamentals/fundamentals-collections/src/main/scala/com/github/pshirshov/izumi/fundamentals/graphs/Toposort.scala
+++ fundamentals/fundamentals-collections/src/main/scala/com/github/pshirshov/izumi/fundamentals/graphs/Toposort.scala
@@ -3,6 +3,28 @@ package com.github.pshirshov.izumi.fundamentals.graphs
 import scala.annotation.tailrec
 
 trait Toposort {
+
+  private def isInvolvedIntoCycle[T](toPreds: Map[T, scala.collection.immutable.Set[T]])(key: T): Boolean = {
+    test(toPreds, Set.empty, key, key)
+  }
+
+  private def test[T](toPreds: Map[T, scala.collection.immutable.Set[T]], stack: Set[T], toTest: T, needle: T): Boolean = {
+    val deps = toPreds.getOrElse(toTest, Set.empty)
+
+    if (deps.contains(needle)) {
+      true
+    } else {
+      deps.exists {
+        d =>
+          if (stack.contains(d)) {
+           false
+          } else {
+            test(toPreds, stack + d, d, needle)
+          }
+      }
+    }
+  }
+
   @tailrec
   final def cycleBreaking[T](toPreds: Map[T, scala.collection.immutable.Set[T]], done: Seq[T], break: Set[T] => T): Seq[T] = {
     val (noPreds, hasPreds) = toPreds.partition {
@@ -12,10 +34,12 @@ trait Toposort {
     if (noPreds.isEmpty) {
       if (hasPreds.isEmpty) {
         done
-      } else { // circular dependency, trying to break it by removing head
-        val breakLoopAt = break(hasPreds.keySet)
+      } else { // circular dependency
+        val loopMembers = hasPreds.filterKeys(isInvolvedIntoCycle(hasPreds))
+        val breakLoopAt = break(loopMembers.keySet)
         val found = Set(breakLoopAt)
         val next = hasPreds.filterKeys(k => k != breakLoopAt).mapValues(_ -- found).toMap
+
         cycleBreaking(next, done ++ found, break) // 2.13 compat
       }
     } else {
