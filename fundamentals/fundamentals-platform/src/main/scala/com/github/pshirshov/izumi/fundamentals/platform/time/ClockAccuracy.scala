85e6ada37611002d3abaa493ba283b685e6cb92b
Alex Liubymov <arowcap@gmail.com>
2019-02-28 12:38:20 +0200

    cleanup

--- fundamentals/fundamentals-platform/src/main/scala/com/github/pshirshov/izumi/fundamentals/platform/time/ClockAccuracy.scala
+++ fundamentals/fundamentals-platform/src/main/scala/com/github/pshirshov/izumi/fundamentals/platform/time/ClockAccuracy.scala
@@ -7,6 +7,14 @@ sealed trait ClockAccuracy
 
 object ClockAccuracy {
 
+  case object DEFAULT extends ClockAccuracy
+  case object NANO extends ClockAccuracy
+  case object MILLIS extends ClockAccuracy
+  case object MICROS extends ClockAccuracy
+  case object SECONDS extends ClockAccuracy
+  case object MINUTES extends ClockAccuracy
+  case object HOURS extends ClockAccuracy
+
   def applyAccuracy(now: ZonedDateTime, clockAccuracy: ClockAccuracy): ZonedDateTime = {
     clockAccuracy match {
       case ClockAccuracy.DEFAULT => now
@@ -18,19 +26,4 @@ object ClockAccuracy {
       case ClockAccuracy.HOURS => now.truncatedTo(ChronoUnit.HOURS)
     }
   }
-
-  case object DEFAULT extends ClockAccuracy
-
-  case object NANO extends ClockAccuracy
-
-  case object MILLIS extends ClockAccuracy
-
-  case object MICROS extends ClockAccuracy
-
-  case object SECONDS extends ClockAccuracy
-
-  case object MINUTES extends ClockAccuracy
-
-  case object HOURS extends ClockAccuracy
-
 }
