f1137313dcce0f6594aea248b150f5aac281b964
Kai <kaidaxofficial@gmail.com>
2019-03-14 17:20:40 +0000

    Fix #492, add helpers to modify request context in IRTWrappedService

--- idealingua/idealingua-runtime-rpc-scala/src/main/scala/com/github/pshirshov/izumi/idealingua/runtime/rpc/IRTMethodWrapper.scala
+++ idealingua/idealingua-runtime-rpc-scala/src/main/scala/com/github/pshirshov/izumi/idealingua/runtime/rpc/IRTMethodWrapper.scala
@@ -1,13 +1,20 @@
 package com.github.pshirshov.izumi.idealingua.runtime.rpc
 
-abstract class IRTMethodWrapper[Or[_, _], C] {
-  type Just[T] = Or[Nothing, T]
+abstract class IRTMethodWrapper[R[_, _], -C] { self =>
+  final type Just[T] = R[Nothing, T]
 
   val signature: IRTMethodSignature
   val marshaller: IRTCirceMarshaller
 
   def invoke(ctx: C, input: signature.Input): Just[signature.Output]
+
+  final def contramap[D](f: D => C): IRTMethodWrapper[R, D] = {
+    new IRTMethodWrapper[R, D] {
+      override final val signature: self.signature.type = self.signature
+      override final val marshaller: self.marshaller.type = self.marshaller
+      override final def invoke(ctx: D, input: self.signature.Input): R[Nothing, self.signature.Output] = {
+        self.invoke(f(ctx), input)
+      }
+    }
+  }
 }
-
-
-
