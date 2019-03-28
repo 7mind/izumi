7908e1610e4394a9deda6dd016ebc96c26e3edf0
Kai <kaidaxofficial@gmail.com>
2019-03-28 16:13:07 +0000

    fix warning in distage-cats

--- distage/distage-cats/src/main/scala/distage/interop/cats/package.scala
+++ distage/distage-cats/src/main/scala/distage/interop/cats/package.scala
@@ -69,17 +69,4 @@ package object cats
         Applicative[F].pure(op)
     }
 
-  private[cats] final class ResolveImportFSemiPlanPartiallyApplied[T](private val plan: SemiPlan) extends AnyVal {
-    def apply[F[_]: Applicative](f: F[T])(implicit ev: Tag[T]): F[SemiPlan] =
-      plan.resolveImportsF[F] {
-        case i if i.target == DIKey.get[T] => f.asInstanceOf[F[Any]]
-      }
-  }
-
-  private[cats] final class ResolveImportFOrderedPlanPartiallyApplied[T](private val plan: OrderedPlan) extends AnyVal {
-    def apply[F[_]: Applicative](f: F[T])(implicit ev: Tag[T]): F[OrderedPlan] =
-      plan.resolveImportsF[F] {
-        case i if i.target == DIKey.get[T] => f.asInstanceOf[F[Any]]
-      }
-  }
 }
