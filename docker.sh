07d1d412d70539c90ff0b2f83e77fc80529d3022
Pavel Shirshov <pshirshov@gmail.com>
2019-05-07 23:59:27 +0100

    wip

--- docker.sh
+++ docker.sh
@@ -7,21 +7,29 @@ export RESOURCEDIR="$( cd "$(dirname "$0")" ; pwd -P )"
 export HOMEDIR="$( cd ~ ; pwd -P )"
 
 if [[ "$SYSTEM_PULLREQUEST_PULLREQUESTNUMBER" == ""  ]] ; then
+    export TRAVIS_PULL_REQUEST=false
+else
+    export TRAVIS_PULL_REQUEST=true
+fi
+
+export TRAVIS_BRANCH=$(echo $BUILD_SOURCEBRANCH | sed -E "s/refs\/heads//")
+export TRAVIS_TAG=$BUILD_SOURCEBRANCH
+
+if [[ "$TRAVIS_PULL_REQUEST" == "false"  ]] ; then
     openssl aes-256-cbc -K ${OPENSSL_KEY} -iv ${OPENSSL_IV} -in secrets.tar.enc -out secrets.tar -d
     tar xvf secrets.tar
     ln -s .secrets/local.sbt local.sbt
-    export TRAVIS_PULL_REQUEST=false
-    echo "UNPACKED!"
-else
-    export TRAVIS_PULL_REQUEST=true
-    echo "P/R!"
 fi
+
+env
 
 docker pull $IMAGE
 
+env
+
 docker run --rm --name $CNAME \
-  -e TRAVIS_BRANCH=${BUILD_SOURCEBRANCH} \
-  -e TRAVIS_TAG=${BUILD_SOURCEBRANCH} \
+  -e TRAVIS_BRANCH=${TRAVIS_BRANCH} \
+  -e TRAVIS_TAG=${TRAVIS_TAG} \
   -e TRAVIS_PULL_REQUEST=${TRAVIS_PULL_REQUEST} \
   -e TRAVIS_BUILD_NUMBER=${BUILD_BUILDID} \
   -e NPM_TOKEN=${TOKEN_NPM} \
