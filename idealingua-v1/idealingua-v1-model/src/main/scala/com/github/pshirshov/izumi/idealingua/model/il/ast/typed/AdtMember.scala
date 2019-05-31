f3e449446624bd4d0940953145f58c1ec9fa39e7
Pavel Shirshov <pshirshov@gmail.com>
2019-05-31 13:03:14 +0100

    adt/wireid fix

--- idealingua-v1/idealingua-v1-model/src/main/scala/com/github/pshirshov/izumi/idealingua/model/il/ast/typed/AdtMember.scala
+++ idealingua-v1/idealingua-v1-model/src/main/scala/com/github/pshirshov/izumi/idealingua/model/il/ast/typed/AdtMember.scala
@@ -3,5 +3,5 @@ package com.github.pshirshov.izumi.idealingua.model.il.ast.typed
 import com.github.pshirshov.izumi.idealingua.model.common.{TypeId, TypeName}
 
 final case class AdtMember(typeId: TypeId, memberName: Option[TypeName], meta: NodeMeta) {
-  def name: TypeName = memberName.getOrElse(typeId.name).capitalize
+  def name: TypeName = memberName.getOrElse(typeId.name)
 }
