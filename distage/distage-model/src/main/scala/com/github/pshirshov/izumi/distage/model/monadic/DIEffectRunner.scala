d7f9666cf12c8809208efda1b8e26577cf5c0d4b
Kai <kaidaxofficial@gmail.com>
2019-07-04 17:35:28 +0100

    Suspend DIEffectRunner argument

--- distage/distage-model/src/main/scala/com/github/pshirshov/izumi/distage/model/monadic/DIEffectRunner.scala
+++ distage/distage-model/src/main/scala/com/github/pshirshov/izumi/distage/model/monadic/DIEffectRunner.scala
@@ -5,24 +5,24 @@ import com.github.pshirshov.izumi.functional.bio.BIORunner
 import com.github.pshirshov.izumi.fundamentals.platform.functional.Identity
 
 trait DIEffectRunner[F[_]] {
-  def run[A](f: F[A]): A
+  def run[A](f: => F[A]): A
 }
 
 object DIEffectRunner {
   def apply[F[_]: DIEffectRunner]: DIEffectRunner[F] = implicitly
 
   implicit object IdentityImpl extends DIEffectRunner[Identity] {
-    override def run[A](f: Identity[A]): A = f
+    override def run[A](f: => A): A = f
   }
 
   implicit object CatsIOImpl extends DIEffectRunner[IO] {
-    override def run[A](f: IO[A]): A = f.unsafeRunSync()
+    override def run[A](f: => IO[A]): A = f.unsafeRunSync()
   }
 
   implicit def bio[F[_, _]: BIORunner]: DIEffectRunner[F[Throwable, ?]] = new BIOImpl[F]
 
   final class BIOImpl[F[_, _]: BIORunner] extends DIEffectRunner[F[Throwable, ?]] {
-    override def run[A](f: F[Throwable, A]): A = BIORunner[F].unsafeRun(f)
+    override def run[A](f: => F[Throwable, A]): A = BIORunner[F].unsafeRun(f)
   }
 
 }
