302aa542e55822945ba5776d6243673d38fbf2b8
Pavel Shirshov <pshirshov@gmail.com>
2019-07-14 23:12:02 +0100

    full distage test flow

--- distage/distage-testkit/src/test/scala/com/github/pshirshov/izumi/distage/testkit/TestkitTest.scala
+++ distage/distage-testkit/src/test/scala/com/github/pshirshov/izumi/distage/testkit/TestkitTest.scala
@@ -4,13 +4,142 @@ import cats.effect.IO
 import com.github.pshirshov.izumi.distage.config.annotations.ConfPathId
 import com.github.pshirshov.izumi.distage.config.{ConfigInjectionOptions, ConfigProvider}
 import com.github.pshirshov.izumi.distage.model.Locator.LocatorRef
-import com.github.pshirshov.izumi.distage.model.definition.ModuleDef
+import com.github.pshirshov.izumi.distage.model.definition.Axis.AxisValue
+import com.github.pshirshov.izumi.distage.model.definition.StandardAxis.Env
+import com.github.pshirshov.izumi.distage.model.definition.{AxisBase, BootstrapModuleDef, ModuleDef}
 import com.github.pshirshov.izumi.distage.model.monadic.DIEffect
-import com.github.pshirshov.izumi.distage.roles.services.ModuleProviderImpl
+import com.github.pshirshov.izumi.distage.model.planning.PlanMergingPolicy
+import com.github.pshirshov.izumi.distage.plugins.load.PluginLoader.PluginConfig
+import com.github.pshirshov.izumi.distage.plugins.merge.{PluginMergeStrategy, SimplePluginMergeStrategy}
+import com.github.pshirshov.izumi.distage.roles.BootstrapConfig
+import com.github.pshirshov.izumi.distage.roles.model.AppActivation
+import com.github.pshirshov.izumi.distage.roles.model.meta.RolesInfo
+import com.github.pshirshov.izumi.distage.roles.services._
+import com.github.pshirshov.izumi.distage.testkit.DistagePluginTestSupport.{CacheKey, CacheValue}
+import com.github.pshirshov.izumi.distage.testkit.DistageTestRunner.DistageTest
 import com.github.pshirshov.izumi.distage.testkit.TestkitTest.NotAddedClass
 import com.github.pshirshov.izumi.distage.testkit.fixtures._
 import com.github.pshirshov.izumi.fundamentals.platform.functional.Identity
+import com.github.pshirshov.izumi.fundamentals.platform.language.Quirks
+import com.github.pshirshov.izumi.logstage.api.{IzLogger, Log}
 import distage.{DIKey, ModuleBase, TagK}
+import org.scalatest.WordSpec
+
+class DistageTestRunnerTest extends WordSpec {
+  "distage test runner" should {
+    "support shared component flow" in {
+      val runner = new DistageTestRunner[IO]()
+
+      val logger = IzLogger.apply(Log.Level.Debug)("phase" -> "test")
+
+      val env = loadEnvironment(logger)
+
+      runner.register(DistageTest({
+        service: TestService1 =>
+          for {
+            _ <- IO.delay(assert(service != null))
+            _ <- IO.delay(println("test1"))
+          } yield {
+
+          }
+
+      }, env))
+
+      runner.register(DistageTest({
+        service: TestService1 =>
+          for {
+            _ <- IO.delay(assert(service != null))
+            _ <- IO.delay(println("test2"))
+          } yield {
+
+          }
+
+      }, env))
+
+      runner.run()
+
+    }
+  }
+
+
+
+  /**
+    * Merge strategy will be applied only once for all the tests with the same bootstrap config when memoization is on
+    */
+  final def loadEnvironment(logger: IzLogger): TestEnvironment = {
+    val config = bootstrapConfig
+
+    def env(): CacheValue = {
+      val plugins = makePluginLoader(config).load()
+      val mergeStrategy = makeMergeStrategy(logger)
+      val defApp = mergeStrategy.merge(plugins.app)
+      val bootstrap = mergeStrategy.merge(plugins.bootstrap)
+      val availableActivations = ActivationParser.findAvailableChoices(logger, defApp)
+      CacheValue(plugins, bootstrap, defApp, availableActivations)
+    }
+
+    val plugins = if (memoizePlugins) {
+      DistagePluginTestSupport.Cache.getOrCompute(CacheKey(config), env())
+    } else {
+      env()
+    }
+
+    doLoad(logger, plugins)
+  }
+
+  protected final def doLoad(logger: IzLogger, env: CacheValue): TestEnvironment = {
+    val roles = loadRoles(logger)
+    val appActivation = AppActivation(env.availableActivations, activation)
+    val defBs = env.bsModule overridenBy new BootstrapModuleDef {
+      make[PlanMergingPolicy].from[PruningPlanMergingPolicy]
+      make[AppActivation].from(appActivation)
+    }
+    TestEnvironment(
+      defBs,
+      env.appModule,
+      roles,
+      appActivation,
+    )
+  }
+
+  protected def memoizePlugins: Boolean = {
+    import com.github.pshirshov.izumi.fundamentals.platform.strings.IzString._
+
+    System.getProperty("izumi.distage.testkit.plugins.memoize")
+      .asBoolean(true)
+  }
+
+  protected def loadRoles(logger: IzLogger): RolesInfo = {
+    Quirks.discard(logger)
+    // For all normal scenarios we don't need roles to setup a test
+    RolesInfo(Set.empty, Seq.empty, Seq.empty, Seq.empty, Set.empty)
+  }
+
+  protected def activation: Map[AxisBase, AxisValue] = Map(Env -> Env.Test)
+
+  protected def makeMergeStrategy(lateLogger: IzLogger): PluginMergeStrategy = {
+    Quirks.discard(lateLogger)
+    SimplePluginMergeStrategy
+  }
+
+  protected def bootstrapConfig: BootstrapConfig = {
+    BootstrapConfig(
+      PluginConfig(debug = false, pluginPackages, Seq.empty),
+      pluginBootstrapPackages.map(p => PluginConfig(debug = false, p, Seq.empty)),
+    )
+  }
+
+  protected def makePluginLoader(bootstrapConfig: BootstrapConfig): PluginSource = {
+    new PluginSourceImpl(bootstrapConfig)
+  }
+
+  protected final def thisPackage: Seq[String] = Seq(this.getClass.getPackage.getName)
+
+  protected def pluginPackages: Seq[String] = thisPackage
+
+  protected def pluginBootstrapPackages: Option[Seq[String]] = None
+}
+
 
 
 abstract class TestkitTest[F[_] : TagK] extends TestkitSelftest[F] {
