d21301aae91f11bff0deb8639f4f1d2a3907d47e
Pavel Shirshov <pshirshov@gmail.com>
2019-07-15 12:42:59 +0100

    full distage test flow: comments & improvements

--- distage/distage-testkit/src/main/scala/com/github/pshirshov/izumi/distage/testkit/DistageTestRunner.scala
+++ distage/distage-testkit/src/main/scala/com/github/pshirshov/izumi/distage/testkit/DistageTestRunner.scala
@@ -20,20 +20,23 @@ import distage.{DIKey, Injector, OrderedPlan, PlannerInput}
 import com.github.pshirshov.izumi.distage.model.reflection.universe.RuntimeDIUniverse._
 import com.github.pshirshov.izumi.fundamentals.platform.functional.Identity
 import com.github.pshirshov.izumi.distage.model.monadic.DIEffect.syntax._
+import com.github.pshirshov.izumi.distage.testkit.DistageTestRunner.TestReporter
 
+// a marker trait just for our demo purposes. All the entities inheriting this trait will be shared between tests contexts
 trait TODOMemoizeMe {}
 
-class DistageTestRunner[F[_]](implicit val tagK: TagK[F]) {
+class DistageTestRunner[F[_]](reporter: TestReporter)(implicit val tagK: TagK[F]) {
 
   import DistageTestRunner._
 
   private val tests = scala.collection.mutable.ArrayBuffer[DistageTest[F]]()
 
   def register(test: DistageTest[F]): Unit = {
+    reporter.testStatus(test.id, TestStatus.Scheduled)
     tests += test
   }
 
-  def run() = {
+  def run(): Unit = {
     val groups = tests.groupBy(_.environment)
 
     groups.foreach {
@@ -43,11 +46,14 @@ class DistageTestRunner[F[_]](implicit val tagK: TagK[F]) {
         val loader = makeConfigLoader(logger)
         val config = loader.buildConfig()
 
+        // here we scan our classpath to enumerate of our components (we have "bootstrap" components - injector plugins, and app components)
         val provider = makeModuleProvider(options, config, logger, env.roles, env.activation)
         val bsModule = provider.bootstrapModules().merge overridenBy env.bsModule overridenBy bootstrapOverride
         val appModule = provider.appModules().merge overridenBy env.appModule
 
         val injector = Injector.Standard(bsModule)
+
+        // first we need to plan runtime for our monad. Identity is also supported
         val runtimeGcRoots: Set[DIKey] = Set(
           DIKey.get[DIEffectRunner[F]],
           DIKey.get[DIEffect[F]],
@@ -55,23 +61,27 @@ class DistageTestRunner[F[_]](implicit val tagK: TagK[F]) {
 
         val runtimePlan = injector.plan(PlannerInput(appModule, runtimeGcRoots))
 
+        // here we plan all the job for each individual test
         val testplans = group.map {
           pm =>
             val keys = pm.test.get.diKeys.toSet
             pm -> injector.plan(PlannerInput(appModule, keys))
         }
 
+        // here we find all the shared components in each of our individual tests
         val sharedKeys = testplans.map(_._2).flatMap {
           plan =>
             plan.steps.filter(op => ExecutableOp.instanceType(op) weak_<:< SafeType.get[TODOMemoizeMe]).map(_.target)
         }.toSet -- runtimeGcRoots
 
+        // here we build merged plan for all our shared components
         val sharedPlan = if (sharedKeys.nonEmpty) {
           injector.plan(PlannerInput(appModule.drop(runtimePlan.keys), sharedKeys))
         } else {
           emptyPlan(runtimePlan)
         }
 
+        // here we extract integration checks out of our shared components plan and build it
         val sharedIntegrationKeys = sharedPlan.collectChildren[IntegrationCheck].map(_.target).toSet
         val sharedIntegrationPlan = if (sharedIntegrationKeys.nonEmpty) {
           // exclude runtime
@@ -79,29 +89,37 @@ class DistageTestRunner[F[_]](implicit val tagK: TagK[F]) {
         } else {
           emptyPlan(runtimePlan)
         }
+
+        // and here we build our final plan for shared components, with integration components excluded
         val sharedPlanNoInteg = if (sharedIntegrationKeys.nonEmpty) {
           injector.plan(PlannerInput(appModule.drop(sharedIntegrationPlan.keys ++ runtimePlan.keys), sharedKeys -- sharedIntegrationKeys))
         } else {
           sharedPlan
         }
 
+        // first we produce our Monad's runtime
         injector.produceF[Identity](runtimePlan).use {
           runtimeLocator =>
             val runner = runtimeLocator.get[DIEffectRunner[F]]
             implicit val effect: DIEffect[F] = runtimeLocator.get[DIEffect[F]]
 
             runner.run {
+              // now we produce integration components for our shared plan
               Injector.inherit(runtimeLocator).produceF[F](sharedIntegrationPlan).use {
                 sharedIntegrationLocator =>
-                  // TODO: global integration checks
+                  // TODO: here we should perform global integration checks and ignore all the tests in case they fail
 
+                  // here we produce our shared plan
                   Injector.inherit(sharedIntegrationLocator).produceF[F](sharedPlanNoInteg).use {
                     sharedLocator =>
                       val testInjector = Injector.inherit(sharedLocator)
 
+                      // now we are ready to run each individual test
+                      // note: scheduling here is custom also and tests may automatically run in parallel for any non-trivial monad
                       effect.traverse_(testplans) {
-                        case (pm, testplan) =>
+                        case (test, testplan) =>
 
+                          // we repeat the same approach with splitting our test plan into two plans - primary and integration
                           val allSharedKeys = sharedLocator.allInstances.map(_.key).toSet
                           val integrationKeys = testplan.collectChildren[IntegrationCheck].map(_.target).toSet -- allSharedKeys
                           val integrationPlan = if (integrationKeys.nonEmpty) {
@@ -116,16 +134,31 @@ class DistageTestRunner[F[_]](implicit val tagK: TagK[F]) {
                             testplan
                           }
 
+                          // we are ready to run the test, finally
                           testInjector.produceF[F](integrationPlan).use {
                             integLocator =>
                               // TODO: test-specific integration checks
                               Injector.inherit(integLocator).produceF[F](planNoInteg).use {
                                 testLocator =>
+                                  def doRun = for {
+                                    _ <- testLocator.run(test.test)
+                                    _ <- effect.maybeSuspend(reporter.testStatus(test.id, TestStatus.Succeed))
+                                  } yield {
+                                  }
+                                  val doRecover: PartialFunction[Throwable, F[Unit]] = {
+                                    // TODO: here we may also ignore individual tests
+                                    case t: Throwable =>
+                                      reporter.testStatus(test.id, TestStatus.Failed(t))
+                                      effect.pure(())
+                                  }
+
                                   for {
-                                    _ <- testLocator.run(pm.test)
+                                    _ <- effect.maybeSuspend(reporter.testStatus(test.id, TestStatus.Running))
+                                    _ <- effect.definitelyRecover(doRun, doRecover)
                                   } yield {
 
                                   }
+
                               }
                           }
 
@@ -186,6 +219,28 @@ class DistageTestRunner[F[_]](implicit val tagK: TagK[F]) {
 
 object DistageTestRunner {
 
-  case class DistageTest[F[_]](test: ProviderMagnet[F[_]], environment: TestEnvironment)
+  case class TestId(string: String)
+
+  case class DistageTest[F[_]](id: TestId, test: ProviderMagnet[F[_]], environment: TestEnvironment)
+
+  sealed trait TestStatus
+
+  object TestStatus {
+
+    case object Scheduled extends TestStatus
+
+    case object Running extends TestStatus
+
+    case object Succeed extends TestStatus
+
+    case class Failed(t: Throwable) extends TestStatus
+
+    case object Ignored extends TestStatus
+
+  }
+
+  trait TestReporter {
+    def testStatus(test: DistageTestRunner.TestId, testStatus: TestStatus)
+  }
 
 }
--- distage/distage-testkit/src/test/scala/com/github/pshirshov/izumi/distage/testkit/DistageTestRunnerTest.scala
+++ distage/distage-testkit/src/test/scala/com/github/pshirshov/izumi/distage/testkit/DistageTestRunnerTest.scala
@@ -1,5 +1,163 @@
 package com.github.pshirshov.izumi.distage.testkit
 
-class DistageTestRunnerTest {
+import cats.effect.IO
+import com.github.pshirshov.izumi.distage.model.definition.Axis.AxisValue
+import com.github.pshirshov.izumi.distage.model.definition.StandardAxis.Env
+import com.github.pshirshov.izumi.distage.model.definition.{AxisBase, BootstrapModuleDef}
+import com.github.pshirshov.izumi.distage.model.planning.PlanMergingPolicy
+import com.github.pshirshov.izumi.distage.plugins.load.PluginLoader.PluginConfig
+import com.github.pshirshov.izumi.distage.plugins.merge.{PluginMergeStrategy, SimplePluginMergeStrategy}
+import com.github.pshirshov.izumi.distage.roles.BootstrapConfig
+import com.github.pshirshov.izumi.distage.roles.model.AppActivation
+import com.github.pshirshov.izumi.distage.roles.model.meta.RolesInfo
+import com.github.pshirshov.izumi.distage.roles.services.{ActivationParser, PluginSource, PluginSourceImpl, PruningPlanMergingPolicy}
+import com.github.pshirshov.izumi.distage.testkit.DistagePluginTestSupport.{CacheKey, CacheValue}
+import com.github.pshirshov.izumi.distage.testkit.DistageTestRunner._
+import com.github.pshirshov.izumi.distage.testkit.fixtures.TestService1
+import com.github.pshirshov.izumi.fundamentals.platform.language.Quirks
+import com.github.pshirshov.izumi.logstage.api.{IzLogger, Log}
+import org.scalatest.WordSpec
 
+import scala.collection.immutable
+
+
+
+class DistageTestRunnerTest extends WordSpec {
+  "distage test runner" should {
+    "support shared component flow" in {
+      // each test suite needs to do some initialization job before it can return a test descriptor
+      val logger = IzLogger.apply(Log.Level.Debug)("phase" -> "test")
+      val env = loadEnvironment(logger)
+
+      // these are all the discovered tests. Not that ProviderMagnet has implicit macro materializer turning
+      // a lambda into a whitebox entity which can be introspected at runtime
+      val discoveredTests: Seq[DistageTest[IO]] = List(
+        DistageTest(TestId("Test1"), {
+          service: TestService1 =>
+            for {
+              _ <- IO.delay(assert(service != null))
+              _ <- IO.delay(println("test1"))
+            } yield {
+
+            }
+
+        }, env),
+        DistageTest(TestId("Test2"), {
+          service: TestService1 =>
+            for {
+              _ <- IO.delay(assert(service != null))
+              _ <- IO.delay(println("test2"))
+            } yield {
+
+            }
+
+        }, env),
+        DistageTest(TestId("Test3"), {
+          service: TestService1 =>
+            ???
+        }, env),
+      )
+
+
+      // a callback for our test framework
+      val reporter = new TestReporter {
+        override def testStatus(test: TestId, testStatus: TestStatus): Unit = {
+          println(s"Test ${test.string} is $testStatus")
+        }
+      }
+
+
+      // control gets back to custom logic
+
+      val runner = new DistageTestRunner[IO](reporter)
+
+      // we add all the collected tests into our custom runner
+      discoveredTests.foreach {
+        t =>
+          runner.register(t)
+      }
+
+      // and run custom logic (continue with runner.run()...)
+      runner.run()
+
+    }
+  }
+
+
+
+  /**
+    * Merge strategy will be applied only once for all the tests with the same bootstrap config when memoization is on
+    */
+  final def loadEnvironment(logger: IzLogger): TestEnvironment = {
+    val config = bootstrapConfig
+
+    def env(): CacheValue = {
+      val plugins = makePluginLoader(config).load()
+      val mergeStrategy = makeMergeStrategy(logger)
+      val defApp = mergeStrategy.merge(plugins.app)
+      val bootstrap = mergeStrategy.merge(plugins.bootstrap)
+      val availableActivations = ActivationParser.findAvailableChoices(logger, defApp)
+      CacheValue(plugins, bootstrap, defApp, availableActivations)
+    }
+
+    val plugins = if (memoizePlugins) {
+      DistagePluginTestSupport.Cache.getOrCompute(CacheKey(config), env())
+    } else {
+      env()
+    }
+
+    doLoad(logger, plugins)
+  }
+
+  protected final def doLoad(logger: IzLogger, env: CacheValue): TestEnvironment = {
+    val roles = loadRoles(logger)
+    val appActivation = AppActivation(env.availableActivations, activation)
+    val defBs = env.bsModule overridenBy new BootstrapModuleDef {
+      make[PlanMergingPolicy].from[PruningPlanMergingPolicy]
+      make[AppActivation].from(appActivation)
+    }
+    TestEnvironment(
+      defBs,
+      env.appModule,
+      roles,
+      appActivation,
+    )
+  }
+
+  protected def memoizePlugins: Boolean = {
+    import com.github.pshirshov.izumi.fundamentals.platform.strings.IzString._
+
+    System.getProperty("izumi.distage.testkit.plugins.memoize")
+      .asBoolean(true)
+  }
+
+  protected def loadRoles(logger: IzLogger): RolesInfo = {
+    Quirks.discard(logger)
+    // For all normal scenarios we don't need roles to setup a test
+    RolesInfo(Set.empty, Seq.empty, Seq.empty, Seq.empty, Set.empty)
+  }
+
+  protected def activation: Map[AxisBase, AxisValue] = Map(Env -> Env.Test)
+
+  protected def makeMergeStrategy(lateLogger: IzLogger): PluginMergeStrategy = {
+    Quirks.discard(lateLogger)
+    SimplePluginMergeStrategy
+  }
+
+  protected def bootstrapConfig: BootstrapConfig = {
+    BootstrapConfig(
+      PluginConfig(debug = false, pluginPackages, Seq.empty),
+      pluginBootstrapPackages.map(p => PluginConfig(debug = false, p, Seq.empty)),
+    )
+  }
+
+  protected def makePluginLoader(bootstrapConfig: BootstrapConfig): PluginSource = {
+    new PluginSourceImpl(bootstrapConfig)
+  }
+
+  protected final def thisPackage: Seq[String] = Seq(this.getClass.getPackage.getName)
+
+  protected def pluginPackages: Seq[String] = thisPackage
+
+  protected def pluginBootstrapPackages: Option[Seq[String]] = None
 }
