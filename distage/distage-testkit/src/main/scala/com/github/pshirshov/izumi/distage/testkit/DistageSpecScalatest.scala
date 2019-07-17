7cb0723e59b1194038e69572928b0d718ee7ae8a
Pavel Shirshov <pshirshov@gmail.com>
2019-07-17 18:28:58 +0100

    wip

--- distage/distage-testkit/src/main/scala/com/github/pshirshov/izumi/distage/testkit/DistageSpecScalatest.scala
+++ distage/distage-testkit/src/main/scala/com/github/pshirshov/izumi/distage/testkit/DistageSpecScalatest.scala
@@ -3,8 +3,6 @@ package com.github.pshirshov.izumi.distage.testkit
 import distage.TagK
 import org.scalatest.DistageScalatestTestSuite
 
-abstract class DistageSpec[F[_]]()(implicit val tagMonoIO: TagK[F]) extends DistageTestSuiteSyntax[F]
-
 abstract class DistageSpecScalatest[F[_]]()(implicit val tagMonoIO: TagK[F]) extends DistageTestSuiteSyntax[F] with DistageScalatestTestSuite[F]
 
 
