a9f8fc672c7a70b5bb5a50e1572952ba6bf0b414
Pavel Shirshov <pshirshov@gmail.com>
2019-07-22 01:36:51 +0100

    working sjs logging

---
+++ logstage/logstage-core/.js/src/test/scala/com/github/pshirshov/izumi/logstage/sink/LoggingAsyncSinkTest.scala
@@ -0,0 +1,22 @@
+package com.github.pshirshov.izumi.logstage.sink
+
+import com.github.pshirshov.izumi.logstage.api.{IzLogger, TestSink}
+import org.scalatest.WordSpec
+
+class LoggingAsyncSinkTest extends WordSpec {
+  "Log macro" should {
+    "support async sink api in sync mode" in {
+      val testSink = new TestSink()
+      val asyncConsoleSinkJson = new QueueingSink(testSink)
+      try {
+        new ExampleService(IzLogger(IzLogger.Level.Trace, asyncConsoleSinkJson)).triggerManyMessages()
+        assert(testSink.fetch().size == 100)
+        asyncConsoleSinkJson.start()
+      } finally {
+        asyncConsoleSinkJson.close()
+      }
+
+      assert(testSink.fetch().size == 100)
+    }
+  }
+}
