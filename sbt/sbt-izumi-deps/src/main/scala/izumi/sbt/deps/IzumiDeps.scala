d36fb221ae64fb3694f0ffdaeaa1e46424b15c05
Scala Steward <me@scala-steward.org>
2019-08-22 13:25:32 +0200

    Update classgraph to 4.8.46

--- sbt/sbt-izumi-deps/src/main/scala/izumi/sbt/deps/IzumiDeps.scala
+++ sbt/sbt-izumi-deps/src/main/scala/izumi/sbt/deps/IzumiDeps.scala
@@ -36,7 +36,7 @@ object IzumiDeps {
     // java, we need it bcs http4s ws client isn't ready yet
     val asynchttpclient = "2.10.1"
 
-    val classgraph = "4.8.45"
+    val classgraph = "4.8.46"
     val slf4j = "1.7.28"
     val typesafe_config = "1.3.4"
 
