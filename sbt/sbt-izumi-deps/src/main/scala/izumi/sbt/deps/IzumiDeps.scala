f59604a24c4f097230f48395cbea5f5d92274abf
Scala Steward <me@scala-steward.org>
2019-08-27 03:32:39 +0200

    Update cats-core to 2.0.0-RC2

--- sbt/sbt-izumi-deps/src/main/scala/izumi/sbt/deps/IzumiDeps.scala
+++ sbt/sbt-izumi-deps/src/main/scala/izumi/sbt/deps/IzumiDeps.scala
@@ -16,8 +16,8 @@ object IzumiDeps {
 
     val shapeless = "2.3.3"
 
-    val cats = "2.0.0-RC1"
-    val cats_effect = "2.0.0-RC1"
+    val cats = "2.0.0-RC2"
+    val cats_effect = "2.0.0-RC2"
     val zio = "1.0.0-RC11-1"
     val zio_interop_cats = "2.0.0.0-RC2"
 
