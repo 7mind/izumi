a1de0f7da1c3817e1f2acbf67ba0c52a27c99bd7
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2019-09-05 01:05:07 +0200

    Update zio to 1.0.0-RC12-1 (#640)

--- sbt/sbt-izumi-deps/src/main/scala/izumi/sbt/deps/IzumiDeps.scala
+++ sbt/sbt-izumi-deps/src/main/scala/izumi/sbt/deps/IzumiDeps.scala
@@ -18,7 +18,7 @@ object IzumiDeps {
 
     val cats = "2.0.0-RC1"
     val cats_effect = "2.0.0-RC1"
-    val zio = "1.0.0-RC11-1"
+    val zio = "1.0.0-RC12-1"
     val zio_interop_cats = "2.0.0.0-RC2"
 
     val circe = "0.12.0-RC4"
