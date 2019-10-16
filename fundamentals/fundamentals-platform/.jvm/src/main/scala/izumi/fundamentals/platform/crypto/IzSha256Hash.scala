42ad2e2f65cb6b8ab46e1b95f5b97d8b733a35f6
Pavel Shirshov <pshirshov@gmail.com>
2019-10-16 19:08:53 +0100

    sjs fuckery

--- fundamentals/fundamentals-platform/.jvm/src/main/scala/izumi/fundamentals/platform/crypto/IzSha256Hash.scala
+++ fundamentals/fundamentals-platform/.jvm/src/main/scala/izumi/fundamentals/platform/crypto/IzSha256Hash.scala
@@ -3,6 +3,10 @@ package izumi.fundamentals.platform.crypto
 import java.security.MessageDigest
 
 object IzSha256Hash extends IzHash {
+  // scalajs workarounds
+  def setImported(): Unit = {}
+  def getImpl: IzHash = this
+
   override def hash(bytes: Array[Byte]): Array[Byte] = {
     MessageDigest.getInstance("SHA-256").digest(bytes)
   }
--- fundamentals/fundamentals-platform/src/test/scala/izumi/fundamentals/platform/IzHashTest.scala
+++ fundamentals/fundamentals-platform/src/test/scala/izumi/fundamentals/platform/IzHashTest.scala
@@ -15,7 +15,10 @@ class IzHashTest extends WordSpec {
   "sha256 hash" should {
     "produce hex output" in {
       import izumi.fundamentals.platform.crypto.IzSha256Hash
-      assert(IzSha256Hash.hash("abc") == "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad")
+      IzSha256Hash.setImported()
+      for (_ <- 0 to 2) {
+        assert(IzSha256Hash.getImpl.hash("abc") == "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad")
+      }
     }
   }
 }
