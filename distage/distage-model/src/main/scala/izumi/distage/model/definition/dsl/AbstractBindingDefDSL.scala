4b486bf525a2efd810255cb5133466ccf2ec26eb
Pavel Shirshov <pshirshov@gmail.com>
2019-10-20 17:54:27 +0100

    cleanups

--- distage/distage-model/src/main/scala/izumi/distage/model/definition/dsl/AbstractBindingDefDSL.scala
+++ distage/distage-model/src/main/scala/izumi/distage/model/definition/dsl/AbstractBindingDefDSL.scala
@@ -173,24 +173,9 @@ object AbstractBindingDefDSL {
     }
   }
 
-//  private def key(basic: DIKey.TypeKey, dis: String, d: ImplDef): DIKey.BasicKey = {
-//    d match {
-//      case implDef: ImplDef.DirectImplDef =>
-//        implDef match {
-//          case _: ImplDef.TypeImpl=>
-//            basic
-//          case _ =>
-//            basic.named(dis)
-//        }
-//      case _: ImplDef.RecursiveImplDef =>
-//        basic.named(dis)
-//    }
-//  }
-
   final class SetElementRef(implDef: ImplDef, pos: SourceFilePosition, ops: mutable.Queue[SetElementInstruction] = mutable.Queue.empty) {
     def interpret(setKey: DIKey.BasicKey): SetElementBinding = {
       val implKey = DIKey.TypeKey(implDef.implType)
-      //val refkey = key(implKey, pos.toString, implDef)
       val refkey = implKey.named(DIKey.SetLocId(pos.toString))
       val elKey = DIKey.SetElementKey(setKey, refkey)
 
@@ -210,30 +195,9 @@ object AbstractBindingDefDSL {
     }
   }
 
-//  final class SetElementRef(implDef: ImplDef, pos: SourceFilePosition, ops: mutable.Queue[SetElementInstruction] = mutable.Queue.empty) {
-//    def interpret(setKey: DIKey.BasicKey): SetElementBinding = {
-//      val implKey = DIKey.TypeKey(implDef.implType).named(pos.toString)
-//      val elKey = DIKey.SetElementKey(setKey, implKey)
-//      ops.foldLeft(SetElementBinding(elKey, implDef, Set.empty, pos)) {
-//        (b, instr) =>
-//          instr match {
-//            case ElementAddTags(tags) => b.addTags(tags)
-//          }
-//      }
-//    }
-//
-//
-//
-//    def append(op: SetElementInstruction): SetElementRef = {
-//      ops += op
-//      this
-//    }
-//  }
-
   final class MultiSetElementRef(implDef: ImplDef, pos: SourceFilePosition, ops: mutable.Queue[MultiSetElementInstruction] = mutable.Queue.empty) {
     def interpret(setKey: DIKey.BasicKey): Seq[Binding] = {
       val ukey = DIKey.IdKey(implDef.implType, DIKey.SetImplId(implDef))
-      //val ukey = DIKey.TypeKey(implDef.implType)
 
       val bind = SingletonBinding(ukey, implDef, Set.empty, pos)
 
