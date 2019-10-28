7b1cf4f955640c1d10b24854512c6e2d865fdab3
Pavel Shirshov <pshirshov@gmail.com>
2019-10-28 17:23:34 +0000

    nowLocal, nowOffset

--- fundamentals/fundamentals-platform/src/main/scala/izumi/fundamentals/platform/time/Clock.scala
+++ fundamentals/fundamentals-platform/src/main/scala/izumi/fundamentals/platform/time/Clock.scala
@@ -1,6 +1,6 @@
 package izumi.fundamentals.platform.time
 
-import java.time.ZonedDateTime
+import java.time.{LocalDateTime, OffsetDateTime, ZoneId, ZonedDateTime}
 
 import izumi.fundamentals.platform.functional.Identity
 
@@ -12,18 +12,31 @@ trait Clock[+F[_]] {
   /** Should return current time (UTC timezone)
     */
   def now(accuracy: ClockAccuracy = ClockAccuracy.DEFAULT): F[ZonedDateTime]
+
+  def nowLocal(accuracy: ClockAccuracy = ClockAccuracy.DEFAULT): F[LocalDateTime]
+  def nowOffset(accuracy: ClockAccuracy = ClockAccuracy.DEFAULT): F[OffsetDateTime]
 }
 
 object Clock {
+  final val TZ_UTC: ZoneId = ZoneId.of("UTC")
+
   def apply[F[_]: Clock]: Clock[F] = implicitly
 
   class Standard extends Clock[Identity] {
+    private def now(): ZonedDateTime = ZonedDateTime.now(TZ_UTC)
 
     override def epoch: Long = java.time.Clock.systemUTC().millis()
 
     override def now(accuracy: ClockAccuracy): Identity[ZonedDateTime] = {
-      val current = IzTime.utcNow
-      ClockAccuracy.applyAccuracy(current, accuracy)
+      ClockAccuracy.applyAccuracy(now(), accuracy)
+    }
+
+    override def nowLocal(accuracy: ClockAccuracy): Identity[LocalDateTime] = {
+      now(accuracy).toLocalDateTime
+    }
+
+    override def nowOffset(accuracy: ClockAccuracy): Identity[OffsetDateTime] = {
+      now(accuracy).toOffsetDateTime
     }
   }
 
@@ -33,6 +46,14 @@ object Clock {
 
     override def now(accuracy: ClockAccuracy): Identity[ZonedDateTime] = {
       ClockAccuracy.applyAccuracy(time, accuracy)
+    }
+
+    override def nowLocal(accuracy: ClockAccuracy): Identity[LocalDateTime] = {
+      now(accuracy).toLocalDateTime
+    }
+
+    override def nowOffset(accuracy: ClockAccuracy): Identity[OffsetDateTime] = {
+      now(accuracy).toOffsetDateTime
     }
   }
 
