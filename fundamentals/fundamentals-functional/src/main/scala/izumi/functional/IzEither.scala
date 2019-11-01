0fd9582b4d8699c55a1a9c6f4f403aef6f9d13ed
Kai <kai@7mind.io>
2019-11-01 19:08:53 +0000

    preserve collection type in IzEither

--- fundamentals/fundamentals-functional/src/main/scala/izumi/functional/IzEither.scala
+++ fundamentals/fundamentals-functional/src/main/scala/izumi/functional/IzEither.scala
@@ -1,51 +1,55 @@
 package izumi.functional
 
-// TODO: canbuildfrom/compat
+import scala.collection.compat._
 
 trait IzEither {
-  implicit class EitherFolderExt1[L, R](result: Seq[Either[List[L], R]]) {
-    def biAggregate: Either[List[L], List[R]] = {
+  implicit final class EitherFolderExt1[L, R, Col[x] <: Iterable[x]](result: Col[Either[List[L], R]]) {
+    def biAggregate(implicit b: Factory[R, Col[R]]): Either[List[L], Col[R]] = {
       val bad = result.collect {
         case Left(e) => e
       }
       if (bad.isEmpty) {
-        Right(result.collect {
-          case Right(r) => r
-        }.toList)
+        Right(b.fromSpecific {
+          result.collect {
+            case Right(r) => r
+          }
+        })
       } else {
         Left(bad.flatten.toList)
       }
     }
   }
 
-  implicit class EitherFolderExt2[L, R](result: Seq[Either[List[L], Iterable[R]]]) {
-    def biFlatAggregate: Either[List[L], List[R]] = {
+  implicit final class EitherFolderExt2[L, R, Col[x] <: Iterable[x]](result: Col[Either[List[L], Iterable[R]]]) {
+    def biFlatAggregate(implicit b: Factory[R, Col[R]]): Either[List[L], Col[R]] = {
       val bad = result.collect {
         case Left(e) => e
       }
       if (bad.isEmpty) {
-        Right(result.collect {
-          case Right(r) => r
-        }.flatten.toList)
+        Right(b.fromSpecific {
+          result.collect {
+            case Right(r) => r
+          }.flatten
+        })
       } else {
         Left(bad.flatten.toList)
       }
     }
   }
 
-  implicit class BiSplit[L, R](e: Seq[Either[L, R]]) {
-    def lrPartition: (Seq[L], Seq[R]) = {
+  implicit final class BiSplit[L, R, Col[x] <: Iterable[x]](e: Col[Either[L, R]]) {
+    def lrPartition(implicit bl: Factory[L, Col[L]],  br: Factory[R, Col[R]]): (Col[L], Col[R]) = {
       val left = e.collect {
         case Left(l) => l
       }
       val right = e.collect {
         case Right(r) => r
       }
-      (left, right)
+      (bl.fromSpecific(left), br.fromSpecific(right))
     }
   }
 
-  implicit class BiFind[T](s: Seq[T]) {
+  implicit final class BiFind[T](s: Iterable[T]) {
     def biFind[E](predicate: T => Either[List[E], Boolean]): Either[List[E], Option[T]] = {
       val i = s.iterator
       while (i.hasNext) {
