6ce410e7606bb32ce93fe5ff7776c43c131a7aee
Kai <kai@7mind.io>
2019-11-07 18:36:11 +0000

    remove BIOFlattenOps conversion

--- fundamentals/fundamentals-bio/src/main/scala/izumi/functional/bio/BIOImplicitPuns.scala
+++ fundamentals/fundamentals-bio/src/main/scala/izumi/functional/bio/BIOImplicitPuns.scala
@@ -17,7 +17,6 @@ trait BIOImplicitPuns extends BIOImplicitPuns1 {
   @inline implicit final def BIOAsync[F[+_, +_]: BIOPanic, E, A](self: F[E, A]): BIOSyntax.BIOPanicOps[F, E, A] = new BIOSyntax.BIOPanicOps[F, E, A](self)
   @inline implicit final def BIOAsync[F[+_, +_]: BIO, E, A](self: F[E, A]): BIOSyntax.BIOOps[F, E, A] = new BIOSyntax.BIOOps[F, E, A](self)
   @inline implicit final def BIOAsync[F[+_, +_]: BIOAsync, E, A](self: F[E, A]): BIOSyntax.BIOAsyncOps[F, E, A] = new BIOSyntax.BIOAsyncOps[F, E, A](self)
-  @inline implicit final def BIOAsync[F[+_, +_]: BIOMonad, E, A](self: F[E, F[E, A]])(implicit dummy: DummyImplicit): BIOSyntax.BIOFlattenOps[F, E, A] = new BIOSyntax.BIOFlattenOps[F, E, A](self)
 
   @inline final def BIOAsync[F[+_, +_]: BIOAsync]: BIOAsync[F] = implicitly
 
@@ -56,7 +55,6 @@ object BIOImplicitPuns {
     @inline implicit final def BIO[F[+_, +_]: BIOBracket, E, A](self: F[E, A]): BIOSyntax.BIOBracketOps[F, E, A] = new BIOSyntax.BIOBracketOps[F, E, A](self)
     @inline implicit final def BIO[F[+_, +_]: BIOPanic, E, A](self: F[E, A]): BIOSyntax.BIOPanicOps[F, E, A] = new BIOSyntax.BIOPanicOps[F, E, A](self)
     @inline implicit final def BIO[F[+_, +_]: BIO, E, A](self: F[E, A]): BIOSyntax.BIOOps[F, E, A] = new BIOSyntax.BIOOps[F, E, A](self)
-    @inline implicit final def BIO[F[+_, +_]: BIOMonad, E, A](self: F[E, F[E, A]])(implicit dummy: DummyImplicit): BIOSyntax.BIOFlattenOps[F, E, A] = new BIOSyntax.BIOFlattenOps[F, E, A](self)
   }
   trait BIOImplicitPuns2 extends BIOImplicitPuns3 {
     @inline implicit final def BIOPanic[F[_, +_]: BIOFunctor, E, A](self: F[E, A]): BIOSyntax.BIOFunctorOps[F, E, A] = new BIOSyntax.BIOFunctorOps[F, E, A](self)
@@ -68,7 +66,6 @@ object BIOImplicitPuns {
     @inline implicit final def BIOPanic[F[+_, +_]: BIOMonadError, E, A](self: F[E, A]): BIOSyntax.BIOMonadErrorOps[F, E, A] = new BIOSyntax.BIOMonadErrorOps[F, E, A](self)
     @inline implicit final def BIOPanic[F[+_, +_]: BIOBracket, E, A](self: F[E, A]): BIOSyntax.BIOBracketOps[F, E, A] = new BIOSyntax.BIOBracketOps[F, E, A](self)
     @inline implicit final def BIOPanic[F[+_, +_]: BIOPanic, E, A](self: F[E, A]): BIOSyntax.BIOPanicOps[F, E, A] = new BIOSyntax.BIOPanicOps[F, E, A](self)
-    @inline implicit final def BIOPanic[F[+_, +_]: BIOMonad, E, A](self: F[E, F[E, A]])(implicit dummy: DummyImplicit): BIOSyntax.BIOFlattenOps[F, E, A] = new BIOSyntax.BIOFlattenOps[F, E, A](self)
 
     @inline final def BIOPanic[F[+_, +_]: BIOPanic]: BIOPanic[F] = implicitly
   }
@@ -81,7 +78,6 @@ object BIOImplicitPuns {
     @inline implicit final def BIOBracket[F[+_, +_]: BIOError, E, A](self: F[E, A]): BIOSyntax.BIOErrorOps[F, E, A] = new BIOSyntax.BIOErrorOps[F, E, A](self)
     @inline implicit final def BIOBracket[F[+_, +_]: BIOMonadError, E, A](self: F[E, A]): BIOSyntax.BIOMonadErrorOps[F, E, A] = new BIOSyntax.BIOMonadErrorOps[F, E, A](self)
     @inline implicit final def BIOBracket[F[+_, +_]: BIOBracket, E, A](self: F[E, A]): BIOSyntax.BIOBracketOps[F, E, A] = new BIOSyntax.BIOBracketOps[F, E, A](self)
-    @inline implicit final def BIOBracket[F[+_, +_]: BIOMonad, E, A](self: F[E, F[E, A]])(implicit dummy: DummyImplicit): BIOSyntax.BIOFlattenOps[F, E, A] = new BIOSyntax.BIOFlattenOps[F, E, A](self)
 
     @inline final def BIOBracket[F[+_, +_]: BIOBracket]: BIOBracket[F] = implicitly
   }
@@ -93,7 +89,6 @@ object BIOImplicitPuns {
     @inline implicit final def BIOMonadError[F[+_, +_]: BIOGuarantee, E, A](self: F[E, A]): BIOSyntax.BIOGuaranteeOps[F, E, A] = new BIOSyntax.BIOGuaranteeOps[F, E, A](self)
     @inline implicit final def BIOMonadError[F[+_, +_]: BIOError, E, A](self: F[E, A]): BIOSyntax.BIOErrorOps[F, E, A] = new BIOSyntax.BIOErrorOps[F, E, A](self)
     @inline implicit final def BIOMonadError[F[+_, +_]: BIOMonadError, E, A](self: F[E, A]): BIOSyntax.BIOMonadErrorOps[F, E, A] = new BIOSyntax.BIOMonadErrorOps[F, E, A](self)
-    @inline implicit final def BIOMonadError[F[+_, +_]: BIOMonad, E, A](self: F[E, F[E, A]])(implicit dummy: DummyImplicit): BIOSyntax.BIOFlattenOps[F, E, A] = new BIOSyntax.BIOFlattenOps[F, E, A](self)
 
     @inline final def BIOMonadError[F[+_, +_]: BIOMonadError]: BIOMonadError[F] = implicitly
   }
@@ -120,7 +115,6 @@ object BIOImplicitPuns {
     @inline implicit final def BIOMonad[F[+_, +_]: BIOBifunctor, E, A](self: F[E, A]): BIOSyntax.BIOBifunctorOps[F, E, A] = new BIOSyntax.BIOBifunctorOps[F, E, A](self)
     @inline implicit final def BIOMonad[F[+_, +_]: BIOApplicative, E, A](self: F[E, A]): BIOSyntax.BIOApplicativeOps[F, E, A] = new BIOSyntax.BIOApplicativeOps[F, E, A](self)
     @inline implicit final def BIOMonad[F[+_, +_]: BIOMonad, E, A](self: F[E, A]): BIOSyntax.BIOMonadOps[F, E, A] = new BIOSyntax.BIOMonadOps[F, E, A](self)
-    @inline implicit final def BIOMonad[F[+_, +_]: BIOMonad, E, A](self: F[E, F[E, A]])(implicit dummy: DummyImplicit): BIOSyntax.BIOFlattenOps[F, E, A] = new BIOSyntax.BIOFlattenOps[F, E, A](self)
 
     @inline final def BIOMonad[F[+_, +_]: BIOMonad]: BIOMonad[F] = implicitly
   }
