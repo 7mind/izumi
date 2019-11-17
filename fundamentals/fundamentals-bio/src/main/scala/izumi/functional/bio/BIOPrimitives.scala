c76c52a2c70aee1612461b1b8876ba6fb6bb3328
Kai <kai@7mind.io>
2019-11-17 01:28:26 +0000

    BIOPrimitives.mkLatch now only creates BIOPromise[Unit]

--- fundamentals/fundamentals-bio/src/main/scala/izumi/functional/bio/BIOPrimitives.scala
+++ fundamentals/fundamentals-bio/src/main/scala/izumi/functional/bio/BIOPrimitives.scala
@@ -4,7 +4,7 @@ import zio.{IO, Promise, Ref, Semaphore}
 
 trait BIOPrimitives[F[+_, +_]] extends BIOPrimitivesInstances {
   def mkRef[A](a: A): F[Nothing, BIORef[F, A]]
-  def mkLatch[A]: F[Nothing, BIOPromise[F, Nothing, A]]
+  def mkLatch: F[Nothing, BIOPromise[F, Nothing, Unit]]
   def mkPromise[E, A]: F[Nothing, BIOPromise[F, E, A]]
   def mkSemaphore(permits: Long): F[Nothing, BIOSemaphore[F]]
 }
@@ -14,7 +14,7 @@ object BIOPrimitivesInstances {
   implicit val zioPrimitives: BIOPrimitives[IO] = {
     new BIOPrimitives[IO] {
       override def mkRef[A](a: A): IO[Nothing, BIORef[IO, A]]                = Ref.make(a).map(BIORef.fromZIO)
-      override def mkLatch[A]: IO[Nothing, BIOPromise[IO, Nothing, A]]       = mkPromise[Nothing, A]
+      override def mkLatch: IO[Nothing, BIOPromise[IO, Nothing, Unit]]       = mkPromise[Nothing, Unit]
       override def mkPromise[E, A]: IO[Nothing, BIOPromise[IO, E, A]]        = Promise.make[E, A].map(BIOPromise.fromZIO)
       override def mkSemaphore(permits: Long): IO[Nothing, BIOSemaphore[IO]] = Semaphore.make(permits).map(BIOSemaphore.fromZIO)
     }
