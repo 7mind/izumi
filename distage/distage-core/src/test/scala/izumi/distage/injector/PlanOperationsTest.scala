560598be99f47e9b664f949b580d37ef26778ab5
Pavel Shirshov <pshirshov@gmail.com>
2019-11-25 11:28:31 +0000

    Better testcase

--- distage/distage-core/src/test/scala/izumi/distage/injector/PlanOperationsTest.scala
+++ distage/distage-core/src/test/scala/izumi/distage/injector/PlanOperationsTest.scala
@@ -9,7 +9,7 @@ class PlanOperationsTest extends WordSpec with MkInjector {
 
   import PlanOperationsTest._
 
-  "maintain correct operation order" in {
+  "support plan trisplit" in {
     val primary = Set[DIKey](DIKey.get[PrimaryComponent])
     val sub = Set[DIKey](DIKey.get[IntegrationComponent])
 
@@ -18,20 +18,27 @@ class PlanOperationsTest extends WordSpec with MkInjector {
       make[IntegrationComponent]
       make[SharedComponent0]
       make[SharedComponent1]
+      make[SharedComponent2]
     }, primary ++ sub)
 
     val injector = mkInjector()
 
     val split = injector.triSplitPlan(definition.bindings, primary)(_ => sub)
 
-    assert(Set[DIKey](DIKey.get[SharedComponent0], DIKey.get[SharedComponent1]).diff(split.shared.plan.index.keySet).isEmpty)
+    assert(Set[DIKey](DIKey.get[SharedComponent0], DIKey.get[SharedComponent2]).diff(split.shared.plan.index.keySet).isEmpty)
     assert((primary ++ sub).intersect(split.shared.plan.index.keySet).isEmpty)
     assert(primary.intersect(split.side.plan.index.keySet).isEmpty)
     assert(sub.intersect(split.primary.plan.index.keySet).isEmpty)
 
-    assert(split.primary.plan.index.keySet.intersect(split.side.plan.index.keySet) == Set[DIKey](DIKey.get[SharedComponent1]))
-    assert(split.primary.plan.index.keySet.intersect(split.shared.plan.index.keySet) == Set[DIKey](DIKey.get[SharedComponent1]))
-    assert(split.side.plan.index.keySet.intersect(split.shared.plan.index.keySet) == Set[DIKey](DIKey.get[SharedComponent1]))
+//    println(split.shared.plan.render())
+//    println("///")
+//    println(split.side.plan.render())
+//    println("///")
+//    println(split.primary.plan.render())
+
+    assert(split.primary.plan.index.keySet.intersect(split.side.plan.index.keySet) == Set[DIKey](DIKey.get[SharedComponent2]))
+    assert(split.primary.plan.index.keySet.intersect(split.shared.plan.index.keySet) == Set[DIKey](DIKey.get[SharedComponent2]))
+    assert(split.side.plan.index.keySet.intersect(split.shared.plan.index.keySet) == Set[DIKey](DIKey.get[SharedComponent2]))
   }
 
 }
@@ -42,8 +49,10 @@ object PlanOperationsTest {
 
   case class SharedComponent1(com0: SharedComponent0)
 
-  case class IntegrationComponent(sharedComponent1: SharedComponent1)
+  case class SharedComponent2(com1: SharedComponent1)
 
-  case class PrimaryComponent(sharedComponent1: SharedComponent1)
+  case class IntegrationComponent(sharedComponent1: SharedComponent2)
+
+  case class PrimaryComponent(sharedComponent1: SharedComponent2)
 
 }
