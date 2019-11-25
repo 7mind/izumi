9a8c3596e2f237b4f46f3d3fd399cb8d3eb0fc1b
Pavel Shirshov <pshirshov@gmail.com>
2019-11-25 00:03:02 +0000

    better test reporting

--- distage/distage-testkit/src/main/scala/org/scalatest/SafeTestReporter.scala
+++ distage/distage-testkit/src/main/scala/org/scalatest/SafeTestReporter.scala
@@ -9,6 +9,7 @@ import scala.collection.mutable
 class SafeTestReporter(underlying: TestReporter) extends TestReporter {
   private val allOpen = mutable.LinkedHashMap.empty[TestMeta, TestStatus]
   private var signalled: Option[TestMeta] = None
+  private var openSuite: Option[SuiteData] = None
 
 
   override def onFailure(f: Throwable): Unit = synchronized {
@@ -18,15 +19,15 @@ class SafeTestReporter(underlying: TestReporter) extends TestReporter {
 
   override def endAll(): Unit = synchronized {
     finish()
-    assert(allOpen.isEmpty)
+    finishOpen()
     underlying.endAll()
   }
 
-  override def beginSuite(id: SuiteData): Unit = {
+  override def beginSuite(id: SuiteData): Unit = synchronized {
     Quirks.discard(id)
   }
 
-  override def endSuite(id: SuiteData): Unit = {
+  override def endSuite(id: SuiteData): Unit = synchronized {
     Quirks.discard(id)
   }
 
@@ -34,22 +35,22 @@ class SafeTestReporter(underlying: TestReporter) extends TestReporter {
   override def testStatus(test: TestMeta, testStatus: TestStatus): Unit = synchronized {
     testStatus match {
       case TestStatus.Scheduled =>
-        underlying.testStatus(test, testStatus)
+        reportStatus(test, testStatus)
 
       case TestStatus.Running =>
         allOpen.put(test, testStatus)
         signalled match {
           case Some(_) =>
           case None =>
-            underlying.testStatus(test, testStatus)
+            reportStatus(test, testStatus)
             signalled = Some(test)
         }
 
 
       case _: Ignored =>
         if (allOpen.isEmpty) {
-          underlying.testStatus(test, TestStatus.Running)
-          underlying.testStatus(test, testStatus)
+          reportStatus(test, TestStatus.Running)
+          reportStatus(test, testStatus)
         } else {
           allOpen.put(test, testStatus)
         }
@@ -65,19 +66,44 @@ class SafeTestReporter(underlying: TestReporter) extends TestReporter {
     }
   }
 
+  private def reportStatus(test: TestMeta, testStatus: TestStatus): Unit = {
+    val fakeSuite = SuiteData(test.id.suiteName, test.id.suiteId, test.id.suiteClassName)
+
+    nextSuite(fakeSuite)
+    underlying.testStatus(test, testStatus)
+  }
+
+  private val fakeSuites = false
+  private def nextSuite(fakeSuite: SuiteData): Unit = {
+    if (fakeSuites) {
+      if (!openSuite.contains(fakeSuite)) {
+        finishOpen()
+        underlying.beginSuite(fakeSuite)
+        openSuite = Some(fakeSuite)
+      }
+    }
+  }
+
+  private def finishOpen(): Unit = {
+    if (fakeSuites) {
+      openSuite.foreach(underlying.endSuite)
+    }
+  }
+
+
   private def finish(): Unit = {
     val finishedTests = allOpen.collect({ case (t, s: Done) => (t, s) })
 
     finishedTests
       .foreach {
         case (t, s) =>
-          underlying.testStatus(t, TestStatus.Running)
+          reportStatus(t, TestStatus.Running)
           finishTest(t, s)
       }
   }
 
   private def finishTest(test: TestMeta, testStatus: TestStatus): Option[TestStatus] = {
-    underlying.testStatus(test, testStatus)
+    reportStatus(test, testStatus)
     allOpen.remove(test)
   }
 }
