83f22c076028c47d037a68e6b73cd2d99a310a41
Kai <kai@7mind.io>
2019-12-04 03:07:12 +0000

    fix build

--- fundamentals/fundamentals-bio/.js/src/main/scala/izumi/functional/bio/BlockingIO3.scala
+++ fundamentals/fundamentals-bio/.js/src/main/scala/izumi/functional/bio/BlockingIO3.scala
@@ -1,7 +1,7 @@
 package izumi.functional.bio
 
 /** Scala.js does not support blockingIO */
-private[bio] sealed trait BlockingIO3[F[_, _, _]] {
+private[bio] trait BlockingIO3[F[_, _, _]] {
   private[bio] def shiftBlocking[R, E, A](f: F[R, E ,A]): F[R, E, A]
   private[bio] def syncBlocking[A](f: => A): F[Any, Throwable, A]
   private[bio] def syncInterruptibleBlocking[A](f: => A): F[Any, Throwable, A]
