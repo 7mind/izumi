6a0b4fa696f7ce169cad786ec628df0fdc2fd044
Pavel Shirshov <pshirshov@gmail.com>
2019-12-09 21:30:19 +0000

    fix

--- distage/distage-core/src/test/scala/izumi/distage/InjectorPerf.scala
+++ distage/distage-core/src/test/scala/izumi/distage/InjectorPerf.scala
@@ -4,14 +4,16 @@ import distage.Injector
 
 object InjectorPerf {
   def main(args: Array[String]): Unit = {
+    println("[IPT] About to start...")
     for (x <- 0 to 500){
       val t1 = System.nanoTime()
       Injector()
       val t2 = System.nanoTime()
       if (x % 100 == 0) {
-        println((t2 - t1) / 1000.0 / 1000.0)
+        println(s"[IPT]  ${(t2 - t1) / 1000.0 / 1000.0}")
       }
     }
+    println("[IPT] finished!")
 
   }
 }
