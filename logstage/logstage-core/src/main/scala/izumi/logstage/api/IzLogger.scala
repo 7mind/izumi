8d2d2d1bfc44af20d19e6516bdc24af7242e1d19
Kai <450507+neko-kai@users.noreply.github.com>
2019-12-27 02:43:24 +0000

    Add LogIO.apply(CustomContext), rename IzLogger.withMapAsCustomContext->withCustomContext (#801)

--- logstage/logstage-core/src/main/scala/izumi/logstage/api/IzLogger.scala
+++ logstage/logstage-core/src/main/scala/izumi/logstage/api/IzLogger.scala
@@ -13,17 +13,13 @@ class IzLogger
 , override val customContext: Log.CustomContext
 ) extends RoutingLogger {
 
-  def apply(context: CustomContext): IzLogger = withCustomContext(context)
-  def apply(context: Map[String, Any]): IzLogger = withMapAsCustomContext(context)
-  def apply(context: (String, Any)*): IzLogger = withMapAsCustomContext(context.toMap)
+  override def withCustomContext(context: CustomContext): IzLogger = new IzLogger(router, customContext + context)
+  final def withCustomContext(context: (String, Any)*): IzLogger = withCustomContext(context.toMap)
+  final def withCustomContext(context: Map[String, Any]): IzLogger = withCustomContext(CustomContext(context))
 
-  implicit override def withCustomContext(context: CustomContext): IzLogger = {
-    new IzLogger(router, customContext + context)
-  }
-
-  implicit def withMapAsCustomContext(context: Map[String, Any]): IzLogger = {
-    withCustomContext(CustomContext(context))
-  }
+  final def apply(context: CustomContext): IzLogger = withCustomContext(context)
+  final def apply(context: (String, Any)*): IzLogger = withCustomContext(context.toMap)
+  final def apply(context: Map[String, Any]): IzLogger = withCustomContext(context)
 
 }
 
