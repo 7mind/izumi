11d72357958fcb2798c8ea82babbe8fba1a8d01c
Kai <kai@7mind.io>
2020-01-14 19:11:53 +0000

    fix build

--- distage/distage-testkit-legacy/src/main/scala/izumi/distage/testkit/services/scalatest/adapter/DISyntaxBIO.scala
+++ distage/distage-testkit-legacy/src/main/scala/izumi/distage/testkit/services/scalatest/adapter/DISyntaxBIO.scala
@@ -1,15 +1,12 @@
 package izumi.distage.testkit.services.scalatest.adapter
 
+import distage.Tag
 import izumi.distage.model.providers.ProviderMagnet
 import izumi.distage.testkit.services.DISyntaxBIOBase
-import distage.Tag
-import izumi.fundamentals.platform.language.{CodePosition, CodePositionMaterializer}
+import izumi.fundamentals.platform.language.CodePositionMaterializer
 
 @deprecated("Use dstest", "2019/Jul/18")
 trait DISyntaxBIO[F[+ _, + _]] extends DISyntaxBIOBase[F] {
-  this: DISyntax[F[Throwable, ?]] =>
-
-  override protected def takeIO(fAsThrowable: ProviderMagnet[F[Throwable, _]], pos: CodePosition): Unit = dio(fAsThrowable)
 
   final def dio(function: ProviderMagnet[F[_, _]])(implicit pos: CodePositionMaterializer, dummyImplicit: DummyImplicit): Unit = {
     takeBIO(function, pos.get)
