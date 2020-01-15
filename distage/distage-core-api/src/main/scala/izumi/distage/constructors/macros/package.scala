066166696d0a1ae3f654787593eb7d56dca40766
Kai <kai@7mind.io>
2020-01-15 21:08:18 +0000

    move common macro methods to package object

--- distage/distage-core-api/src/main/scala/izumi/distage/constructors/macros/package.scala
+++ distage/distage-core-api/src/main/scala/izumi/distage/constructors/macros/package.scala
@@ -1,18 +1,50 @@
 package izumi.distage.constructors
 
+import izumi.distage.model.reflection.universe.StaticDIUniverse
+import izumi.fundamentals.platform.console.TrivialLogger
 import izumi.fundamentals.reflection.ReflectionUtil
 
 import scala.reflect.macros.blackbox
 
 package object macros {
 
-  def requireConcreteTypeConstructor(c: blackbox.Context)(macroName: String, tpe: c.Type): Unit = {
+  private[macros] def requireConcreteTypeConstructor(c: blackbox.Context)(macroName: String, tpe: c.Type): Unit = {
     if (!ReflectionUtil.allPartsStrong(tpe.dealias.typeConstructor)) {
       c.abort(c.enclosingPosition,
         s"""$macroName: Can't generate constructor for $tpe:
            |Type constructor is an unresolved type parameter `${tpe.dealias.typeConstructor}`.
            |Did you forget to put a $macroName context bound on the ${tpe.dealias.typeConstructor}, such as [${tpe.dealias.typeConstructor}: $macroName]?
            |""".stripMargin)
+    }
+  }
+
+  private[macros] def mkArgFromAssociation(c: blackbox.Context)
+                                          (u: StaticDIUniverse.Aux[c.universe.type])
+                                          (logger: TrivialLogger)
+                                          (association: u.Association
+                                          // parameter: u.Association.Parameter, ctorArgument: u.u.Tree, traitMethodImpl: u.u.Tree, ctorArgumentName: u.u.TermName
+                                          ): (u.Association.Parameter, u.u.Tree, (u.u.Tree, u.u.TermName)) = {
+    import c.universe._
+    import u.Association._
+
+    association match {
+      case method: AbstractMethod =>
+        val paramTpe = method.symbol.finalResultType
+        val methodName = TermName(method.name)
+        val freshArgName = c.freshName(methodName)
+        // force by-name
+        val byNameParamTpe = appliedType(definitions.ByNameParamClass, paramTpe)
+
+        val parameter = method.asParameter
+        logger.log(s"original method return: $paramTpe, after by-name: $byNameParamTpe, $parameter")
+
+        (parameter, q"val $freshArgName: $byNameParamTpe", q"final lazy val $methodName: $paramTpe = $freshArgName" -> freshArgName)
+      case parameter: Parameter =>
+        val paramTpe = parameter.symbol.finalResultType
+        val methodName = TermName(parameter.name)
+        val freshArgName = c.freshName(TermName(parameter.name))
+
+        (parameter, q"val $freshArgName: $paramTpe", q"final lazy val $methodName: $paramTpe = $freshArgName" -> freshArgName)
     }
   }
 
--- distage/distage-extension-plugins/src/main/scala/izumi/distage/plugins/DebugProperties.scala
+++ distage/distage-extension-plugins/src/main/scala/izumi/distage/plugins/DebugProperties.scala
@@ -1,13 +1,13 @@
 package izumi.distage.plugins
 
-import izumi.fundamentals.platform.properties.DebugProperties
+import izumi.fundamentals.platform.properties
 
 /**
   * Java properties that control behavior of [[load.PluginLoader]]
   *
   * @see [[DebugProperties]]
   */
-object DebugProperties extends DebugProperties {
+object DebugProperties extends properties.DebugProperties {
   /** Scan classpath only once per test run */
   final val `izumi.distage.plugins.cache` = "izumi.distage.plugins.cache"
 }
