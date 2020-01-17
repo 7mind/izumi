37cb593135b9770b0b03c5da2e29b121ede1f6ee
Kai <kai@7mind.io>
2020-01-17 21:18:06 +0000

    attempt2

--- distage/distage-extension-plugins/src/main/scala/izumi/distage/plugins/PluginConfig.scala
+++ distage/distage-extension-plugins/src/main/scala/izumi/distage/plugins/PluginConfig.scala
@@ -1,17 +1,33 @@
 package izumi.distage.plugins
 
+final case class PluginLoadConfig(
+                                   packagesEnabled: Seq[String],
+                                   packagesDisabled: Seq[String],
+                                   cachePackages: Boolean,
+                                   debug: Boolean,
+                                 )
+object PluginLoadConfig {
+  def cached(packagesEnabled: Seq[String]): PluginLoadConfig = PluginLoadConfig(packagesEnabled, Nil, cachePackages = cacheEnabled, debug = false)
+  def packages(packagesEnabled: Seq[String]): PluginLoadConfig = PluginLoadConfig(packagesEnabled, Nil, cachePackages = false, debug = false)
+  lazy val initial: Option[PluginLoadConfig] = Some(cached(Nil))
+
+  private[this] lazy val cacheEnabled: Boolean = {
+    import izumi.fundamentals.platform.strings.IzString._
+    System
+      .getProperty(DebugProperties.`izumi.distage.plugins.cache`)
+      .asBoolean(true)
+  }
+}
+
 final case class PluginConfig(
-                             packagesEnabled: Seq[String],
-                             packagesDisabled: Seq[String],
-                             cachePackages: Boolean,
-                             debug: Boolean,
-                             merges: Seq[PluginBase],
-                             overrides: Seq[PluginBase],
-                           ) {
-  def enablePackages(packagesEnabled: Seq[String]): PluginConfig = copy(packagesEnabled = this.packagesEnabled ++ packagesEnabled)
+                               loadConfig: Option[PluginLoadConfig],
+                               merges: Seq[PluginBase],
+                               overrides: Seq[PluginBase],
+                             ) {
+  def enablePackages(packagesEnabled: Seq[String]): PluginConfig = modifyLoadConfig(c => c.copy(packagesEnabled = c.packagesEnabled ++ packagesEnabled))
   def enablePackage(packageEnabled: String): PluginConfig = enablePackages(Seq(packageEnabled))
 
-  def disablePackages(packagesDisabled: Seq[String]): PluginConfig = copy(packagesDisabled = this.packagesDisabled ++ packagesDisabled)
+  def disablePackages(packagesDisabled: Seq[String]): PluginConfig = modifyLoadConfig(c => c.copy(packagesDisabled = c.packagesDisabled ++ packagesDisabled))
   def disablePackage(packageDisabled: String): PluginConfig = disablePackages(Seq(packageDisabled))
 
   def ++(plugins: Seq[PluginBase]): PluginConfig = copy(merges = merges ++ plugins)
@@ -20,24 +36,21 @@ final case class PluginConfig(
   def overridenBy(plugins: Seq[PluginBase]): PluginConfig = copy(overrides = overrides ++ plugins)
   def overridenBy(plugin: PluginBase): PluginConfig = copy(overrides = overrides ++ Seq(plugin))
 
-  def cachePackages(cachePackages: Boolean): PluginConfig = copy(cachePackages = cachePackages)
-  def debug(debug: Boolean): PluginConfig = copy(debug = debug)
+  def cachePackages(cachePackages: Boolean): PluginConfig = copy(loadConfig = loadConfig.map(_.copy(cachePackages = cachePackages)))
+  def debug(debug: Boolean): PluginConfig = copy(loadConfig = loadConfig.map(_.copy(debug = debug)))
+
+  @inline private[this] def modifyLoadConfig(f: PluginLoadConfig => PluginLoadConfig): PluginConfig = {
+    copy(loadConfig = Some(f(loadConfig.getOrElse(PluginLoadConfig.initial))))
+  }
 }
 
 object PluginConfig {
   /** Scan the specified packages for modules that inherit [[PluginBase]] */
-  def cached(packagesEnabled: Seq[String]): PluginConfig = PluginConfig(packagesEnabled, Nil, cachePackages = cacheEnabled, debug = false, Nil, Nil)
-  def packages(packagesEnabled: Seq[String]): PluginConfig = PluginConfig(packagesEnabled, Nil, cachePackages = false, debug = false, Nil, Nil)
+  def cached(packagesEnabled: Seq[String]): PluginConfig = PluginConfig(Some(PluginLoadConfig.cached(packagesEnabled)), Nil, Nil)
+  def packages(packagesEnabled: Seq[String]): PluginConfig = PluginConfig(Some(PluginLoadConfig.packages(packagesEnabled)), Nil, Nil)
 
   /** Create a [[PluginConfig]] that simply returns the specified plugins */
-  def const(plugins: Seq[PluginBase]): PluginConfig = PluginConfig(Nil, Nil, cachePackages = false, debug = false, plugins, Nil)
+  def const(plugins: Seq[PluginBase]): PluginConfig = PluginConfig(None, plugins, Nil)
   def const(plugin: PluginBase): PluginConfig = const(Seq(plugin))
   lazy val empty: PluginConfig = const(Nil)
-
-  private[this] lazy val cacheEnabled: Boolean = {
-    import izumi.fundamentals.platform.strings.IzString._
-    System
-      .getProperty(DebugProperties.`izumi.distage.plugins.cache`)
-      .asBoolean(true)
-  }
 }
