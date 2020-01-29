b84d2d29477c5558901a127949ead9b996a33b13
Scala Steward <me@scala-steward.org>
2020-01-29 20:01:47 +0100

    Update sbt-unidoc to 0.4.3

--- project/project/PluginVersions.scala
+++ project/project/PluginVersions.scala
@@ -3,7 +3,7 @@ object PV {
   val sbt_paradox_material_theme = "0.6.0"
   val sbt_ghpages = "0.6.3"
   val sbt_site = "1.3.3"
-  val sbt_unidoc = "0.4.2"
+  val sbt_unidoc = "0.4.3"
   val sbt_scoverage = "1.6.1"
   val sbt_pgp = "2.0.1"
   val sbt_assembly = "0.14.9"
