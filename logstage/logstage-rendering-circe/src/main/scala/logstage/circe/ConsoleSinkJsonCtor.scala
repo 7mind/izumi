d3ac268bc4b0f05d26d8f3a69314c4b2927ebd2c
Kai <450507+neko-kai@users.noreply.github.com>
2020-01-29 18:28:05 +0000

    logstage: Deprecate `ConsoleSink.json` because it requires an obscure wildcard import, use `ConsoleSink(LogstageCirceRenderingPolicy())` (#867)

--- logstage/logstage-rendering-circe/src/main/scala/logstage/circe/ConsoleSinkJsonCtor.scala
+++ logstage/logstage-rendering-circe/src/main/scala/logstage/circe/ConsoleSinkJsonCtor.scala
@@ -4,8 +4,9 @@ import izumi.logstage.sink.ConsoleSink
 
 final class ConsoleSinkJsonCtor(private val consoleSink: ConsoleSink.type) extends AnyVal {
 
+  @deprecated("Deprecated because this method requires a wildcard import to use. Use `logstage.ConsoleSink(logstage.circe.LogstageCirceRenderingPolicy(prettyPrint))`", "since 0.10.1")
   def json(prettyPrint: Boolean = false): ConsoleSink = {
-    consoleSink(new LogstageCirceRenderingPolicy(prettyPrint))
+    consoleSink(LogstageCirceRenderingPolicy(prettyPrint))
   }
 
 }
--- logstage/logstage-rendering-circe/src/main/scala/logstage/circe/LogstageCirce.scala
+++ logstage/logstage-rendering-circe/src/main/scala/logstage/circe/LogstageCirce.scala
@@ -8,6 +8,7 @@ import scala.language.implicitConversions
 trait LogstageCirce {
 
   type LogstageCirceRenderingPolicy = json.LogstageCirceRenderingPolicy
+  val LogstageCirceRenderingPolicy: json.LogstageCirceRenderingPolicy.type = json.LogstageCirceRenderingPolicy
 
   @inline implicit final def ToConsoleSinkJsonCtor(consoleSink: ConsoleSink.type): ConsoleSinkJsonCtor = new ConsoleSinkJsonCtor(consoleSink)
 
