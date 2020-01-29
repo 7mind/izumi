e80498a705b1828ea4938cdd12142f16586f3f01
Kai <kai@7mind.io>
2020-01-29 18:27:35 +0000

    logstage: Deprecate `ConsoleSink.json` because it requires an obscure wildcard import, use `ConsoleSink(LogstageCirceRenderingPolicy())`

--- logstage/logstage-rendering-circe/src/main/scala/logstage/circe/package.scala
+++ logstage/logstage-rendering-circe/src/main/scala/logstage/circe/package.scala
@@ -5,5 +5,6 @@ import izumi.logstage.api.rendering.json
 package object circe extends LogstageCirce {
 
   override type LogstageCirceRenderingPolicy = json.LogstageCirceRenderingPolicy
+  override val LogstageCirceRenderingPolicy: json.LogstageCirceRenderingPolicy.type = json.LogstageCirceRenderingPolicy
 
 }
