99723b22ab787104e640e410fe375c4ea8f32c37
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-02-12 15:08:23 +0100

    Update sbt-scala-native-crossproject to 1.0.0 (#890)

--- project/project/PluginVersions.scala
+++ project/project/PluginVersions.scala
@@ -10,6 +10,6 @@ object PV {
 
   val scala_js_version = "0.6.29"
   val scala_native_version = "0.4.0-M2"
-  val crossproject_version = "0.6.1"
+  val crossproject_version = "1.0.0"
   val scalajs_bundler_version = "0.14.0"
 }
