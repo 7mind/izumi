a1256cd8397b4269478f2357b642b58276a91aa1
Pavel Shirshov <pshirshov@gmail.com>
2020-02-14 17:16:25 +0000

    wip

--- distage/distage-core/src/main/scala/distage/InjectorF.scala
+++ distage/distage-core/src/main/scala/distage/InjectorF.scala
@@ -10,6 +10,8 @@ sealed trait InjectorF[F[_], T] {
 
 object InjectorF {
 
+  case class State(locator: Locator)
+
   case class FlatMapLocator[F[_], T1](
                                        prev: InjectorF[F, PlannerInput],
                                        f: Locator => InjectorF[F, T1]
@@ -41,9 +43,21 @@ object InjectorF {
     }
   }
 
+  case class UseState[F[_], T](t: State => T) extends InjectorF[F, T] {
+    def map[T1](f: T => T1): InjectorF[F, T1] = {
+      UseState(locator => f(t(locator)))
+    }
+
+    def flatMap[T1](f: T => InjectorF[F, T1]): InjectorF[F, T1] = {
+      FlatMapValue(this, f)
+    }
+  }
+
+
   def declare[F[_]](module: PlannerInput): DeclareModule[F] = new DeclareModule[F](module)
   def useLocator[F[_], T](f: Locator => T): UseLocator[F, T] = new UseLocator[F, T](f)
   def use[F[_], T](f: ProviderMagnet[T]): UseLocator[F, T] = new UseLocator[F, T](_.run(f))
+  def raw[F[_], T](f: State => T): UseState[F, T] = new UseState[F, T](f)
 
   def run[F[_] : DIEffect : TagK, T](
                                       injectorF: InjectorF[F, T],
@@ -52,16 +66,16 @@ object InjectorF {
                                     ): F[T] = {
     val F = implicitly[DIEffect[F]]
 
-    def interpret[T1, B](in: Locator, i: InjectorF[F, T1]): F[B] = {
+    def interpret[T1, B](state: State, i: InjectorF[F, T1]): F[B] = {
       val out: F[B] = i match {
         case fm: FlatMapLocator[F, PlannerInput]@unchecked =>
           for {
-            ms <- F.maybeSuspend(interpret[PlannerInput, PlannerInput](in, fm.prev))
+            ms <- F.maybeSuspend(interpret[PlannerInput, PlannerInput](state, fm.prev))
             m <- ms
-            loc <- F.maybeSuspend(Injector.inherit(in).produceF[F](m))
+            loc <- F.maybeSuspend(Injector.inherit(state.locator).produceF[F](m))
             out <- loc.use {
-              loc =>
-                interpret[PlannerInput, B](loc, fm.f(loc))
+              newLocator =>
+                interpret[PlannerInput, B](state.copy(locator = newLocator), fm.f(newLocator))
             }
           } yield {
             out
@@ -69,10 +83,10 @@ object InjectorF {
 
         case fm: FlatMapValue[F, T1, B]@unchecked =>
           for {
-            p <- F.maybeSuspend(interpret[T1, T1](in, fm.prev))
+            p <- F.maybeSuspend(interpret[T1, T1](state, fm.prev))
             v <- p
             i = fm.f.apply(v)
-            out <- F.maybeSuspend(interpret[B, B](in, i))
+            out <- F.maybeSuspend(interpret[B, B](state, i))
             r <- out
           } yield {
             r
@@ -80,7 +94,7 @@ object InjectorF {
 
         case m: Map[F, T1, B]@unchecked =>
           for {
-            p <- F.maybeSuspend(interpret[T1, T1](in, m.prev).map(m.f))
+            p <- F.maybeSuspend(interpret[T1, T1](state, m.prev).map(m.f))
             out <- p
           } yield {
             out
@@ -90,7 +104,19 @@ object InjectorF {
           F.pure(module)
 
         case u: UseLocator[F, B]@unchecked =>
-          F.maybeSuspend(u.t(in))
+//          for {
+//            loc <- F.maybeSuspend(Injector.inherit(state.locator).produceF[F](m))
+//            out <- loc.use {
+//              newLocator =>
+//                interpret[PlannerInput, B](state.copy(locator = newLocator), fm.f(newLocator))
+//            }
+//            u <- F.maybeSuspend(u.t(state.locator))
+//          } yield {
+//          }
+          F.maybeSuspend(u.t(state.locator))
+
+        case u: UseState[F, B]@unchecked =>
+          F.maybeSuspend(u.t(state))
       }
       out
     }
@@ -100,7 +126,8 @@ object InjectorF {
     val bootstrapLocator = new BootstrapLocator(bootstrapDefinition overridenBy new BootstrapModuleDef {
       make[BootstrapModule].fromValue(bootstrapDefinition)
     })
-    interpret(bootstrapLocator, injectorF)
+    val state = State(bootstrapLocator)
+    interpret(state, injectorF)
   }
 }
 
