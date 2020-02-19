10d315ab16b59212fcfab1b5ea415ff054659293
Kai <kai@7mind.io>
2020-02-19 21:34:53 +0000

    distage-testkit: add effectful assertions

---
+++ distage/distage-testkit-scalatest/src/main/scala/izumi/distage/testkit/scalatest/AssertBIO.scala
@@ -0,0 +1,25 @@
+package izumi.distage.testkit.scalatest
+
+import izumi.distage.testkit.scalatest.AssertBIO.AssertBIOMacro
+import izumi.functional.bio.BIO
+import org.scalactic.{Prettifier, source}
+import org.scalatest.{Assertion, DistageAssertionsMacro}
+
+import scala.language.experimental.macros
+import scala.reflect.macros.blackbox
+
+trait AssertBIO {
+  final def assertBIO[F[+_, +_]](arg: Boolean)(implicit BIO: BIO[F], prettifier: Prettifier, pos: source.Position): F[Nothing, Assertion] = macro AssertBIOMacro.impl[F]
+}
+
+object AssertBIO extends AssertBIO {
+
+  object AssertBIOMacro {
+    def impl[F[+_, +_]](c: blackbox.Context)(arg: c.Expr[Boolean])(BIO: c.Expr[BIO[F]], prettifier: c.Expr[Prettifier], pos: c.Expr[org.scalactic.source.Position]): c.Expr[F[Nothing, Assertion]] = {
+      import c.universe._
+      c.Expr[F[Nothing, Assertion]](
+        q"$BIO.sync(${DistageAssertionsMacro.assert(c)(arg)(prettifier, pos)})")
+    }
+  }
+
+}
---
+++ distage/distage-testkit-scalatest/src/main/scala/org/scalatest/DistageAssertionsMacro.scala
@@ -0,0 +1,21 @@
+package org.scalatest
+
+import org.scalactic.Prettifier
+import org.scalactic.source.Position
+
+import scala.reflect.macros.{blackbox, whitebox}
+
+object DistageAssertionsMacro {
+  def assert(c: blackbox.Context)(arg: c.Expr[Boolean])(prettifier: c.Expr[Prettifier], pos: c.Expr[Position]): c.Expr[Assertion] = {
+    // Force scalatest's `assert` to run in `blackbox` Context
+    // The casts don't fail because at runtime there's actually only one `Context`
+    val c0 = c.asInstanceOf[whitebox.Context]
+    AssertionsMacro
+      .assert(
+        context = c.asInstanceOf[c0.type]
+      )(condition = arg.asInstanceOf[c0.Expr[Boolean]])(
+        prettifier = prettifier.asInstanceOf[c0.Expr[Prettifier]],
+        pos        = pos.asInstanceOf[c0.Expr[Position]],
+      ).asInstanceOf[c.Expr[Assertion]]
+  }
+}
