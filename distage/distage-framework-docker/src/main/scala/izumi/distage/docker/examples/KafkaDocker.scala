e5a1f483a7ae66abd278b3dd47c3f1059f9b35ed
Alex Liubymov <arowcap@gmail.com>
2020-03-13 15:16:03 +0200

    always random kafka and ZK (with one network)

--- distage/distage-framework-docker/src/main/scala/izumi/distage/docker/examples/KafkaDocker.scala
+++ distage/distage-framework-docker/src/main/scala/izumi/distage/docker/examples/KafkaDocker.scala
@@ -1,8 +1,10 @@
 package izumi.distage.docker.examples
 
 import distage.{ModuleDef, TagK}
-import izumi.distage.docker.ContainerDef
-import izumi.distage.docker.Docker.{ContainerConfig, ContainerNetwork, DockerPort}
+import izumi.distage.docker.Docker.{ContainerConfig, DockerPort}
+import izumi.distage.docker.{ContainerDef, DockerClientWrapper, DockerContainer}
+import izumi.distage.model.effect.{DIEffect, DIEffectAsync}
+import izumi.logstage.api.IzLogger
 
 object KafkaDocker extends ContainerDef {
   val primaryPort: DockerPort.TCP = DockerPort.TCP(9092)
@@ -10,17 +12,23 @@ object KafkaDocker extends ContainerDef {
     ContainerConfig(
       image = "wurstmeister/kafka:1.0.0",
       ports = Seq(primaryPort),
-      networks = Seq(ZookeeperDocker.zookeeperNetwork),
       env = Map(
-        "KAFKA_ADVERTISED_HOST_NAME" -> "127.0.0.1",
-        "KAFKA_ZOOKEEPER_CONNECT" -> "zookeeper-service:2181"
-      )
+        "KAFKA_ADVERTISED_HOST_NAME" -> "127.0.0.1"
+      ),
+      networks = Seq(ZookeeperDocker.zookeeperNetwork),
+      reuse = false
     )
   }
 }
 
 class KafkaDockerModule[F[_]: TagK] extends ModuleDef {
   make[KafkaDocker.Container].fromResource {
-    KafkaDocker.make[F].dependOnDocker(ZookeeperDocker)
+    (zookeeperDocker: ZookeeperDocker.Container, wr: DockerClientWrapper[F], log: IzLogger, eff: DIEffect[F], effAsync: DIEffectAsync[F]) =>
+      val conf = zookeeperDocker.containerConfig.name.fold(KafkaDocker.config) {
+        name =>
+          val zkEnv = KafkaDocker.config.env ++ Map("KAFKA_ZOOKEEPER_CONNECT" -> s"$name:2181")
+          KafkaDocker.config.copy(env = zkEnv)
+      }
+      new DockerContainer.Resource[F, KafkaDocker.Tag](conf, wr, log)(eff, effAsync)
   }
 }
--- distage/distage-framework-docker/src/main/scala/izumi/distage/docker/examples/ZookkeeperDocker.scala
+++ distage/distage-framework-docker/src/main/scala/izumi/distage/docker/examples/ZookkeeperDocker.scala
@@ -1,8 +1,12 @@
 package izumi.distage.docker.examples
 
+import java.util.UUID
+
 import distage.{ModuleDef, TagK}
-import izumi.distage.docker.ContainerDef
 import izumi.distage.docker.Docker.{ContainerConfig, ContainerNetwork, DockerPort}
+import izumi.distage.docker.{ContainerDef, DockerClientWrapper, DockerContainer}
+import izumi.distage.model.effect.{DIEffect, DIEffectAsync}
+import izumi.logstage.api.IzLogger
 
 object ZookeeperDocker extends ContainerDef {
   val zookeeperNetwork: ContainerNetwork = ContainerNetwork("zookeeper-network")
@@ -10,14 +14,16 @@ object ZookeeperDocker extends ContainerDef {
     ContainerConfig(
       image = "zookeeper:3.4.13",
       ports = Seq(DockerPort.TCP(2181), DockerPort.TCP(2888), DockerPort.TCP(3888)),
-      name = Some("zookeeper-service"),
-      networks = Seq(zookeeperNetwork)
+      networks = Seq(zookeeperNetwork),
+      reuse = false
     )
   }
 }
 
 class ZookeeperDockerModule[F[_]: TagK] extends ModuleDef {
   make[ZookeeperDocker.Container].fromResource {
-    ZookeeperDocker.make[F]
+    (wr: DockerClientWrapper[F], log: IzLogger, eff: DIEffect[F], effAsync: DIEffectAsync[F]) =>
+      val randomName = s"zookeeper-${UUID.randomUUID().toString.take(8)}"
+      new DockerContainer.Resource[F, ZookeeperDocker.Tag](ZookeeperDocker.config.copy(name = Some(randomName)), wr, log)(eff, effAsync)
   }
 }
