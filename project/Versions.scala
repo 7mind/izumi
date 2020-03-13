8220bebd889278c79a810b3054f12dda12b2f99b
Scala Steward <me@scala-steward.org>
2020-03-13 18:05:38 +0100

    Update circe-derivation to 0.13.0-M3

--- project/Versions.scala
+++ project/Versions.scala
@@ -14,7 +14,7 @@ object V {
 
   val circe = "0.13.0"
   val circe_generic_extras = "0.13.0"
-  val circe_derivation = "0.13.0-M2"
+  val circe_derivation = "0.13.0-M3"
   val pureconfig = "0.12.3"
   val jawn = "1.0.0"
 
