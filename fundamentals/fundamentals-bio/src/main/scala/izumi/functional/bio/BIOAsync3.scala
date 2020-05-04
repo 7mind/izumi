b982d5f3e6ccbae9162e729bbca49d26e5f11063
Adriani Furtado <36544115+Adriani277@users.noreply.github.com>
2020-05-04 17:01:56 +0100

    Added zipWithPar and derived zipPar, zipParLeft and zipParRight (#1029)
    * Added zipWithPar and derived zipPar, zipParLeft and zipParRight
    
    * Added BIOParallel sub-hierarchy
    
    * Made changes proposed in the PR

--- fundamentals/fundamentals-bio/src/main/scala/izumi/functional/bio/BIOAsync3.scala
+++ fundamentals/fundamentals-bio/src/main/scala/izumi/functional/bio/BIOAsync3.scala
@@ -4,7 +4,7 @@ import java.util.concurrent.CompletionStage
 
 import scala.concurrent.{ExecutionContext, Future}
 
-trait BIOAsync3[F[-_, +_, +_]] extends BIO3[F] {
+trait BIOAsync3[F[-_, +_, +_]] extends BIO3[F] with BIOParallel[F] {
   final type Canceler = F[Any, Nothing, Unit]
 
   def async[E, A](register: (Either[E, A] => Unit) => Unit): F[Any, E, A]
@@ -20,16 +20,10 @@ trait BIOAsync3[F[-_, +_, +_]] extends BIO3[F] {
   def race[R, E, A](r1: F[R, E, A], r2: F[R, E, A]): F[R, E, A]
   def racePair[R, E, A, B](fa: F[R, E, A], fb: F[R, E, B]): F[R, E, Either[(A, BIOFiber3[F, E, B]), (BIOFiber3[F, E, A], B)]]
 
-  def parTraverseN[R, E, A, B](maxConcurrent: Int)(l: Iterable[A])(f: A => F[R, E, B]): F[R, E, List[B]]
-  def parTraverse[R, E, A, B](l: Iterable[A])(f: A => F[R, E, B]): F[R, E, List[B]]
-
   def uninterruptible[R, E, A](r: F[R, E, A]): F[R, E, A]
 
   // defaults
   def never: F[Any, Nothing, Nothing] = async(_ => ())
-
-  def parTraverse_[R, E, A, B](l: Iterable[A])(f: A => F[R, E, B]): F[R, E, Unit] = void(parTraverse(l)(f))
-  def parTraverseN_[R, E, A, B](maxConcurrent: Int)(l: Iterable[A])(f: A => F[R, E, B]): F[R, E, Unit] = void(parTraverseN(maxConcurrent)(l)(f))
 
   @inline final def fromFuture[A](mkFuture: => Future[A]): F[Any, Throwable, A] = fromFuture(_ => mkFuture)
 }
