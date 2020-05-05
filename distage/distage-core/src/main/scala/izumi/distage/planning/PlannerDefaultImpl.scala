e1854b004c2b3202c2ef617e357f1032207e1aca
Pavel Shirshov <pshirshov@gmail.com>
2020-05-05 23:11:14 +0100

    cleanups

--- distage/distage-core/src/main/scala/izumi/distage/planning/PlannerDefaultImpl.scala
+++ distage/distage-core/src/main/scala/izumi/distage/planning/PlannerDefaultImpl.scala
@@ -154,8 +154,7 @@ final class PlannerDefaultImpl(
     }
   }
 
-  @deprecated("", "")
-  override def truncate(plan: OrderedPlan, roots: Set[DIKey]): OrderedPlan = {
+  protected[distage] override def truncate(plan: OrderedPlan, roots: Set[DIKey]): OrderedPlan = {
     if (roots.isEmpty) {
       OrderedPlan.empty
     } else {
@@ -169,37 +168,29 @@ final class PlannerDefaultImpl(
     hook.hookDefinition(module)
   }
 
-  @deprecated("", "")
+  @deprecated("used in tests only!", "")
   override def finish(semiPlan: SemiPlan): OrderedPlan = {
     Value(semiPlan)
       .map(addImports)
       .eff(planningObserver.onPhase05PreGC)
       .map(gc.gc)
-      .map(hook.phase10PostGC)
-      .eff(planningObserver.onPhase10PostGC)
-      .map(hook.phase20Customization)
-      .eff(planningObserver.onPhase20Customization)
       .map(order)
       .get
   }
 
-  @deprecated("", "")
-  def finishNoGC(semiPlan: SemiPlan): OrderedPlan = {
+  private def finishNoGC(semiPlan: SemiPlan): OrderedPlan = {
     Value(semiPlan)
       .map(addImports)
-      //      .eff(planningObserver.onPhase05PreGC)
-      //      .map(gc.gc)
-      .map(hook.phase10PostGC)
-      .eff(planningObserver.onPhase10PostGC)
-      .map(hook.phase20Customization)
-      .eff(planningObserver.onPhase20Customization)
       .map(order)
       .get
   }
 
-  @deprecated("", "")
   private[this] def order(semiPlan: SemiPlan): OrderedPlan = {
     Value(semiPlan)
+      .map(hook.phase10PostGC)
+      .eff(planningObserver.onPhase10PostGC)
+      .map(hook.phase20Customization)
+      .eff(planningObserver.onPhase20Customization)
       .map(hook.phase45PreForwardingCleanup)
       .map(hook.phase50PreForwarding)
       .eff(planningObserver.onPhase50PreForwarding)
@@ -218,7 +209,6 @@ final class PlannerDefaultImpl(
   }
 
   @silent("Unused import")
-  @deprecated("", "")
   private[this] def addImports(plan: SemiPlan): SemiPlan = {
     import scala.collection.compat._
 
