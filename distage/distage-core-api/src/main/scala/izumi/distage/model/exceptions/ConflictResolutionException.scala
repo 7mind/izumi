6cae04b3b220d734b2b18c80f571985cfcfc5f88
Pavel Shirshov <pshirshov@gmail.com>
2020-05-05 20:33:05 +0100

    cleanups

--- distage/distage-core-api/src/main/scala/izumi/distage/model/exceptions/ConflictResolutionException.scala
+++ distage/distage-core-api/src/main/scala/izumi/distage/model/exceptions/ConflictResolutionException.scala
@@ -3,5 +3,4 @@ package izumi.distage.model.exceptions
 import izumi.distage.model.reflection.DIKey
 import izumi.fundamentals.graphs.ConflictResolutionError
 
-//class ConflictingDIKeyBindingsException(message: String, val conflicts: Map[DIKey, DIKeyConflictResolution.Failed]) extends DIException(message)
 class ConflictResolutionException(message: String, val conflicts: List[ConflictResolutionError[DIKey]]) extends DIException(message)
