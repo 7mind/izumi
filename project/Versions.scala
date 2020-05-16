e0f0f7276810485b675be40645e87fde16257e54
Scala Steward <me@scala-steward.org>
2020-05-16 14:02:04 +0200

    Update zio to 1.0.0-RC19

--- project/Versions.scala
+++ project/Versions.scala
@@ -9,7 +9,7 @@ object V {
 
   val cats = "2.1.1"
   val cats_effect = "2.1.3"
-  val zio = "1.0.0-RC18-2"
+  val zio = "1.0.0-RC19"
   val zio_interop_cats = "2.0.0.0-RC12"
 
   val circe = "0.13.0"
