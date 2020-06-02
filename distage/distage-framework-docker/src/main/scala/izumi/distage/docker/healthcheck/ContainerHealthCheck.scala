02793efb2cd91ddecd5a15e1105f79e87f485f61
Alex Liubymov <arowcap@gmail.com>
2020-06-02 11:43:11 +0300

    add combine logic to health checks

--- distage/distage-framework-docker/src/main/scala/izumi/distage/docker/healthcheck/ContainerHealthCheck.scala
+++ distage/distage-framework-docker/src/main/scala/izumi/distage/docker/healthcheck/ContainerHealthCheck.scala
@@ -8,20 +8,42 @@ import izumi.logstage.api.IzLogger
 
 trait ContainerHealthCheck[Tag] {
   def check(logger: IzLogger, container: DockerContainer[Tag]): HealthCheckResult
-}
 
+  final def combine(next: ContainerHealthCheck[Tag]): ContainerHealthCheck[Tag] = this ++ next
+  final def ++(next: ContainerHealthCheck[Tag]): ContainerHealthCheck[Tag] = {
+    (logger: IzLogger, container: DockerContainer[Tag]) =>
+      check(logger, container) match {
+        case status: HealthCheckResult.PortStatus if status.requiredPortsAccessible =>
+          next.check(logger, container) match {
+            case HealthCheckResult.Ignored | HealthCheckResult.ContainerAvailable => status
+            case nextStatus: HealthCheckResult.PortStatus => status.merge(nextStatus)
+          }
+        case HealthCheckResult.ContainerAvailable => next.check(logger, container)
+        case other => other
+      }
+  }
+}
 
 object ContainerHealthCheck {
   sealed trait HealthCheckResult
   object HealthCheckResult {
     case object Ignored extends HealthCheckResult
-
+    case object ContainerAvailable extends HealthCheckResult
     final case class PortStatus(
-                                 availablePorts: VerifiedContainerConnectivity,
-                                 unavailablePorts: UnavailablePorts,
-                                 unverifiedPorts: Set[DockerPort],
-                                 requiredPortsAccessible: Boolean,
-                               ) extends HealthCheckResult
+      availablePorts: VerifiedContainerConnectivity,
+      unavailablePorts: UnavailablePorts,
+      unverifiedPorts: Set[DockerPort],
+      requiredPortsAccessible: Boolean,
+    ) extends HealthCheckResult {
+      def merge(other: PortStatus): PortStatus = {
+        PortStatus(
+          VerifiedContainerConnectivity(availablePorts.availablePorts ++ other.availablePorts.availablePorts),
+          UnavailablePorts(unavailablePorts.unavailablePorts ++ other.unavailablePorts.unavailablePorts),
+          unverifiedPorts ++ other.unverifiedPorts,
+          requiredPortsAccessible && other.requiredPortsAccessible,
+        )
+      }
+    }
   }
 
   case class UnavailablePorts(unavailablePorts: Map[DockerPort, List[(AvailablePort, Option[Throwable])]])
