1f0350b56dd1175ee0deb8b32d95454ec2a03e61
Alex Liubymov <arowcap@gmail.com>
2020-06-03 18:07:33 +0300

    use advertised gost/port instead of listeners

--- distage/distage-framework-docker/src/main/scala/izumi/distage/docker/examples/KafkaDocker.scala
+++ distage/distage-framework-docker/src/main/scala/izumi/distage/docker/examples/KafkaDocker.scala
@@ -8,14 +8,15 @@ object KafkaDocker extends ContainerDef {
   val primaryPort: DockerPort = DockerPort.DynamicTCP("dynamic_kafka_port")
 
   private[this] def portVars: String = Seq(
-    s"""KAFKA_LISTENERS="PLAINTEXT://:$$${primaryPort.toEnvVariable}"""",
-    s"""KAFKA_ADVERTISED_LISTENERS="PLAINTEXT://127.0.0.1:$$${primaryPort.toEnvVariable}"""",
+    s"""KAFKA_ADVERTISED_PORT=$$${primaryPort.toEnvVariable}""",
+    s"""KAFKA_PORT=$$${primaryPort.toEnvVariable}""",
   ).map(defn => s"export $defn").mkString("; ")
 
   override def config: Config = {
     Config(
       image = "wurstmeister/kafka:2.12-2.4.1",
       ports = Seq(primaryPort),
+      env = Map("KAFKA_ADVERTISED_HOST_NAME" -> "127.0.0.1"),
       entrypoint = Seq(
         "sh",
         "-c",
