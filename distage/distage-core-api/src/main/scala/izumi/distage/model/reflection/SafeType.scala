37b09d0733ee21eddc79bffa0e86fd8f952d4ac0
Kai <kai@7mind.io>
2020-06-05 17:30:37 +0100

    java8 compat for `SafeType#hasPreciseClass`

--- distage/distage-core-api/src/main/scala/izumi/distage/model/reflection/SafeType.scala
+++ distage/distage-core-api/src/main/scala/izumi/distage/model/reflection/SafeType.scala
@@ -4,10 +4,10 @@ import izumi.fundamentals.platform.functional.Identity
 import izumi.reflect.macrortti.LightTypeTag
 import izumi.reflect.{Tag, TagK, WeakTag}
 
-final case class SafeType private(
-                                   tag: LightTypeTag,
-                                   /*private[distage] val */ cls: Class[_],
-                                 ) {
+final case class SafeType private (
+  tag: LightTypeTag,
+  /*private[distage] val */ cls: Class[_],
+) {
   override final lazy val hashCode: Int = tag.hashCode()
   override final def toString: String = tag.repr
 
@@ -29,8 +29,10 @@ final case class SafeType private(
   }
 
   final def hasPreciseClass: Boolean = {
-    try tag.shortName == cls.getSimpleName catch {
-      case i: InternalError if i.getMessage == "Malformed class name" => false
+    try tag.shortName == cls.getSimpleName
+    catch {
+      case i: InternalError if i.getMessage == "Malformed class name" =>
+        tag.shortName == cls.getName.reverse.takeWhile(c => c != '$' && c != '.').reverse
     }
   }
 }
