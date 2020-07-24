aa46f54244f2fe2696b8df3419424e0f7e1d6f81
Kai <kai@7mind.io>
2020-07-24 12:20:45 +0100

    fixup IntegrationCheck doc after F[_] addition

--- distage/distage-framework-api/src/main/scala/izumi/distage/framework/model/IntegrationCheck.scala
+++ distage/distage-framework-api/src/main/scala/izumi/distage/framework/model/IntegrationCheck.scala
@@ -4,12 +4,9 @@ import izumi.fundamentals.platform.integration.ResourceCheck
 
 /** A trait designating a component depending on an external, possibly unavailable resource.
   *
-  * In [[RoleStarter]], if `resourcesAvailable()` method returns a [[ResourceCheck.Failure]],
-  * startup should stop. When used with [[izumi.distage.testkit.DistageSpec]],
+  * In [[izumi.distage.roles.RoleAppLauncher]], if `resourcesAvailable()` method returns a [[ResourceCheck.Failure]],
+  * startup should stop. When used with [[izumi.distage.testkit.scalatest.DistageSpecScalatest]],
   * if `resourcesAvailable()` method returns a [[ResourceCheck.Failure]] the test will be skipped.
-  *
-  * Not wrapped in `F` because you shouldn't use async or any non-trivial effect inside IntegrationCheck,
-  * it must be _trivial_ and return near-instantly. The execution will be suspended in F.
   */
 trait IntegrationCheck[F[_]] {
   /** This method must never throw, on error return [[ResourceCheck.Failure]] instead * */
