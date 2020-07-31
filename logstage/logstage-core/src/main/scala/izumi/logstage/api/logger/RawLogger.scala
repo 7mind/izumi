e95fa12aa5d642dd6e9320a8ab777959a6206bea
catostrophe <40268503+catostrophe@users.noreply.github.com>
2020-07-31 19:38:07 +0300

    relax delegate type

--- logstage/logstage-core/src/main/scala/izumi/logstage/api/logger/RawLogger.scala
+++ logstage/logstage-core/src/main/scala/izumi/logstage/api/logger/RawLogger.scala
@@ -3,7 +3,7 @@ package izumi.logstage.api.logger
 import izumi.logstage.api.Log.CustomContext
 import izumi.logstage.api.rendering.AnyEncoded
 
-final class RawLogger[E <: AnyEncoded](delegate: EncodingAwareRoutingLogger[E]) extends EncodingAwareRoutingLogger[E] with AbstractMacroRawLogger {
+final class RawLogger[E <: AnyEncoded](delegate: RoutingLogger) extends EncodingAwareRoutingLogger[E] with AbstractMacroRawLogger {
   override type Self = RawLogger[E]
 
   def router: LogRouter = delegate.router
