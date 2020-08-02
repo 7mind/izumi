bf9fe464019aed58b05dad50734d077dd3ceaf9a
catostrophe <40268503+catostrophe@users.noreply.github.com>
2020-08-02 11:01:03 +0300

    conform macros

--- logstage/logstage-core/src/main/scala/izumi/logstage/macros/LogIOMacroMethods.scala
+++ logstage/logstage-core/src/main/scala/izumi/logstage/macros/LogIOMacroMethods.scala
@@ -2,84 +2,89 @@ package izumi.logstage.macros
 
 import izumi.fundamentals.platform.language.CodePositionMaterializer.CodePositionMaterializerMacro.getEnclosingPosition
 import izumi.logstage.api.Log.{Level, Message}
-import logstage.{Log, LogIO}
+import logstage.AbstractLogIO
 
 import scala.reflect.macros.blackbox
 
 object LogIOMacroMethods {
-  def scTraceMacro[F[_]](c: blackbox.Context { type PrefixType = LogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
-    doLog(c)(message, Log.Level.Trace, Mode.NonStrict)
+  def scTraceMacro[F[_]](c: blackbox.Context { type PrefixType = AbstractLogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
+    doLog(c)(message, Level.Trace, EncodingMode.NonStrict)
   }
 
-  def scDebugMacro[F[_]](c: blackbox.Context { type PrefixType = LogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
-    doLog(c)(message, Log.Level.Debug, Mode.NonStrict)
+  def scDebugMacro[F[_]](c: blackbox.Context { type PrefixType = AbstractLogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
+    doLog(c)(message, Level.Debug, EncodingMode.NonStrict)
   }
 
-  def scInfoMacro[F[_]](c: blackbox.Context { type PrefixType = LogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
-    doLog(c)(message, Log.Level.Info, Mode.NonStrict)
+  def scInfoMacro[F[_]](c: blackbox.Context { type PrefixType = AbstractLogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
+    doLog(c)(message, Level.Info, EncodingMode.NonStrict)
   }
 
-  def scWarnMacro[F[_]](c: blackbox.Context { type PrefixType = LogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
-    doLog(c)(message, Log.Level.Warn, Mode.NonStrict)
+  def scWarnMacro[F[_]](c: blackbox.Context { type PrefixType = AbstractLogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
+    doLog(c)(message, Level.Warn, EncodingMode.NonStrict)
   }
 
-  def scErrorMacro[F[_]](c: blackbox.Context { type PrefixType = LogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
-    doLog(c)(message, Log.Level.Error, Mode.NonStrict)
+  def scErrorMacro[F[_]](c: blackbox.Context { type PrefixType = AbstractLogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
+    doLog(c)(message, Level.Error, EncodingMode.NonStrict)
   }
 
-  def scCritMacro[F[_]](c: blackbox.Context { type PrefixType = LogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
-    doLog(c)(message, Log.Level.Crit, Mode.NonStrict)
+  def scCritMacro[F[_]](c: blackbox.Context { type PrefixType = AbstractLogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
+    doLog(c)(message, Level.Crit, EncodingMode.NonStrict)
   }
 
-  def scTraceMacroStrict[F[_]](c: blackbox.Context { type PrefixType = LogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
-    doLog(c)(message, Log.Level.Trace, Mode.Strict)
+  def scTraceMacroStrict[F[_]](c: blackbox.Context { type PrefixType = AbstractLogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
+    doLog(c)(message, Level.Trace, EncodingMode.Strict)
   }
 
-  def scDebugMacroStrict[F[_]](c: blackbox.Context { type PrefixType = LogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
-    doLog(c)(message, Log.Level.Debug, Mode.Strict)
+  def scDebugMacroStrict[F[_]](c: blackbox.Context { type PrefixType = AbstractLogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
+    doLog(c)(message, Level.Debug, EncodingMode.Strict)
   }
 
-  def scInfoMacroStrict[F[_]](c: blackbox.Context { type PrefixType = LogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
-    doLog(c)(message, Log.Level.Info, Mode.Strict)
+  def scInfoMacroStrict[F[_]](c: blackbox.Context { type PrefixType = AbstractLogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
+    doLog(c)(message, Level.Info, EncodingMode.Strict)
   }
 
-  def scWarnMacroStrict[F[_]](c: blackbox.Context { type PrefixType = LogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
-    doLog(c)(message, Log.Level.Warn, Mode.Strict)
+  def scWarnMacroStrict[F[_]](c: blackbox.Context { type PrefixType = AbstractLogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
+    doLog(c)(message, Level.Warn, EncodingMode.Strict)
   }
 
-  def scErrorMacroStrict[F[_]](c: blackbox.Context { type PrefixType = LogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
-    doLog(c)(message, Log.Level.Error, Mode.Strict)
+  def scErrorMacroStrict[F[_]](c: blackbox.Context { type PrefixType = AbstractLogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
+    doLog(c)(message, Level.Error, EncodingMode.Strict)
   }
 
-  def scCritMacroStrict[F[_]](c: blackbox.Context { type PrefixType = LogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
-    doLog(c)(message, Log.Level.Crit, Mode.Strict)
+  def scCritMacroStrict[F[_]](c: blackbox.Context { type PrefixType = AbstractLogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
+    doLog(c)(message, Level.Crit, EncodingMode.Strict)
   }
 
-  def scTraceMacroRaw[F[_]](c: blackbox.Context { type PrefixType = LogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
-    doLog(c)(message, Log.Level.Trace, Mode.Raw)
+  def scTraceMacroRaw[F[_]](c: blackbox.Context { type PrefixType = AbstractLogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
+    doLog(c)(message, Level.Trace, EncodingMode.Raw)
   }
 
-  def scDebugMacroRaw[F[_]](c: blackbox.Context { type PrefixType = LogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
-    doLog(c)(message, Log.Level.Debug, Mode.Raw)
+  def scDebugMacroRaw[F[_]](c: blackbox.Context { type PrefixType = AbstractLogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
+    doLog(c)(message, Level.Debug, EncodingMode.Raw)
   }
 
-  def scInfoMacroRaw[F[_]](c: blackbox.Context { type PrefixType = LogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
-    doLog(c)(message, Log.Level.Info, Mode.Raw)
+  def scInfoMacroRaw[F[_]](c: blackbox.Context { type PrefixType = AbstractLogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
+    doLog(c)(message, Level.Info, EncodingMode.Raw)
   }
 
-  def scWarnMacroRaw[F[_]](c: blackbox.Context { type PrefixType = LogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
-    doLog(c)(message, Log.Level.Warn, Mode.Raw)
+  def scWarnMacroRaw[F[_]](c: blackbox.Context { type PrefixType = AbstractLogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
+    doLog(c)(message, Level.Warn, EncodingMode.Raw)
   }
 
-  def scErrorMacroRaw[F[_]](c: blackbox.Context { type PrefixType = LogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
-    doLog(c)(message, Log.Level.Error, Mode.Raw)
+  def scErrorMacroRaw[F[_]](c: blackbox.Context { type PrefixType = AbstractLogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
+    doLog(c)(message, Level.Error, EncodingMode.Raw)
   }
 
-  def scCritMacroRaw[F[_]](c: blackbox.Context { type PrefixType = LogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
-    doLog(c)(message, Log.Level.Crit, Mode.Raw)
+  def scCritMacroRaw[F[_]](c: blackbox.Context { type PrefixType = AbstractLogIO[F] })(message: c.Expr[String]): c.Expr[F[Unit]] = {
+    doLog(c)(message, Level.Crit, EncodingMode.Raw)
   }
 
-  private[this] def doLog[F[_]](c: blackbox.Context { type PrefixType = LogIO[F] })(message: c.Expr[String], level: Level, mode: Mode): c.universe.Expr[F[Unit]] = {
+  @inline private[this] def doLog[F[_]](
+    c: blackbox.Context { type PrefixType = AbstractLogIO[F] }
+  )(message: c.Expr[String],
+    level: Level,
+    mode: EncodingMode,
+  ): c.universe.Expr[F[Unit]] = {
     val m: c.Expr[Message] = mode.fold(c.universe.reify(Message.raw(message.splice))) {
       strict =>
         new LogMessageMacro0[c.type](c, strict = strict).logMessageMacro(message)
@@ -104,16 +109,4 @@ object LogIOMacroMethods {
     }
   }
 
-  private sealed trait Mode {
-    final def fold[R](onRaw: => R)(onStrictness: Boolean => R): R = this match {
-      case Mode.NonStrict => onStrictness(false)
-      case Mode.Strict => onStrictness(true)
-      case Mode.Raw => onRaw
-    }
-  }
-  private object Mode {
-    case object NonStrict extends Mode
-    case object Strict extends Mode
-    case object Raw extends Mode
-  }
 }
