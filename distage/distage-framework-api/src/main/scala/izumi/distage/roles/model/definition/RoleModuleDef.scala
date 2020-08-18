8d596c890e2d73475ecb16b2cd69db4f6a641662
Kai <kai@7mind.io>
2020-08-18 16:19:01 +0100

    fix scaladoc build

--- distage/distage-framework-api/src/main/scala/izumi/distage/roles/model/definition/RoleModuleDef.scala
+++ distage/distage-framework-api/src/main/scala/izumi/distage/roles/model/definition/RoleModuleDef.scala
@@ -1,7 +1,8 @@
 package izumi.distage.roles.model.definition
 
+import izumi.distage.constructors.macros.AnyConstructorMacro
 import izumi.distage.model.definition.ModuleDef
-import izumi.distage.model.definition.dsl.ModuleDefDSL
+import izumi.distage.model.definition.dsl.ModuleDefDSL.MakeDSL
 import izumi.distage.roles.model.RoleDescriptor.GetRoleDescriptor
 
 import scala.language.experimental.macros
@@ -9,7 +10,7 @@ import scala.reflect.macros.blackbox
 
 trait RoleModuleDef extends ModuleDef {
 
-  final protected[this] def makeRole[T](implicit getRoleDescriptor: GetRoleDescriptor[T]): ModuleDefDSL.MakeDSL[T] =
+  final protected[this] def makeRole[T](implicit getRoleDescriptor: GetRoleDescriptor[T]): MakeDSL[T] =
     macro RoleModuleDef.RoleModuleDefMacros.makeRole[T]
 
 }
@@ -17,13 +18,9 @@ trait RoleModuleDef extends ModuleDef {
 object RoleModuleDef {
 
   object RoleModuleDefMacros {
-    def makeRole[T: c.WeakTypeTag](c: blackbox.Context)(getRoleDescriptor: c.Expr[GetRoleDescriptor[T]]): c.Expr[ModuleDefDSL.MakeDSL[T]] = {
-      import c.universe._
-      val makeExpr = c.Expr[ModuleDefDSL.MakeDSL[T]] {
-        q"${c.prefix}.make[${weakTypeOf[T]}]"
-      }
-      reify {
-        makeExpr.splice.tagged(RoleTag(getRoleDescriptor.splice.roleDescriptor))
+    def makeRole[T: c.WeakTypeTag](c: blackbox.Context)(getRoleDescriptor: c.Expr[GetRoleDescriptor[T]]): c.Expr[MakeDSL[T]] = {
+      c.universe.reify {
+        AnyConstructorMacro.make[MakeDSL, T](c).splice.tagged(RoleTag(getRoleDescriptor.splice.roleDescriptor))
       }
     }
   }
