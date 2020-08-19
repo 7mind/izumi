d20cd65e2503cad8a49b754342f08ad5eca5df0a
VladPodilnyk <vlad.podilnyk@gmail.com>
2020-08-19 17:05:06 +0300

    fix tests.

--- distage/distage-framework/src/main/scala/izumi/distage/effect/modules/CatsDIEffectModule.scala
+++ distage/distage-framework/src/main/scala/izumi/distage/effect/modules/CatsDIEffectModule.scala
@@ -1,7 +1,7 @@
 package izumi.distage.effect.modules
 
 import cats.Parallel
-import cats.effect.{Concurrent, ConcurrentEffect, ContextShift, IO, IOApp, Timer}
+import cats.effect.{Concurrent, ConcurrentEffect, ContextShift, ExitCode, IO, IOApp, Timer}
 import izumi.distage.model.definition.ModuleDef
 
 object CatsDIEffectModule extends CatsDIEffectModule
@@ -14,6 +14,11 @@ trait CatsDIEffectModule extends ModuleDef {
 
   make[ContextShift[IO]].from((_: PublicIOApp).contextShift)
   make[Timer[IO]].from((_: PublicIOApp).timer)
+
+  make[PublicIOApp].from {
+    new PublicIOApp { override def run(args: List[String]): IO[ExitCode] = IO.pure(ExitCode(0)) }
+  }
+
   include(PolymorphicCatsDIEffectModule[IO])
 }
 
