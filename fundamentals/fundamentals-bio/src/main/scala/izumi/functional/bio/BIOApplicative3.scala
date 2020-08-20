89dc35714d3032ac61f18e51a134ca00446f4c4f
Kai <450507+neko-kai@users.noreply.github.com>
2020-08-20 17:52:20 +0100

    Add BIOExit.map/.leftMap/.flatMap (#1190)

--- fundamentals/fundamentals-bio/src/main/scala/izumi/functional/bio/BIOApplicative3.scala
+++ fundamentals/fundamentals-bio/src/main/scala/izumi/functional/bio/BIOApplicative3.scala
@@ -1,5 +1,7 @@
 package izumi.functional.bio
 
+import scala.collection.immutable.Queue
+
 trait BIOApplicative3[F[-_, +_, +_]] extends BIOFunctor3[F] {
   def pure[A](a: A): F[Any, Nothing, A]
 
@@ -7,12 +9,16 @@ trait BIOApplicative3[F[-_, +_, +_]] extends BIOFunctor3[F] {
   def map2[R, E, A, B, C](firstOp: F[R, E, A], secondOp: => F[R, E, B])(f: (A, B) => C): F[R, E, C]
 
   /** execute two operations in order, return result of second operation */
-  def *>[R, E, A, B](firstOp: F[R, E, A], secondOp: => F[R, E, B]): F[R, E, B]
+  def *>[R, E, A, B](firstOp: F[R, E, A], secondOp: => F[R, E, B]): F[R, E, B] = map2(firstOp, secondOp)((_, b) => b)
 
   /** execute two operations in order, same as `*>`, but return result of first operation */
-  def <*[R, E, A, B](firstOp: F[R, E, A], secondOp: => F[R, E, B]): F[R, E, A]
+  def <*[R, E, A, B](firstOp: F[R, E, A], secondOp: => F[R, E, B]): F[R, E, A] = map2(firstOp, secondOp)((a, _) => a)
 
-  def traverse[R, E, A, B](l: Iterable[A])(f: A => F[R, E, B]): F[R, E, List[B]]
+  def traverse[R, E, A, B](l: Iterable[A])(f: A => F[R, E, B]): F[R, E, List[B]] = map(
+    l.foldLeft(pure(Queue.empty[B]): F[R, E, Queue[B]])(
+      (q, a) => map2(q, f(a))(_ :+ _)
+    )
+  )(_.toList)
 
   @inline final def forever[R, E, A](r: F[R, E, A]): F[R, E, Nothing] = *>(r, forever(r))
   def traverse_[R, E, A](l: Iterable[A])(f: A => F[R, E, Unit]): F[R, E, Unit] = void(traverse(l)(f))
--- fundamentals/fundamentals-bio/src/main/scala/izumi/functional/bio/BIOExit.scala
+++ fundamentals/fundamentals-bio/src/main/scala/izumi/functional/bio/BIOExit.scala
@@ -2,7 +2,11 @@ package izumi.functional.bio
 
 import zio.{Cause, Exit, FiberFailure}
 
-sealed trait BIOExit[+E, +A]
+sealed trait BIOExit[+E, +A] {
+  def map[B](f: A => B): BIOExit[E, B]
+  def leftMap[E1](f: E => E1): BIOExit[E1, A]
+  def flatMap[E1 >: E, B](f: A => BIOExit[E1, B]): BIOExit[E1, B]
+}
 
 object BIOExit {
 
@@ -17,14 +21,16 @@ object BIOExit {
       */
     def unsafeAttachTrace(conv: E => Throwable): Throwable
 
+    def map[E1](f: E => E1): Trace[E1]
+
     override final def toString: String = asString
   }
-
   object Trace {
     def empty: Trace[Nothing] = new Trace[Nothing] {
-      val asString: String = "<empty trace>"
-      def toThrowable: Throwable = new RuntimeException(asString)
-      def unsafeAttachTrace(conv: Nothing => Throwable): Throwable = toThrowable
+      override val asString: String = "<empty trace>"
+      override def toThrowable: Throwable = new RuntimeException(asString)
+      override def unsafeAttachTrace(conv: Nothing => Throwable): Throwable = toThrowable
+      override def map[E1](f: Nothing => E1): Trace[E1] = this
     }
 
     final case class ZIOTrace[+E](cause: Cause[E]) extends Trace[E] {
@@ -34,10 +40,15 @@ object BIOExit {
         case t: Throwable => t
         case e => conv(e)
       }
+      override def map[E1](f: E => E1): Trace[E1] = ZIOTrace(cause.map(f))
     }
   }
 
-  final case class Success[+A](value: A) extends BIOExit[Nothing, A]
+  final case class Success[+A](value: A) extends BIOExit[Nothing, A] {
+    override def map[B](f: A => B): Success[B] = Success(f(value))
+    override def leftMap[E1](f: Nothing => E1): this.type = this
+    override def flatMap[E1 >: Nothing, B](f: A => BIOExit[E1, B]): BIOExit[E1, B] = f(value)
+  }
 
   sealed trait Failure[+E] extends BIOExit[E, Nothing] {
     def trace: Trace[E]
@@ -48,24 +59,27 @@ object BIOExit {
     final def toThrowable(implicit ev: E <:< Throwable): Throwable = toEitherCompound.fold(identity, ev)
     final def toThrowable(conv: E => Throwable): Throwable = toEitherCompound.fold(identity, conv)
     final def unsafeAttachTrace(conv: E => Throwable): Throwable = trace.unsafeAttachTrace(conv)
+
+    override final def map[B](f: Nothing => B): this.type = this
+    override final def flatMap[E1 >: E, B](f: Nothing => BIOExit[E1, B]): this.type = this
   }
 
   final case class Error[+E](error: E, trace: Trace[E]) extends BIOExit.Failure[E] {
     override def toEither: Right[Nothing, E] = Right(error)
     override def toEitherCompound: Right[Nothing, E] = Right(error)
+    override def leftMap[E1](f: E => E1): BIOExit[E1, Nothing] = Error[E1](f(error), trace.map(f))
   }
 
   final case class Termination(compoundException: Throwable, allExceptions: List[Throwable], trace: Trace[Nothing]) extends BIOExit.Failure[Nothing] {
     override def toEither: Left[List[Throwable], Nothing] = Left(allExceptions)
     override def toEitherCompound: Left[Throwable, Nothing] = Left(compoundException)
+    override def leftMap[E1](f: Nothing => E1): this.type = this
   }
-
   object Termination {
     def apply(exception: Throwable, trace: Trace[Nothing]): Termination = new Termination(exception, List(exception), trace)
   }
 
   object ZIOExit {
-
     @inline def toBIOExit[E, A](result: Exit[E, A]): BIOExit[E, A] = result match {
       case Exit.Success(v) =>
         Success(v)
@@ -91,7 +105,18 @@ object BIOExit {
           Termination(compound, exceptions, Trace.ZIOTrace(cause))
       }
     }
+  }
 
+  implicit lazy val BIOMonadBIOExit: BIOMonad[BIOExit] = new BIOMonad[BIOExit] {
+    override final def pure[A](a: A): BIOExit[Nothing, A] = BIOExit.Success(a)
+    override final def map[R, E, A, B](r: BIOExit[E, A])(f: A => B): BIOExit[E, B] = r.map(f)
+    override final def flatMap[R, E, A, B](r: BIOExit[E, A])(f: A => BIOExit[E, B]): BIOExit[E, B] = r.flatMap(f)
+  }
+
+  implicit lazy val BIOBifunctorBIOExit: BIOBifunctor[BIOExit] = new BIOBifunctor[BIOExit] {
+    override final val InnerF: BIOFunctor[BIOExit] = BIOMonadBIOExit
+    override final def bimap[R, E, A, E2, A2](r: BIOExit[E, A])(f: E => E2, g: A => A2): BIOExit[E2, A2] = r.leftMap(f).map(g)
+    override final def leftMap[R, E, A, E2](r: BIOExit[E, A])(f: E => E2): BIOExit[E2, A] = r.leftMap(f)
   }
 
 }
