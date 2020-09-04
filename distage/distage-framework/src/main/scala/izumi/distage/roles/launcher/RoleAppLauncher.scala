89354427351bedc33355a573f79aa57812dbad14
Pavel Shirshov <pshirshov@gmail.com>
2020-09-04 21:35:39 +0100

    wip

--- distage/distage-framework/src/main/scala/izumi/distage/roles/launcher/RoleAppLauncher.scala
+++ distage/distage-framework/src/main/scala/izumi/distage/roles/launcher/RoleAppLauncher.scala
@@ -8,17 +8,3 @@ import izumi.fundamentals.platform.functional.Identity
 trait RoleAppLauncher[F[_]] {
   def launch(parameters: RawAppArgs): DIResourceBase[Identity, PreparedApp[F]]
 }
-
-//object RoleAppLauncher {
-//  abstract class LauncherF[F[_]: TagK: LiftIO](executionContext: ExecutionContext = ExecutionContext.global) extends RoleAppLauncherImpl[F] {
-//    override protected val shutdownStrategy: AppShutdownStrategy[F] = new CatsEffectIOShutdownStrategy(executionContext)
-//  }
-//
-//  abstract class LauncherBIO[F[+_, +_]: TagKK: BIOAsync] extends RoleAppLauncherImpl[F[Throwable, *]] {
-//    override protected val shutdownStrategy: AppShutdownStrategy[F[Throwable, *]] = new BIOShutdownStrategy[F]
-//  }
-//
-//  abstract class LauncherIdentity extends RoleAppLauncherImpl[Identity] {
-//    override protected val shutdownStrategy: AppShutdownStrategy[Identity] = new JvmExitHookLatchShutdownStrategy
-//  }
-//}
