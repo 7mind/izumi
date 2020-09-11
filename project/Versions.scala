7e7a0762cea00b13d1913aa43dec7de7c7706685
Scala Steward <me@scala-steward.org>
2020-09-11 08:02:06 +0200

    Update scala-collection-compat to 2.2.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -3,7 +3,7 @@ object V {
   val izumi_reflect = "1.0.0-M7"
 
   // foundation
-  val collection_compat = "2.1.6"
+  val collection_compat = "2.2.0"
 
   val kind_projector = "0.11.0"
   val silencer = "1.7.1"
