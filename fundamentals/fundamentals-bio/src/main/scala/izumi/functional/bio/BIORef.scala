a8089da7a2ddf9cda4802ce1b276a24b7f1f403b
Kai <kai@7mind.io>
2020-09-21 19:45:37 +0100

    Implement `BIOPrimitives` for `monix-bio`

--- fundamentals/fundamentals-bio/src/main/scala/izumi/functional/bio/BIORef.scala
+++ fundamentals/fundamentals-bio/src/main/scala/izumi/functional/bio/BIORef.scala
@@ -21,4 +21,13 @@ object BIORef {
       override def update(f: A => A): IO[Nothing, A] = ref.updateAndGet(f)
       override def update_(f: A => A): IO[Nothing, Unit] = ref.update(f)
     }
+
+  def fromCats[F[+_, +_]: BIOPanic, A](ref: cats.effect.concurrent.Ref[F[Throwable, ?], A]): BIORef[F, A] =
+    new BIORef[F, A] {
+      override def get: F[Nothing, A] = ref.get.orTerminate
+      override def set(a: A): F[Nothing, Unit] = ref.set(a).orTerminate
+      override def modify[B](f: A => (B, A)): F[Nothing, B] = ref.modify(f(_).swap).orTerminate
+      override def update(f: A => A): F[Nothing, A] = ref.updateAndGet(f).orTerminate
+      override def update_(f: A => A): F[Nothing, Unit] = ref.update(f).orTerminate
+    }
 }
