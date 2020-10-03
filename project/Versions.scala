9f5b0fc644337974d08471ede63435f64911bb4b
Scala Steward <me@scala-steward.org>
2020-10-03 22:03:25 +0200

    Update discipline-scalatest to 2.0.1

--- project/Versions.scala
+++ project/Versions.scala
@@ -15,7 +15,7 @@ object V {
   val cats_effect = "2.2.0"
 
   val discipline = "1.0.3"
-  val discipline_scalatest = "2.0.0"
+  val discipline_scalatest = "2.0.1"
 
   val zio = "1.0.1"
   val zio_interop_cats = "2.1.4.0"
