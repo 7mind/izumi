5cf965d0e5a7a46fe00e971e5442c6c6dfb77bd9
VladPodilnyk <vlad.podilnyk@gmail.com>
2020-10-03 20:46:26 +0300

    apply suggestions.

--- fundamentals/fundamentals-bio/.jvm/src/test/scala/izumi/functional/bio/test/BioZioLawsTest.scala
+++ fundamentals/fundamentals-bio/.jvm/src/test/scala/izumi/functional/bio/test/BioZioLawsTest.scala
@@ -3,15 +3,14 @@ package izumi.functional.bio.test
 import cats.effect.laws.ConcurrentLaws
 import cats.effect.laws.discipline.ConcurrentTests
 import cats.effect.{Concurrent, ContextShift}
-import izumi.functional.bio.catz._
+import izumi.functional.bio.catz.BIOAsyncForkToConcurrent
 import izumi.functional.bio.env.ZIOTestEnv
 
 class BioZioLawsTest extends CatsLawsTestBase with ZIOTestEnv {
   val concurrentTestZio = new ConcurrentTests[zio.Task] {
     val laws = new ConcurrentLaws[zio.Task] {
-      val F = Concurrent[zio.Task]
-      import zio.interop.catz.zioContextShift
-      val contextShift = ContextShift[zio.Task]
+      val F = Concurrent[zio.Task](BIOAsyncForkToConcurrent)
+      val contextShift = ContextShift[zio.Task](zio.interop.catz.zioContextShift)
     }
   }
 
