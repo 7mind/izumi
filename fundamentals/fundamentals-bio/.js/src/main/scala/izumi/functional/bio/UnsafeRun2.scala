8b746e1b2902a68e0e0cecce7bbe999a0824a49a
Kai <kai@7mind.io>
2020-10-06 01:00:38 +0100

    fix js build

--- fundamentals/fundamentals-bio/.js/src/main/scala/izumi/functional/bio/UnsafeRun2.scala
+++ fundamentals/fundamentals-bio/.js/src/main/scala/izumi/functional/bio/UnsafeRun2.scala
@@ -1,10 +1,10 @@
 package izumi.functional.bio
 
-/** Scala.js does not support UnsafeRun2 */
+/** Scala.js does not support UnsafeRun */
 trait UnsafeRun2[F[_, _]]
 
 object UnsafeRun2 {
   @inline def apply[F[_, _]](implicit ev: UnsafeRun2[F]): UnsafeRun2[F] = ev
 
-  implicit def anyBIORunner[F[_, _]]: UnsafeRun2[F] = new UnsafeRun2[F] {}
+  implicit def anyUnsafeRun2[F[_, _]]: UnsafeRun2[F] = new UnsafeRun2[F] {}
 }
