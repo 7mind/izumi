63ccb7e1ff129dc64512a8d4b6da4822288fc8e2
Alex Liubymov <arowcap@gmail.com>
2020-10-10 18:10:10 +0300

    add bracket case

--- fundamentals/fundamentals-bio/src/main/scala/izumi/functional/bio/data/FreeBracket.scala
+++ fundamentals/fundamentals-bio/src/main/scala/izumi/functional/bio/data/FreeBracket.scala
@@ -14,7 +14,9 @@ sealed trait FreeBracket[S[_, _], +E, +A] {
   @inline final def catchAll[A1 >: A, E1](err: E => FreeBracket[S, E1, A1]): FreeBracket[S, E1, A1] = FreeBracket.Redeem(this, err, (a: A) => FreeBracket.pure(a))
   @inline final def guarantee(g: FreeBracket[S, Nothing, Unit]): FreeBracket[S, E, A] = FreeBracket.Guarantee[S, E, A](this, g)
   @inline final def bracket[B, E1 >: E](release: A => FreeBracket[S, Nothing, Unit])(use: A => FreeBracket[S, E1, B]): FreeBracket[S, E1, B] =
-    FreeBracket.Bracket(this, release, use)
+    FreeBracket.BracketCase(this, (a: A, _: BIOExit[E1, B]) => release(a), use)
+  @inline final def bracketCase[B, E1 >: E](release: (A, BIOExit[E1, B]) => FreeBracket[S, Nothing, Unit])(use: A => FreeBracket[S, E1, B]): FreeBracket[S, E1, B] =
+    FreeBracket.BracketCase(this, release, use)
 
   @inline final def void: FreeBracket[S, E, Unit] = map(_ => ())
 
@@ -31,15 +33,13 @@ sealed trait FreeBracket[S[_, _], +E, +A] {
           )
       case s: FreeBracket.Sandbox[S, _, _] =>
         s.sub.foldMap(transform).sandboxBIOExit.map(_.asInstanceOf[A])
-
-      case FreeBracket.Bracket(acquire, release, use) =>
-        acquire.foldMap(transform).bracket(a => release(a).foldMap(transform))(a => use(a).foldMap(transform))
+      case FreeBracket.BracketCase(acquire, release, use) =>
+        acquire.foldMap(transform).bracketCase((a, e: BIOExit[E, A]) => release(a, e).foldMap(transform))(a => use(a).foldMap(transform))
       case FreeBracket.FlatMap(sub, cont) =>
         sub match {
           case FreeBracket.FlatMap(sub2, cont2) => sub2.flatMap(a => cont2(a).flatMap(cont)).foldMap(transform)
           case another => another.foldMap(transform).flatMap(cont(_).foldMap(transform))
         }
-
     }
   }
 }
@@ -52,7 +52,7 @@ object FreeBracket {
   )(release: A0 => FreeBracket[S, Nothing, Unit]
   )(use: A0 => FreeBracket[S, E, A]
   ): FreeBracket[S, E, A] = {
-    Bracket(acquire, release, use)
+    BracketCase(acquire, (a: A0, _: BIOExit[E, A]) => release(a), use)
   }
 
   private[data] final case class Pure[S[_, _], A](a: A) extends FreeBracket[S, Nothing, A]
@@ -65,9 +65,9 @@ object FreeBracket {
   private[data] final case class Redeem[S[_, _], E, E1, A, B](sub: FreeBracket[S, E, A], err: E => FreeBracket[S, E1, B], suc: A => FreeBracket[S, E1, B])
     extends FreeBracket[S, E1, B]
 
-  private[data] final case class Bracket[S[_, _], E, A0, A](
+  private[data] final case class BracketCase[S[_, _], E, A0, A](
     acquire: FreeBracket[S, E, A0],
-    release: A0 => FreeBracket[S, Nothing, Unit],
+    release: (A0, BIOExit[E, A]) => FreeBracket[S, Nothing, Unit],
     use: A0 => FreeBracket[S, E, A],
   ) extends FreeBracket[S, E, A] {
     override def toString: String = s"${this.getClass.getName}[bracket=${use.getClass.getSimpleName}]"
