29f1f461f3e0c2b904ede76f1b10c7f83c740210
Kai <kai@7mind.io>
2020-10-14 20:05:20 +0100

    Allow Activation to be parameterized by AxisValues alone

--- distage/distage-core-api/src/main/scala/izumi/distage/model/definition/Activation.scala
+++ distage/distage-core-api/src/main/scala/izumi/distage/model/definition/Activation.scala
@@ -5,13 +5,16 @@ import izumi.distage.model.definition.Axis.AxisValue
 final case class Activation(activeChoices: Map[Axis, AxisValue]) extends AnyVal {
   @inline def ++(activation: Activation): Activation = Activation(activeChoices ++ activation.activeChoices)
   @inline def +(axisChoice: (Axis, AxisValue)): Activation = Activation(activeChoices + axisChoice)
+  @inline def +(axisValue: AxisValue): Activation = Activation(activeChoices + (axisValue.axis -> axisValue))
 
-  /** `that` activation is larger than `this` activation if all axis choices in `this` are present in `that` */
+  /** `that` activation subsumes `this` activation if all axis choices in `this` are present in `that` */
   def <=(that: Activation): Boolean = activeChoices.toSet.subsetOf(that.activeChoices.toSet)
+  @inline def subsetOf(that: Activation): Boolean = this <= that
 }
 
 object Activation {
-  def apply(activeChoices: (Axis, AxisValue)*): Activation = Activation(Map(activeChoices: _*))
+  def apply(activeChoices: (Axis, AxisValue)*): Activation = Activation(activeChoices.toMap)
+  def apply(activeChoices: AxisValue*)(implicit d: DummyImplicit): Activation = Activation(activeChoices.iterator.map(v => v.axis -> v).toMap)
   def apply(): Activation = empty
-  def empty: Activation = new Activation(Map.empty)
+  def empty: Activation = Activation(Map.empty[Axis, AxisValue])
 }
