65df648ed6f83f179af32040e51096547b85a955
Nikita Vilunov <nikitaoryol@gmail.com>
2020-11-04 22:58:51 +0300

    Add a docker container config flag to control explicit image pulling (#1300)

--- distage/distage-framework-docker/src/main/scala/izumi/distage/docker/Docker.scala
+++ distage/distage-framework-docker/src/main/scala/izumi/distage/docker/Docker.scala
@@ -109,6 +109,9 @@ object Docker {
     * @param cwd      Working directory to use inside the docker container
     *
     * @param mounts   Host paths mounted to Volumes inside the docker container
+    *
+    * @param alwaysPull Pull the image before starting the container
+    *                   default: true
     */
   final case class ContainerConfig[T](
     image: String,
@@ -128,6 +131,7 @@ object Docker {
     pullTimeout: FiniteDuration = FiniteDuration(120, TimeUnit.SECONDS),
     healthCheck: ContainerHealthCheck[T] = ContainerHealthCheck.portCheck[T],
     portProbeTimeout: FiniteDuration = FiniteDuration(200, TimeUnit.MILLISECONDS),
+    alwaysPull: Boolean = true,
   ) {
     def tcpPorts: Set[DockerPort] = ports.collect { case t: DockerPort.TCPBase => t: DockerPort }.toSet
     def udpPorts: Set[DockerPort] = ports.collect { case t: DockerPort.UDPBase => t: DockerPort }.toSet
