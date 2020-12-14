fa7e4a7a49c4017185e16c164a9ae12de115a514
Kai <kai@7mind.io>
2020-12-14 07:30:04 +0000

    Scala-steward version updates

--- project/Versions.scala
+++ project/Versions.scala
@@ -30,18 +30,21 @@ object V {
   val magnolia = "0.17.0"
   val jawn = "1.0.0"
 
-  val doobie = "0.9.4"
-
   // good to drop - scala
   val scala_java_time = "2.0.0-RC5"
-  val scalamock = "5.0.0"
 
   // java-only dependencies below
-  val classgraph = "4.8.93"
+  val classgraph = "4.8.94"
   val slf4j = "1.7.30"
   val typesafe_config = "1.4.0"
 
   // good to drop - java
   val cglib_nodep = "3.3.0"
-  val docker_java = "3.2.6"
+  val docker_java = "3.2.7"
+
+  // microsite-only
+  val doobie = "0.9.4"
+
+  // test-only
+  val scalamock = "5.1.0"
 }
