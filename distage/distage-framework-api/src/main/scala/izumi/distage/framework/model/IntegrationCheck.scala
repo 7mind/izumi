970996ef333ca2ad4a7a8c536fa400bdfc34bfa4
Kai <450507+neko-kai@users.noreply.github.com>
2021-01-15 14:25:23 +0000

    Make `IntegrationCheck[F]` covariant in F to allow effect subtypes same as in roles and resources (#1367)
    * Make `IntegrationCheck[F]` covariant in F to allow effect subtypes same as in roles and resources
    
    * return `assume`

--- distage/distage-framework-api/src/main/scala/izumi/distage/framework/model/IntegrationCheck.scala
+++ distage/distage-framework-api/src/main/scala/izumi/distage/framework/model/IntegrationCheck.scala
@@ -8,7 +8,7 @@ import izumi.fundamentals.platform.integration.ResourceCheck
   * startup should stop. When used with [[izumi.distage.testkit.scalatest.DistageSpecScalatest]],
   * if `resourcesAvailable()` method returns a [[izumi.fundamentals.platform.integration.ResourceCheck.Failure]] the test will be skipped.
   */
-trait IntegrationCheck[F[_]] {
+trait IntegrationCheck[+F[_]] {
   /** This method must never throw, on error return [[izumi.fundamentals.platform.integration.ResourceCheck.Failure]] instead * */
   def resourcesAvailable(): F[ResourceCheck]
 }
