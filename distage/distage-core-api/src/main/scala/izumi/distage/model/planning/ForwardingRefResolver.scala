1cbfe10a9a82bb771df3eaf98c8f0ceb7d4fbd25
Pavel Shirshov <pshirshov@gmail.com>
2021-04-07 18:06:05 +0100

    cleanups

--- distage/distage-core-api/src/main/scala/izumi/distage/model/planning/ForwardingRefResolver.scala
+++ distage/distage-core-api/src/main/scala/izumi/distage/model/planning/ForwardingRefResolver.scala
@@ -1,6 +1,6 @@
 package izumi.distage.model.planning
 
-import izumi.distage.model.definition.errors.LoopResolutionError
+import izumi.distage.model.definition.errors.DIError.LoopResolutionError
 import izumi.distage.model.plan.ExecutableOp
 import izumi.distage.model.reflection.DIKey
 import izumi.fundamentals.graphs.DG
