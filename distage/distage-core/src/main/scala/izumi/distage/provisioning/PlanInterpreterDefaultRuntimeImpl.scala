e8362ee0ccfa39e03c1e547d437bcd49e018e94f
Pavel Shirshov <pshirshov@gmail.com>
2021-04-14 15:51:57 +0100

    AbstractPlan removed, OrderedPlan turned into implementation detail of the sequential interpreter

--- distage/distage-core/src/main/scala/izumi/distage/provisioning/PlanInterpreterDefaultRuntimeImpl.scala
+++ distage/distage-core/src/main/scala/izumi/distage/provisioning/PlanInterpreterDefaultRuntimeImpl.scala
@@ -30,6 +30,40 @@ import scala.collection.mutable.ArrayBuffer
 import scala.concurrent.duration.Duration
 import scala.util.{Failure, Success, Try}
 
+object PlanInterpreterDefaultRuntimeImpl {
+  import izumi.distage.model.plan.repr._
+  import izumi.distage.model.plan.topology.PlanTopology
+  import izumi.distage.model.reflection._
+  import izumi.fundamentals.platform.strings.IzString.toRichString
+
+  /**
+    * Linearized graph which is ready to be consumed by linear executors
+    *
+    * May contain cyclic dependencies resolved with proxies
+    */
+  @deprecated("should be removed once we finish transition to parallel interpreter", "13/04/2021")
+  final case class OrderedPlan(
+    steps: Vector[ExecutableOp],
+    declaredRoots: Set[DIKey],
+    topology: PlanTopology,
+  ) {
+    def keys: Set[DIKey] = {
+      steps.map(_.target).toSet
+    }
+
+    /** Print while omitting package names for unambiguous types */
+    override def toString: String = {
+      val minimizer = KeyMinimizer(this.keys, DIRendering.colorsEnabled)
+      val tf = TypeFormatter.minimized(minimizer)
+      val kf = KeyFormatter.minimized(minimizer)
+      val opFormatter = OpFormatter(kf, tf, DIRendering.colorsEnabled)
+
+      this.steps.map(opFormatter.format).mkString("\n").listing()
+    }
+  }
+
+}
+
 // TODO: add introspection capabilities
 class PlanInterpreterDefaultRuntimeImpl(
   setStrategy: SetStrategy,
@@ -46,6 +80,8 @@ class PlanInterpreterDefaultRuntimeImpl(
 ) extends PlanInterpreter
   with OperationExecutor {
   import scala.collection.compat._
+
+  import PlanInterpreterDefaultRuntimeImpl._
 
   type OperationMetadata = Long
 
