238e4d284900fcdf1342cf242b21405648697b78
Alex Liubymov <arowcap@gmail.com>
2021-05-06 17:47:10 +0300

    Feature: di-docker bugfixes (#1478)
    * handling of integration exception in distage test run for induvidual tests; fallback to default localhost for ServiceHost.local
    
    * remove debug error

--- distage/distage-framework-api/src/main/scala/izumi/distage/framework/model/exceptions/IntegrationCheckException.scala
+++ distage/distage-framework-api/src/main/scala/izumi/distage/framework/model/exceptions/IntegrationCheckException.scala
@@ -8,4 +8,6 @@ import izumi.fundamentals.platform.strings.IzString._
 class IntegrationCheckException(val failures: NonEmptyList[ResourceCheck.Failure])
   extends DIException(
     s"""Integration check failed, failures were: ${failures.toList.niceList()}""".stripMargin
-  )
+  ) {
+  def this(failure: ResourceCheck.Failure) = this(NonEmptyList(failure))
+}
