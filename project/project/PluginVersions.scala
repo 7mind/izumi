dae1a6945ee57eaa5aa90d663e0f13276aee3e0c
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-05-11 09:56:10 +0200

    Update sbt-scoverage to 1.8.0 (#1481)

--- project/project/PluginVersions.scala
+++ project/project/PluginVersions.scala
@@ -4,7 +4,7 @@ object PV {
   val sbt_ghpages = "0.6.3"
   val sbt_site = "1.3.3"
   val sbt_unidoc = "0.4.3"
-  val sbt_scoverage = "1.7.3"
+  val sbt_scoverage = "1.8.0"
   val sbt_pgp = "2.1.1"
   val sbt_assembly = "0.14.9"
 
