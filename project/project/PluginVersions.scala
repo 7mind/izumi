f8b6b86bb14c0d67ddf0646a7b7223ece6689110
Kai <kai@7mind.io>
2021-05-29 00:40:46 +0100

    Update to Scala.js 1.5.1

--- project/project/PluginVersions.scala
+++ project/project/PluginVersions.scala
@@ -8,7 +8,7 @@ object PV {
   val sbt_pgp = "2.1.1"
   val sbt_assembly = "0.14.9"
 
-  val scala_js_version = "1.5.0"
+  val scala_js_version = "1.5.1"
   val crossproject_version = "1.0.0"
   val scalajs_bundler_version = "0.20.0"
 }
