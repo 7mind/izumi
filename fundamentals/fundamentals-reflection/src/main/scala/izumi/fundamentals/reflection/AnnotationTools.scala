4f9068dc16694e874c687686a8bba2261e02c159
Kai <kai@7mind.io>
2021-05-29 01:24:04 +0100

    Fix regression introduced in `Return older implementations without FatalError`

--- fundamentals/fundamentals-reflection/src/main/scala/izumi/fundamentals/reflection/AnnotationTools.scala
+++ fundamentals/fundamentals-reflection/src/main/scala/izumi/fundamentals/reflection/AnnotationTools.scala
@@ -1,20 +1,25 @@
 package izumi.fundamentals.reflection
 
+import izumi.fundamentals.reflection.ReflectionUtil.stripByName
+
+import scala.annotation.nowarn
 import scala.reflect.api.Universe
 
 object AnnotationTools {
 
   def getAllAnnotations(u: Universe)(symb: u.Symbol): List[u.Annotation] = {
-    symb.annotations ++ getAllTypeAnnotations[u.type](symb.typeSignature)
+    symb.annotations ++ getAllTypeAnnotations(u)(symb.typeSignature)
   }
 
-  def getAllTypeAnnotations[U <: Universe with Singleton](typ: U#Type): List[U#Annotation] = {
-    typ.finalResultType.dealias match {
-      case t: U#AnnotatedTypeApi =>
+  def getAllTypeAnnotations(u: Universe)(typ: u.Type): List[u.Annotation] = {
+    @nowarn("msg=outer reference")
+    val out = stripByName(u)(typ.finalResultType.dealias) match {
+      case t: u.AnnotatedTypeApi =>
         t.annotations
       case _ =>
         Nil
     }
+    out
   }
 
   def annotationTypeEq(u: Universe)(tpe: u.Type, ann: u.Annotation): Boolean = {
