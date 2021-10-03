9fbdbc9ff0e3bddac5480263756c6695b59815cb
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-10-03 16:14:57 +0200

    Update zio to 1.0.12 (#1596)

--- project/Versions.scala
+++ project/Versions.scala
@@ -15,7 +15,7 @@ object V {
   val discipline = "1.1.5"
   val discipline_scalatest = "2.0.0"
 
-  val zio = "1.0.11"
+  val zio = "1.0.12"
   val zio_interop_cats = "2.5.1.0"
 
   val monix = "3.4.0"
