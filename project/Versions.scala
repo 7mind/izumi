fcc4c6ee8c45c4757f71fd28710469196d7ffb81
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-11-26 12:58:54 +0100

    Update classgraph to 4.8.136 (#1651)

--- project/Versions.scala
+++ project/Versions.scala
@@ -31,7 +31,7 @@ object V {
   val scala_java_time = "2.0.0"
 
   // java-only dependencies below
-  val classgraph = "4.8.135"
+  val classgraph = "4.8.136"
   val slf4j = "1.7.32"
   val typesafe_config = "1.4.0"
 
