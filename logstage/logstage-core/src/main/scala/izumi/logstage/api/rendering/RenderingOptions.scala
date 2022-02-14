1c106dcd7d275ef4e54dc826e7aa72ff2bcf3df3
Kai <450507+neko-kai@users.noreply.github.com>
2022-02-14 16:00:13 +0000

    Fix regression in logstage config parsing from #980 - allow partial logstage config in HOCON again

--- logstage/logstage-core/src/main/scala/izumi/logstage/api/rendering/RenderingOptions.scala
+++ logstage/logstage-core/src/main/scala/izumi/logstage/api/rendering/RenderingOptions.scala
@@ -5,12 +5,12 @@ package izumi.logstage.api.rendering
   * @param colored        if `true`, use colors in console output
   */
 final case class RenderingOptions(
-  withExceptions: Boolean,
-  colored: Boolean,
-  hideKeys: Boolean,
+  withExceptions: Boolean = true,
+  colored: Boolean = true,
+  hideKeys: Boolean = false,
 )
 object RenderingOptions {
-  def default: RenderingOptions = RenderingOptions(withExceptions = true, colored = true, hideKeys = false)
-  def colorless: RenderingOptions = RenderingOptions(withExceptions = true, colored = false, hideKeys = false)
-  def simple: RenderingOptions = RenderingOptions(withExceptions = false, colored = false, hideKeys = false)
+  def default: RenderingOptions = RenderingOptions() // NOTE: Use this method in code – the default parameters in constructor are only there for pureconfig derivation
+  def colorless: RenderingOptions = RenderingOptions(colored = false)
+  def simple: RenderingOptions = RenderingOptions(withExceptions = false, colored = false)
 }
