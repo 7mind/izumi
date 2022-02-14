4fe2152e7a0fabdbca83d7d4472fcc69b2d0cae6
Scala Steward <me@scala-steward.org>
2022-02-14 02:02:09 +0100

    Update zio-interop-cats to 3.2.9.1

--- project/Versions.scala
+++ project/Versions.scala
@@ -16,7 +16,7 @@ object V {
   val discipline_scalatest = "2.0.0"
 
   val zio = "1.0.13"
-  val zio_interop_cats = "2.5.1.0"
+  val zio_interop_cats = "3.2.9.1"
 
   val monix = "3.4.0"
   val monix_bio = "1.2.0"
