e98d1455bf01f90eb227ee92dc36e066d074b90c
Kai <450507+neko-kai@users.noreply.github.com>
2022-02-17 19:51:01 +0000

    in SchedulerZio use Clock.Service instead of Clock

--- fundamentals/fundamentals-bio/src/main/scala/izumi/functional/bio/retry/Scheduler2.scala
+++ fundamentals/fundamentals-bio/src/main/scala/izumi/functional/bio/retry/Scheduler2.scala
@@ -1,10 +1,11 @@
 package izumi.functional.bio.retry
 
+import izumi.functional.bio.PredefinedHelper
 import izumi.functional.bio.PredefinedHelper.Predefined
 import izumi.functional.bio.impl.SchedulerZio
 import zio.ZIO
 
-trait Scheduler2[F[+_, +_]] extends SchedulerInstances {
+trait Scheduler2[F[+_, +_]] extends SchedulerInstances with PredefinedHelper {
   def repeat[E, A, B](eff: F[E, A])(policy: RetryPolicy[F, A, B]): F[E, A]
   def retry[E, E1 >: E, S, A](eff: F[E, A])(policy: RetryPolicy[F, E1, S]): F[E, A]
   def retryOrElse[E, E1 >: E, E2, A, A2 >: A, S](eff: F[E, A])(policy: RetryPolicy[F, E1, S])(orElse: E1 => F[E2, A2]): F[E2, A2]
@@ -17,7 +18,7 @@ object Scheduler2 {
 private[bio] sealed trait SchedulerInstances
 object SchedulerInstances extends SchedulerLowPriorityInstances {
   @inline implicit def SchedulerFromZio(implicit clockService: zio.clock.Clock): Predefined.Of[Scheduler3[ZIO]] =
-    new SchedulerZio(clockService).asInstanceOf[Predefined.Of[Scheduler3[ZIO]]]
+    Predefined(new SchedulerZio(clockService.get[zio.clock.Clock.Service]))
 }
 
 sealed trait SchedulerLowPriorityInstances {
@@ -25,6 +26,6 @@ sealed trait SchedulerLowPriorityInstances {
 //    implicit
 //    timer: Timer[MonixBIO[Nothing, _]]
 //  ): Predefined.Of[Scheduler2[MonixBIO]] = {
-//    new SchedulerMonix(timer.asInstanceOf[cats.effect.kernel.Clock[monix.bio.UIO]]).asInstanceOf[Predefined.Of[Scheduler2[MonixBIO]]]
+//    Predefined(new SchedulerMonix(timer.asInstanceOf[cats.effect.kernel.Clock[monix.bio.UIO]]))
 //  }
 }
