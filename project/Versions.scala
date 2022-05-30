22240ccc3893dde0608a8a221681bf99a10c02b8
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-05-30 23:48:58 +0200

    Update circe-core, circe-literal, ... to 0.14.2 (#1735)

--- project/Versions.scala
+++ project/Versions.scala
@@ -21,7 +21,7 @@ object V {
   val monix = "3.4.0"
   val monix_bio = "1.2.0"
 
-  val circe = "0.14.1"
+  val circe = "0.14.2"
   val circe_derivation = "0.13.0-M5"
   val pureconfig = "0.17.1"
   val magnolia = "0.17.0"
