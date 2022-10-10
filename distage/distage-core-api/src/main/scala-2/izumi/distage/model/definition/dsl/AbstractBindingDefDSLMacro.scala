ff6b1662e5a9edd25d9cd9e795adc8bcab67b123
Pavel Shirshov <pshirshov@eml.cc>
2022-10-10 14:49:05 +0100

    makeFactory/wip

--- distage/distage-core-api/src/main/scala-2/izumi/distage/model/definition/dsl/AbstractBindingDefDSLMacro.scala
+++ distage/distage-core-api/src/main/scala-2/izumi/distage/model/definition/dsl/AbstractBindingDefDSLMacro.scala
@@ -3,6 +3,24 @@ package izumi.distage.model.definition.dsl
 import izumi.distage.constructors.macros.AnyConstructorMacro
 import scala.language.experimental.macros
 
-trait AbstractBindingDefDSLMacro[BindDSL[_], BindDSLAfterFrom[_], SetDSL[_]] {
+import izumi.distage.constructors.FactoryConstructor
+import izumi.fundamentals.platform.language.CodePositionMaterializer
+import izumi.reflect.Tag
+import scala.reflect.macros.blackbox
+
+trait AbstractBindingDefDSLMacro[BindDSL[_], BindDSLAfterFrom[_], SetDSL[_]] { this: AbstractBindingDefDSL[BindDSL, BindDSLAfterFrom, SetDSL] =>
+
   final protected[this] def make[T]: BindDSL[T] = macro AnyConstructorMacro.make[BindDSL, T]
+
+  final protected[this] def makeFactory[T](implicit tag: Tag[T], pos: CodePositionMaterializer): BindDSLAfterFrom[T] = macro
+    AbstractBindingDefDSLMacroImpl.makeFactory[BindDSLAfterFrom, T]
+}
+
+object AbstractBindingDefDSLMacroImpl {
+
+  def makeFactory[B[_], T: c.WeakTypeTag](c: blackbox.Context)(tag: c.Expr[Tag[T]], pos: c.Expr[CodePositionMaterializer]): c.Expr[B[T]] = {
+    import c.universe._
+    c.Expr[B[T]](q"""${c.prefix}._makeSimple[${weakTypeOf[T]}](${c.inferImplicitValue(weakTypeOf[FactoryConstructor[T]], silent = false)}.provider)($tag, $pos)""")
+  }
+
 }
