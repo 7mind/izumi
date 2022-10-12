dada90ab332a00d65aa062c440ff3ddfe8f1a46d
Pavel Shirshov <pshirshov@eml.cc>
2022-10-12 16:23:35 +0100

    cleanups

--- distage/distage-core-api/src/main/scala-2/izumi/distage/model/definition/dsl/AbstractBindingDefDSLMacro.scala
+++ distage/distage-core-api/src/main/scala-2/izumi/distage/model/definition/dsl/AbstractBindingDefDSLMacro.scala
@@ -1,47 +1,9 @@
 package izumi.distage.model.definition.dsl
 
-import izumi.distage.constructors.FactoryConstructor
 import izumi.distage.constructors.macros.AnyConstructorMacro
-import izumi.distage.model.providers.Functoid
-import izumi.fundamentals.platform.language.CodePositionMaterializer
-import izumi.reflect.Tag
 
 import scala.language.experimental.macros
-import scala.reflect.macros.blackbox
 
 trait AbstractBindingDefDSLMacro[BindDSL[_], BindDSLAfterFrom[_], SetDSL[_]] { this: AbstractBindingDefDSL[BindDSL, BindDSLAfterFrom, SetDSL] =>
-  import AbstractBindingDefDSLMacroImpl.*
-
   final protected[this] def make[T]: BindDSL[T] = macro AnyConstructorMacro.make[BindDSL, T]
-
-  final protected[this] def makeFactory[T](implicit tag: Tag[T], pos: CodePositionMaterializer): BindDSLAfterFrom[T] = macro makeFactoryOut[BindDSLAfterFrom, T]
-}
-
-trait AbstractModuleDefDSLMacro[T, AfterBind] { this: ModuleDefDSL.MakeDSLBase[T, AfterBind] =>
-  import AbstractBindingDefDSLMacroImpl.*
-  final def fromFactory[I <: T]: AfterBind = macro makeFactoryImpl[AfterBind, I]
-}
-
-object AbstractBindingDefDSLMacroImpl {
-  def makeFactoryProvider[T: c.WeakTypeTag](c: blackbox.Context): c.Expr[Functoid[T]] = {
-    import c.universe.*
-    c.Expr[Functoid[T]](q"""${c.inferImplicitValue(weakTypeOf[FactoryConstructor[T]], silent = false)}.provider""")
-  }
-
-  def makeFactoryOut[B[_], T: c.WeakTypeTag](c: blackbox.Context)(tag: c.Expr[Tag[T]], pos: c.Expr[CodePositionMaterializer]): c.Expr[B[T]] = {
-    import c.universe.*
-    c.Expr[B[T]](q"""${c.prefix}._makeSimple[${weakTypeOf[T]}](${makeFactoryProvider[T](c)})($tag, $pos)""")
-  }
-
-  def makeFactoryImpl[B, T: c.WeakTypeTag](c: blackbox.Context): c.Expr[B] = {
-    import c.universe.*
-
-    c.Expr[B](q"""
-              ${c.prefix}.bind(
-                _root_.izumi.distage.model.definition.ImplDef.ProviderImpl(
-                  _root_.izumi.distage.model.reflection.SafeType.get[${weakTypeOf[T]}], 
-                  ${makeFactoryProvider[T](c)}.get
-                )
-              )""")
-  }
 }
--- distage/distage-core-api/src/main/scala/izumi/distage/model/definition/dsl/ModuleDefDSL.scala
+++ distage/distage-core-api/src/main/scala/izumi/distage/model/definition/dsl/ModuleDefDSL.scala
@@ -1,12 +1,12 @@
 package izumi.distage.model.definition.dsl
 
-import izumi.distage.constructors.{AnyConstructor, HasConstructor}
+import izumi.distage.constructors.{AnyConstructor, FactoryConstructor, HasConstructor}
 import izumi.distage.model.definition.Lifecycle.{LifecycleTag, TrifunctorHasLifecycleTag}
-import izumi.distage.model.definition._
+import izumi.distage.model.definition.*
 import izumi.distage.model.definition.dsl.AbstractBindingDefDSL.MultiSetElementInstruction.MultiAddTags
 import izumi.distage.model.definition.dsl.AbstractBindingDefDSL.SetElementInstruction.ElementAddTags
-import izumi.distage.model.definition.dsl.AbstractBindingDefDSL.SingletonInstruction._
-import izumi.distage.model.definition.dsl.AbstractBindingDefDSL.{SetInstruction, SingletonInstruction, _}
+import izumi.distage.model.definition.dsl.AbstractBindingDefDSL.SingletonInstruction.*
+import izumi.distage.model.definition.dsl.AbstractBindingDefDSL.{SetInstruction, SingletonInstruction, *}
 import izumi.distage.model.definition.dsl.ModuleDefDSL.{MakeDSL, MakeDSLUnnamedAfterFrom, SetDSL}
 import izumi.distage.model.providers.Functoid
 import izumi.distage.model.reflection.{DIKey, SafeType}
@@ -14,7 +14,7 @@ import izumi.functional.bio.Local3
 import izumi.functional.bio.data.Morphism1
 import izumi.fundamentals.platform.language.CodePositionMaterializer
 import izumi.reflect.{Tag, TagK, TagK3}
-import zio._
+import zio.*
 
 import scala.collection.immutable.HashSet
 
@@ -101,7 +101,7 @@ trait ModuleDefDSL extends AbstractBindingDefDSL[MakeDSL, MakeDSLUnnamedAfterFro
 
 object ModuleDefDSL {
 
-  trait MakeDSLBase[T, AfterBind] extends AnyKindShim with AbstractModuleDefDSLMacro[T, AfterBind] {
+  trait MakeDSLBase[T, AfterBind] extends AnyKindShim {
     final def from[I <: T: AnyConstructor]: AfterBind =
       from(AnyConstructor[I])
 
@@ -317,7 +317,10 @@ object ModuleDefDSL {
       bind(ImplDef.ProviderImpl(provider.ret, provider))
     }
 
-    def bind(impl: ImplDef): AfterBind
+    final def fromFactory[I <: T: FactoryConstructor]: AfterBind =
+      from(FactoryConstructor[I])
+
+    protected[this] def bind(impl: ImplDef): AfterBind
     protected[this] def key: DIKey
   }
 
@@ -689,7 +692,7 @@ object ModuleDefDSL {
       addOp(SetIdFromImplName())(new MakeNamedDSL[T](_, key))
     }
 
-    override def bind(impl: ImplDef): MakeDSLUnnamedAfterFrom[T] = {
+    override protected[this] def bind(impl: ImplDef): MakeDSLUnnamedAfterFrom[T] = {
       addOp(SetImpl(impl))(new MakeDSLUnnamedAfterFrom[T](_, key))
     }
 
@@ -705,7 +708,7 @@ object ModuleDefDSL {
   ) extends MakeDSLMutBase[T, MakeNamedDSL[T]]
     with MakeDSLBase[T, MakeDSLNamedAfterFrom[T]] {
 
-    override def bind(impl: ImplDef): MakeDSLNamedAfterFrom[T] = {
+    override protected[this] def bind(impl: ImplDef): MakeDSLNamedAfterFrom[T] = {
       addOp(SetImpl(impl))(new MakeDSLNamedAfterFrom[T](_, key))
     }
 
