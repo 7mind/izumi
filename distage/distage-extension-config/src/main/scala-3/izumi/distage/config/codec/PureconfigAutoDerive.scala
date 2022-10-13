12af3f7811d2aed6f8ea91f8c97c2617d88100c4
Pavel Shirshov <pshirshov@eml.cc>
2022-10-13 17:07:02 +0100

    scala 3/pureconfig

--- distage/distage-extension-config/src/main/scala-3/izumi/distage/config/codec/PureconfigAutoDerive.scala
+++ distage/distage-extension-config/src/main/scala-3/izumi/distage/config/codec/PureconfigAutoDerive.scala
@@ -1,6 +1,11 @@
 package izumi.distage.config.codec
 
+import izumi.distage.constructors.{ClassConstructor, FactoryConstructor}
+import izumi.reflect.Tag
 import pureconfig.ConfigReader
+
+import scala.deriving.Mirror
+import scala.language.implicitConversions
 
 /**
   * Derive `pureconfig.ConfigReader` for A and for its fields recursively with `pureconfig-magnolia`
@@ -37,5 +42,16 @@ object PureconfigAutoDerive {
 
   @inline def derived[A](implicit ev: PureconfigAutoDerive[A]): ConfigReader[A] = ev.value
 
-  implicit def materialize[A]: PureconfigAutoDerive[A] = ???
+  import pureconfig.generic._
+  import pureconfig.generic.derivation.default.derived
+
+  inline implicit def derivedRecursive[A]: ConfigReader[A] = scala.compiletime.summonFrom {
+    case c: ConfigReader[A] => c
+    case m: Mirror.Of[A] => ConfigReader.derived[A](using m)
+  }
+
+  inline implicit def materialize[A: Mirror.Of]: PureconfigAutoDerive[A] = {
+    import izumi.distage.config.codec.PureconfigAutoDerive.derivedRecursive
+    new PureconfigAutoDerive[A](ConfigReader.derived[A])
+  }
 }
