9cfa165dbaab0633d76406603a133ec50ed804fe
Pavel Shirshov <pshirshov@eml.cc>
2023-01-04 18:31:21 +0000

    scala3: RoleModuleDef.makeRole

--- distage/distage-framework-api/src/main/scala-3/izumi/distage/roles/model/definition/RoleModuleDef.scala
+++ distage/distage-framework-api/src/main/scala-3/izumi/distage/roles/model/definition/RoleModuleDef.scala
@@ -3,13 +3,19 @@ package izumi.distage.roles.model.definition
 import izumi.distage.model.definition.ModuleDef
 import izumi.distage.model.definition.dsl.ModuleDefDSL.MakeDSL
 import izumi.distage.roles.model.RoleDescriptor.GetRoleDescriptor
-
+import izumi.distage.constructors.AnyConstructorMacro
+import izumi.distage.model.definition.dsl.ModuleDefDSL.MakeDSL
+import scala.annotation.experimental
+import scala.quoted.{Expr, Quotes, Type}
 
 trait RoleModuleDef extends ModuleDef {
-  final protected[this] def makeRole[T](implicit getRoleDescriptor: GetRoleDescriptor[T]): MakeDSL[T] = ???
+  inline final protected[this] def makeRole[T](implicit getRoleDescriptor: GetRoleDescriptor[T]): MakeDSL[T] =
+    makeRoleImpl[T].tagged(RoleTag(getRoleDescriptor.roleDescriptor))
+
+  inline final protected[this] def makeRoleImpl[T]: MakeDSL[T] =
+    ${AnyConstructorMacro.makeMethod[T, MakeDSL[T]]}
 }
 
 object RoleModuleDef {
-
 
 }
