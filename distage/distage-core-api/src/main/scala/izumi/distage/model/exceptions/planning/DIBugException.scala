7c43452c8e1c9c763bcc2b138647ec3f68f81edf
Pavel Shirshov <pshirshov@eml.cc>
2023-01-07 17:18:19 +0000

    #1786: wip

--- distage/distage-core-api/src/main/scala/izumi/distage/model/exceptions/planning/DIBugException.scala
+++
@@ -1,7 +0,0 @@
-package izumi.distage.model.exceptions.planning
-
-import izumi.distage.model.exceptions.DIException
-import izumi.fundamentals.platform.IzumiProject
-
-@deprecated("Needs to be removed", "20/10/2021")
-class DIBugException(message: String) extends DIException(IzumiProject.bugReportPrompt(message))
