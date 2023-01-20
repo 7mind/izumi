8390b67b9a63609cb63a1d5db706e028c08098ff
Kai <450507+neko-kai@users.noreply.github.com>
2023-01-20 18:03:42 +0000

    Unused import

--- distage/distage-core/.js/src/main/scala/izumi/distage/modules/platform/CatsIOPlatformDependentSupportModule.scala
+++ distage/distage-core/.js/src/main/scala/izumi/distage/modules/platform/CatsIOPlatformDependentSupportModule.scala
@@ -4,7 +4,6 @@ import cats.effect.unsafe.IORuntime
 import izumi.distage.model.definition.{Lifecycle, ModuleDef}
 import izumi.fundamentals.platform.functional.Identity
 
-import scala.annotation.unused
 import scala.concurrent.ExecutionContext
 
 private[distage] trait CatsIOPlatformDependentSupportModule extends ModuleDef {
