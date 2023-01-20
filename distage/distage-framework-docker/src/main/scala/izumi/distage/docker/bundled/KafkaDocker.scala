20bc3c70ebce7ac6b1f9ad3c7df7dd0a76983ff6
Pavel Shirshov <pshirshov@eml.cc>
2023-01-20 18:31:10 +0000

    user-contributed docker descriptor for Kafka with local listener, closes #1303

--- distage/distage-framework-docker/src/main/scala/izumi/distage/docker/bundled/KafkaDocker.scala
+++ distage/distage-framework-docker/src/main/scala/izumi/distage/docker/bundled/KafkaDocker.scala
@@ -22,9 +22,36 @@ object KafkaDocker extends ContainerDef {
   }
 }
 
+object KafkaTwofaceDocker extends ContainerDef {
+  val insidePort: DockerPort = DockerPort.DynamicTCP("dynamic_kafka_port_inside")
+  val outsidePort: DockerPort = DockerPort.DynamicTCP("dynamic_kafka_port_outside")
+
+  private[this] def portVars: String = Seq(
+    s"""KAFKA_LISTENERS=INSIDE://:$$${insidePort.toEnvVariable},OUTSIDE://:$$${outsidePort.toEnvVariable}""",
+    s"""KAFKA_ADVERTISED_LISTENERS=INSIDE://:$$${insidePort.toEnvVariable},OUTSIDE://127.0.0.1:$$${outsidePort.toEnvVariable}""",
+    """KAFKA_INTER_BROKER_LISTENER_NAME=INSIDE""",
+    """KAFKA_LISTENER_SECURITY_PROTOCOL_MAP=INSIDE:PLAINTEXT,OUTSIDE:PLAINTEXT""",
+  ).map(defn => s"export $defn").mkString("; ")
+
+  override def config: Config = {
+    Config(
+      image = "wurstmeister/kafka:2.12-2.4.1",
+      ports = Seq(insidePort, outsidePort),
+      entrypoint = Seq("sh", "-c", s"$portVars ; start-kafka.sh"),
+    )
+  }
+}
+
 class KafkaDockerModule[F[_]: TagK] extends ModuleDef {
   make[KafkaDocker.Container].fromResource {
     KafkaDocker
+      .make[F]
+      .connectToNetwork(KafkaZookeeperNetwork)
+      .dependOnContainerPorts(ZookeeperDocker)(2181 -> "KAFKA_ZOOKEEPER_CONNECT")
+  }
+
+  make[KafkaTwofaceDocker.Container].named("twoface").fromResource {
+    KafkaTwofaceDocker
       .make[F]
       .connectToNetwork(KafkaZookeeperNetwork)
       .dependOnContainerPorts(ZookeeperDocker)(2181 -> "KAFKA_ZOOKEEPER_CONNECT")
