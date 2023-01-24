80ee26faa080368260b1168be7e8ae3818eb1f27
Pavel Shirshov <pshirshov@eml.cc>
2023-01-24 15:47:20 +0000

    Show binding example in missing instance error message, closes #991

--- distage/distage-core-api/src/main/scala/izumi/distage/model/exceptions/runtime/MissingInstanceException.scala
+++ distage/distage-core-api/src/main/scala/izumi/distage/model/exceptions/runtime/MissingInstanceException.scala
@@ -8,7 +8,17 @@ class MissingInstanceException(message: String, val key: DIKey) extends DIExcept
 
 object MissingInstanceException {
   def format(target: DIKey, references: Set[DIKey]): String = {
+    // TODO: this representation is imperfect, we will have to change this once we have https://github.com/zio/izumi-reflect/issues/367 implemented
+    import izumi.reflect.macrortti.LTTRenderables.Short._
+
+    val refRepr = target.tpe.tag.ref.render()
+
     s"""Instance is not available in the object graph: $target.
-       |Required by refs: ${references.niceList(prefix = "* ")}""".stripMargin
+       |Required by refs: ${references.niceList(prefix = "* ")}
+       |
+       |You may add missing binding with code alike to
+       |
+       |  make[$refRepr]
+       |""".stripMargin
   }
 }
