b8575aec43999c4e51506fe68dfc23d46e6f81de
Scala Steward <scala_steward@virtuslab.com>
2023-01-29 13:05:25 +0000

    Update sbt-mdoc to 2.3.7

--- project/project/PluginVersions.scala
+++ project/project/PluginVersions.scala
@@ -1,7 +1,7 @@
 object PV {
   val scala_js_version = "1.12.0"
 
-  val sbt_mdoc = "2.3.6"
+  val sbt_mdoc = "2.3.7"
   val sbt_paradox_material_theme = "0.6.0"
   val sbt_paradox = "0.10.3"
   val sbt_ghpages = "0.7.0"
