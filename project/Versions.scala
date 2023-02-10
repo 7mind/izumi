79aa8b3d8a7c4b3c0426b9506c0b38131c597b8b
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-02-10 15:35:41 +0100

    Update zio to 1.0.18 (#1892)
          Co-authored-by: Paul S <pshirshov@eml.cc>

--- project/Versions.scala
+++ project/Versions.scala
@@ -15,7 +15,7 @@ object V {
   val discipline = "1.5.1"
   val discipline_scalatest = "2.2.0"
 
-  val zio = "1.0.17"
+  val zio = "1.0.18"
   val zio_interop_cats = "13.0.0.1"
 
   val monix = "3.4.0"
