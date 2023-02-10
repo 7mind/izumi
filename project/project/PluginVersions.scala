5ebd56c5748b1a88055f9654cf3afa4ebae7fbe2
Scala Steward <scala_steward@virtuslab.com>
2023-02-10 13:03:03 +0000

    Update sbt-scoverage to 2.0.7

--- project/project/PluginVersions.scala
+++ project/project/PluginVersions.scala
@@ -7,7 +7,7 @@ object PV {
   val sbt_ghpages = "0.7.0"
   val sbt_site = "1.4.1"
   val sbt_unidoc = "0.4.3"
-  val sbt_scoverage = "2.0.6"
+  val sbt_scoverage = "2.0.7"
   val sbt_pgp = "2.1.1"
   val sbt_assembly = "0.14.9"
 }
