d0bdd579d7c8315b567fc6694ce0801b4a45875a
Pavel Shirshov <pshirshov@eml.cc>
2023-04-06 17:17:13 +0100

    fix

---
+++ logstage/logstage-core/.js/src/test/scala/izumi/logstage/sink/LoggingAsyncSinkTest.scala
@@ -0,0 +1,24 @@
+package izumi.logstage.sink
+
+import izumi.logstage.api.{IzLogger, TestSink}
+import org.scalatest.wordspec.AnyWordSpec
+
+import scala.concurrent.duration.DurationInt
+
+class LoggingAsyncSinkTest extends AnyWordSpec {
+  "Log macro" should {
+    "support async sinks" in {
+      val testSink = new TestSink()
+      val asyncConsoleSinkJson = new ThreadingLogQueue(1.millis, 10)
+      try {
+        new ExampleService(IzLogger(IzLogger.Level.Trace, testSink, buffer = asyncConsoleSinkJson)).triggerManyMessages()
+        //assert(testSink.fetch().isEmpty) // js version is immediate
+        asyncConsoleSinkJson.start()
+      } finally {
+        asyncConsoleSinkJson.close()
+      }
+
+      assert(testSink.fetch().size == 100)
+    }
+  }
+}
