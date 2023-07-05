5f0c5ffd693c9f1c6e827ed544cc2afdd815e138
Pavel Shirshov <pshirshov@eml.cc>
2023-07-05 19:58:48 +0100

    IzFilesTest: workaround for stdlib concurrency bug

--- fundamentals/fundamentals-platform/.jvm/src/test/scala/izumi/fundamentals/platform/IzFilesTest.scala
+++ fundamentals/fundamentals-platform/.jvm/src/test/scala/izumi/fundamentals/platform/IzFilesTest.scala
@@ -1,12 +1,12 @@
 package izumi.fundamentals.platform
 
 import java.util.concurrent.TimeUnit
-
 import izumi.fundamentals.platform.files.IzFiles
 import org.scalatest.wordspec.AnyWordSpec
 
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
+import scala.util.Try
 
 class IzFilesTest extends AnyWordSpec {
 
@@ -24,7 +24,9 @@ class IzFilesTest extends AnyWordSpec {
       // unfortunately this test is unreliable, the filesystem logic might occasionally fail during initialization
       import scala.concurrent.ExecutionContext.Implicits.global
 
-
+      while (Try(IzResources.getPath("library.properties")).isFailure) {
+        Thread.sleep(100)
+      }
 
       val seq = (0 to 200).map {
         _ =>
