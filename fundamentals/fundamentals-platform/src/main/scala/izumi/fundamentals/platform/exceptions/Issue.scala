4c786d2aa6ccba8f90ad06f88d385d44e5691ed9
Paul S <pshirshov@eml.cc>
2023-08-29 17:21:20 +0100

    TextTree and Issue (#1992)

---
+++ fundamentals/fundamentals-platform/src/main/scala/izumi/fundamentals/platform/exceptions/Issue.scala
@@ -0,0 +1,34 @@
+package izumi.fundamentals.platform.exceptions
+
+import izumi.fundamentals.platform.exceptions.Issue.IssueContext
+import izumi.fundamentals.platform.language.{SourceFilePosition, SourceFilePositionMaterializer}
+
+trait Issue { this: Product =>
+
+  val context: IssueContext
+
+  override def toString: String = {
+    this.productIterator
+      .map(_.toString)
+      .mkString(s"$productPrefix(", ",", ")") + s" $context"
+  }
+}
+
+object Issue {
+  final case class IssueContext(sourceFilePosition: SourceFilePosition, stackTrace: Throwable) {
+
+    import izumi.fundamentals.platform.exceptions.IzThrowable.*
+    import izumi.fundamentals.platform.strings.IzString.*
+
+    override def toString: String =
+      s"""$sourceFilePosition {
+         |${stackTrace.stackTrace.shift(4)}
+         |}""".stripMargin
+  }
+
+  object IssueContext {
+    implicit def materializeExceptionContext(
+      implicit pos: SourceFilePositionMaterializer
+    ): IssueContext = new IssueContext(pos.get, new Throwable())
+  }
+}
