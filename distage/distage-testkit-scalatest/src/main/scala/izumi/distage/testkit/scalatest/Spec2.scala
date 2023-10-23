0ac3c739eaf9ac6142b8e66a9f079854b18d6712
Kai <450507+neko-kai@users.noreply.github.com>
2023-10-23 02:03:41 +0100

    SpecCtx dsl (only works on Scala 3...)

--- distage/distage-testkit-scalatest/src/main/scala/izumi/distage/testkit/scalatest/Spec2.scala
+++ distage/distage-testkit-scalatest/src/main/scala/izumi/distage/testkit/scalatest/Spec2.scala
@@ -1,10 +1,13 @@
 package izumi.distage.testkit.scalatest
 
-import distage.{DefaultModule2, TagKK}
+import distage.DefaultModule2
+import izumi.distage.constructors.ClassConstructor
 import izumi.distage.testkit.model.TestConfig
 import izumi.distage.testkit.services.scalatest.dstest.DistageAbstractScalatestSpec
 import izumi.distage.testkit.services.scalatest.dstest.DistageAbstractScalatestSpec.DSWordSpecStringWrapper2
+import izumi.fundamentals.platform.language.SourceFilePositionMaterializer
 import izumi.logstage.distage.LogIO2Module
+import izumi.reflect.{Tag, TagKK}
 import org.scalatest.distage.DistageScalatestTestSuiteRunner
 
 import scala.language.implicitConversions
@@ -20,4 +23,46 @@ abstract class Spec2[F[+_, +_]: DefaultModule2](implicit val tagBIO: TagKK[F])
   override protected def config: TestConfig = super.config.copy(
     moduleOverrides = LogIO2Module[F]()(tagBIO)
   )
+}
+
+abstract class SpecCtx2[F[+_, +_], Ctx: ClassConstructor: Tag](implicit defaultModule: DefaultModule2[F], tagBIO: TagKK[F]) extends Spec2[F] { x =>
+
+//  implicit class SuperXa(s: String) {
+//    def in(function: Ctx => F[Any, Unit])(implicit pos: SourceFilePositionMaterializer): Unit = {
+//      x.convertToWordSpecStringWrapperDS2(s).in(ClassConstructor[Ctx].map(function))
+//    }
+//  }
+
+  trait XWrapper extends DSWordSpecStringWrapper2[F] {
+    def in(function: Ctx => F[Any, Unit])(implicit pos: SourceFilePositionMaterializer): Unit = {
+      in(ClassConstructor[Ctx].map(function))
+    }
+  }
+
+  override protected implicit def convertToWordSpecStringWrapperDS2(s: String): XWrapper =
+    new DSWordSpecStringWrapper2[F](context, distageSuiteName, distageSuiteId, Seq(s), this, testEnv) with XWrapper {
+      override def in(function: Ctx => F[Any, Unit])(implicit pos: SourceFilePositionMaterializer): Unit = {
+        in(ClassConstructor[Ctx].map(function))
+      }
+    }
+
+}
+
+import zio.*
+case class Ctx(i: Int)
+class Test extends SpecCtx2[IO, Ctx] {
+
+  "test" should {
+    "abc" in {
+      ctx =>
+        for {
+          _ <- ZIO.consoleWith(_.print("abc"))
+        } yield ()
+    }
+
+    "xn" in {
+      (_: Int) => ZIO.unit
+    }
+  }
+
 }
