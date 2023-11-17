1151fc89e5dd646d4ff30bb2702b30a18fa6e59b
Scala Steward <scala_steward@virtuslab.com>
2023-11-17 13:24:13 +0000

    Update classgraph to 4.8.165

--- project/Versions.scala
+++ project/Versions.scala
@@ -31,7 +31,7 @@ object V {
   val scala_java_time = "2.5.0"
 
   // java-only dependencies below
-  val classgraph = "4.8.164"
+  val classgraph = "4.8.165"
   val slf4j = "2.0.9"
   val typesafe_config = "1.4.0"
 
