7mind/izumi

commit 4e5f4cb439b49e6f2517f93dbde056f4bb1c8f7b

ConfigLoader retains individual role configs in AppConfig, ConfigWriter reuses these configs
Co-authored-by: Kai <450507+neko-kai@users.noreply.github.com>

distage/distage-extension-config/.jvm/src/main/scala/izumi/distage/config/model/AppConfig.scala
@@ -1,0 +1,67 @@
+package izumi.distage.config.model
+
+import com.typesafe.config.{Config, ConfigFactory}
+import izumi.distage.config.DistageConfigImpl
+
+import java.io.File
+
+final case class AppConfig(
+  config: DistageConfigImpl,
+  shared: List[ConfigLoadResult.Success],
+  roles: List[LoadedRoleConfigs],
+)
+
+object AppConfig {
+  val empty: AppConfig = AppConfig(ConfigFactory.empty(), List.empty, List.empty)
+  def provided(config: DistageConfigImpl): AppConfig = AppConfig(config, List.empty, List.empty)
+}
+
+sealed trait GenericConfigSource
+
+object GenericConfigSource {
+  case class ConfigFile(file: File) extends GenericConfigSource
+
+  case object ConfigDefault extends GenericConfigSource
+}
+
+case class RoleConfig(role: String, active: Boolean, configSource: GenericConfigSource)
+
+case class LoadedRoleConfigs(roleConfig: RoleConfig, loaded: Seq[ConfigLoadResult.Success])
+
+sealed trait ConfigLoadResult {
+  def clue: String
+
+  def src: ConfigSource
+
+  def toEither: Either[ConfigLoadResult.Failure, ConfigLoadResult.Success]
+}
+
+object ConfigLoadResult {
+  case class Success(clue: String, src: ConfigSource, config: Config) extends ConfigLoadResult {
+    override def toEither: Either[ConfigLoadResult.Failure, ConfigLoadResult.Success] = Right(this)
+  }
+
+  case class Failure(clue: String, src: ConfigSource, failure: Throwable) extends ConfigLoadResult {
+    override def toEither: Either[ConfigLoadResult.Failure, ConfigLoadResult.Success] = Left(this)
+  }
+}
+
+sealed trait ConfigSource
+
+object ConfigSource {
+  final case class Resource(name: String, kind: ResourceConfigKind) extends ConfigSource {
+    override def toString: String = s"resource:$name"
+  }
+
+  final case class File(file: java.io.File) extends ConfigSource {
+    override def toString: String = s"file:$file"
+  }
+}
+
+sealed trait ResourceConfigKind
+
+object ResourceConfigKind {
+  case object Primary extends ResourceConfigKind
+
+  case object Development extends ResourceConfigKind
+}

distage/distage-extension-config/.js/src/main/scala/izumi/distage/config/model/AppConfig.scala
@@ -1,0 +1,12 @@
+package izumi.distage.config.model
+
+import izumi.distage.config.DistageConfigImpl
+
+final case class AppConfig(
+  config: DistageConfigImpl
+)
+
+object AppConfig {
+  val empty: AppConfig = AppConfig(Map.empty[String, String])
+  def provided(config: DistageConfigImpl): AppConfig = AppConfig(config)
+}

distage/distage-extension-config/src/main/scala/izumi/distage/config/model/AppConfig.scala
@@ -1,7 +1,0 @@
-package izumi.distage.config.model
-
-import izumi.distage.config.DistageConfigImpl
-
-final case class AppConfig(config: DistageConfigImpl)
-
-object AppConfig extends AppConfigSyntax {}

distage/distage-extension-config/.js/src/main/scala/izumi/distage/config/model/AppConfigSyntax.scala
@@ -1,5 +1,0 @@
-package izumi.distage.config.model
-
-trait AppConfigSyntax {
-  val empty: AppConfig = AppConfig(Map.empty[String, String])
-}

distage/distage-extension-config/.jvm/src/main/scala/izumi/distage/config/model/AppConfigSyntax.scala
@@ -1,7 +1,0 @@
-package izumi.distage.config.model
-
-import com.typesafe.config.ConfigFactory
-
-trait AppConfigSyntax {
-  val empty: AppConfig = AppConfig(ConfigFactory.empty())
-}

distage/distage-extension-config/.jvm/src/test/scala/izumi/distage/config/ConfigTest.scala
@@ -20,7 +20,7 @@ final class ConfigTest extends AnyWordSpec {
   }
 
   def mkModule(config: Config): AppConfigModule = {
-    val appConfig = AppConfig(config)
+    val appConfig = AppConfig(config, List.empty, List.empty)
     new AppConfigModule(appConfig)
   }
 

distage/distage-extension-config/src/main/scala/izumi/distage/config/AppConfigModule.scala
@@ -5,11 +5,9 @@ import izumi.distage.model.definition.ModuleDef
 
 class AppConfigModule(appConfig: AppConfig) extends ModuleDef {
   make[AppConfig].fromValue(appConfig)
-
-  def this(config: DistageConfigImpl) = this(AppConfig(config))
 }
 
 object AppConfigModule {
   def apply(appConfig: AppConfig): AppConfigModule = new AppConfigModule(appConfig)
-  def apply(config: DistageConfigImpl): AppConfigModule = new AppConfigModule(config)
+  def apply(config: DistageConfigImpl): AppConfigModule = new AppConfigModule(AppConfig.provided(config))
 }

distage/distage-framework/.js/src/main/scala/izumi/distage/roles/RoleAppMain.scala
@@ -1,6 +1,7 @@
 package izumi.distage.roles
 
 import distage.Injector
+import distage.config.AppConfig
 import izumi.distage.framework.config.PlanningOptions
 import izumi.distage.model.Locator
 import izumi.distage.model.definition.{Activation, Axis, Module, ModuleDef}
@@ -9,6 +10,7 @@ import izumi.distage.plugins.PluginConfig
 import izumi.distage.roles.RoleAppMain.ArgV
 import izumi.distage.roles.launcher.AppResourceProvider.AppResource
 import izumi.distage.roles.launcher.AppShutdownStrategy
+import izumi.distage.roles.launcher.ActivationParser
 import izumi.functional.lifecycle.Lifecycle
 import izumi.functional.quasi.QuasiIO
 import izumi.fundamentals.platform.cli.model.raw.{RawAppArgs, RawEntrypointParams, RawRoleParams, RequiredRoles}
@@ -107,7 +109,12 @@ abstract class RoleAppMain[F[_]](
     ) ++ new ModuleDef {
       make[RawAppArgs].fromValue(RawAppArgs(RawEntrypointParams.empty, additionalRoles.requiredRoles))
       make[PlanningOptions].fromValue(planningOptions())
-      make[Activation].named("roleapp").fromValue(activation())
+      make[ActivationParser].from[ActivationParser.Impl]
+      make[Activation].named("entrypoint").fromValue(activation())
+      make[Activation].named("roleapp").from {
+        (parser: ActivationParser, config: AppConfig) =>
+          parser.parseActivation(config)
+      }
     }
   }
 

commit 4889b6bf34135bd15eb3589049429ef4452c2ed9

Fix memoization regression in 1.2.0 (#2042)
Exclude new AppConfig fields from equals/hashCode to fix test environment merging for memoization regression introduced in https://github.com/7mind/izumi/pull/2040

distage/distage-extension-config/.jvm/src/main/scala/izumi/distage/config/model/AppConfig.scala
@@ -9,7 +9,16 @@ final case class AppConfig(
   config: DistageConfigImpl,
   shared: List[ConfigLoadResult.Success],
   roles: List[LoadedRoleConfigs],
-)
+) {
+  // FIXME: exclude `shared` & `roles` fields from equals/hashCode for now,
+  //  to prevent them breaking test environment merging for memoization
+  //  (fields added in https://github.com/7mind/izumi/pull/2040)
+  override def equals(obj: Any): Boolean = obj match {
+    case that: AppConfig => this.config == that.config
+    case _ => false
+  }
+  override def hashCode(): Int = config.hashCode()
+}
 
 object AppConfig {
   val empty: AppConfig = AppConfig(ConfigFactory.empty(), List.empty, List.empty)