817952005a157dcc96b76c6d078a0afbf3290838
Kai <450507+neko-kai@users.noreply.github.com>
2024-02-20 11:57:34 +0000

    Fix broken `MacroParameters` on Scala 3

--- fundamentals/fundamentals-reflection/src/main/scala-3/izumi/fundamentals/platform/build/MacroParameters.scala
+++ fundamentals/fundamentals-reflection/src/main/scala-3/izumi/fundamentals/platform/build/MacroParameters.scala
@@ -4,22 +4,22 @@ import scala.quoted.{Expr, Quotes, Type}
 
 object MacroParameters {
 
-  def scalaVersion(): Option[String] = macroSetting("scala-version")
-  def scalaCrossVersions(): Option[String] = macroSetting("scala-versions")
+  inline def scalaVersion(): Option[String] = macroSetting("scala-version")
+  inline def scalaCrossVersions(): Option[String] = macroSetting("scala-versions")
 
-  def projectGroupId(): Option[String] = macroSetting("product-group")
-  def artifactVersion(): Option[String] = macroSetting("product-version")
-  def artifactName(): Option[String] = macroSetting("product-name")
+  inline def projectGroupId(): Option[String] = macroSetting("product-group")
+  inline def artifactVersion(): Option[String] = macroSetting("product-version")
+  inline def artifactName(): Option[String] = macroSetting("product-name")
 
-  def sbtVersion(): Option[String] = macroSetting("sbt-version")
-  def scalatestVersion(): Option[String] = macroSetting("scalatest-version")
+  inline def sbtVersion(): Option[String] = macroSetting("sbt-version")
+  inline def scalatestVersion(): Option[String] = macroSetting("scalatest-version")
 
-  def gitRepoClean(): Option[Boolean] = macroSettingBool("git-repo-clean")
-  def gitBranch(): Option[String] = macroSetting("git-branch")
-  def gitHeadCommit(): Option[String] = macroSetting("git-head-commit")
-  def gitDescribedVersion(): Option[String] = macroSetting("git-described-version")
+  inline def gitRepoClean(): Option[Boolean] = macroSettingBool("git-repo-clean")
+  inline def gitBranch(): Option[String] = macroSetting("git-branch")
+  inline def gitHeadCommit(): Option[String] = macroSetting("git-head-commit")
+  inline def gitDescribedVersion(): Option[String] = macroSetting("git-described-version")
 
-  def sbtIsInsideCI(): Option[Boolean] = macroSettingBool("is-ci")
+  inline def sbtIsInsideCI(): Option[Boolean] = macroSettingBool("is-ci")
 
   inline def macroSetting(inline name: String): Option[String] = {
     ${ MacroParametersImpl.extractString('{ name }) }
