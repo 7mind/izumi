070413cf2f6ef7a02b7baa85ab0a7cdb2f05140a
Scala Steward <scala_steward@virtuslab.com>
2024-04-08 09:43:26 +0000

    Update sbt-site, sbt-site-paradox to 1.7.0

--- project/project/PluginVersions.scala
+++ project/project/PluginVersions.scala
@@ -5,7 +5,7 @@ object PV {
   val sbt_paradox_material_theme = "0.7.0"
   val sbt_paradox = "0.10.6"
   val sbt_ghpages = "0.8.0"
-  val sbt_site = "1.6.0"
+  val sbt_site = "1.7.0"
   val sbt_unidoc = "0.5.0"
   val sbt_scoverage = "2.0.11"
   val sbt_pgp = "2.1.1"
