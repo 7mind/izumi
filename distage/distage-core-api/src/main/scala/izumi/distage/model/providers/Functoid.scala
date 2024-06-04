600c0e8dc3b797b261fd0e9c615b2299f37b8eb6
Pavel Shirshov <pshirshov@eml.cc>
2024-06-04 16:49:16 +0100

    wip

--- distage/distage-core-api/src/main/scala/izumi/distage/model/providers/Functoid.scala
+++ distage/distage-core-api/src/main/scala/izumi/distage/model/providers/Functoid.scala
@@ -1,13 +1,8 @@
 package izumi.distage.model.providers
 
-import izumi.distage.model.definition.Identifier
 import izumi.distage.model.reflection.*
 import izumi.distage.reflection.macros.FunctoidMacroMethods
-import izumi.fundamentals.platform.language.Quirks.*
 import izumi.reflect.Tag
-
-import scala.annotation.unchecked.uncheckedVariance
-import scala.annotation.unused
 
 /**
   * A function that receives its arguments from DI object graph, including named instances via [[izumi.distage.model.definition.Id]] annotation.
@@ -68,91 +63,14 @@ import scala.annotation.unused
   * @see Functoid is based on the Magnet Pattern: [[http://spray.io/blog/2012-12-13-the-magnet-pattern/]]
   * @see Essentially Functoid is a function-like entity with additional properties, so it's funny name is reasonable enough: [[https://en.wiktionary.org/wiki/-oid#English]]
   */
-final case class Functoid[+A](get: Provider) {
-  def map[B: Tag](f: A => B): Functoid[B] = {
-    copy[B](get = get.unsafeMap(SafeType.get[B], (any: Any) => f(any.asInstanceOf[A])))
-  }
-
-  def zip[B](that: Functoid[B]): Functoid[(A, B)] = {
-    implicit val tagA: Tag[A] = this.getRetTag
-    implicit val tagB: Tag[B] = that.getRetTag
-    tagA.discard() // used for assembling Tag[(A, B)] below
-    tagB.discard()
-    copy[(A, B)](get = get.unsafeZip(SafeType.get[(A, B)], that.get))
-  }
-
-  def map2[B, C: Tag](that: Functoid[B])(f: (A, B) => C): Functoid[C] = {
-    zip(that).map[C](f.tupled)
-  }
-
-  /**
-    * Applicative's `ap` method - can be used to chain transformations like `flatMap`.
-    *
-    * Apply a function produced by `that` Functoid to the value produced by `this` Functoid.
-    *
-    * Same as
-    * {{{
-    *   this.map2(that)((a, f) => f(a))
-    * }}}
-    */
-  def flatAp[B: Tag](that: Functoid[A => B]): Functoid[B] = {
-    map2(that)((a, f) => f(a))
-  }
-
-  /**
-    * Apply a function produced by `this` Functoid to the argument produced by `that` Functoid.
-    *
-    * Same as
-    * {{{
-    *   this.map2(that)((f, a) => f(a))
-    * }}}
-    */
-  def ap[B, C](that: Functoid[B])(implicit @unused ev: A <:< (B => C), tag: Tag[C]): Functoid[C] = {
-    map2(that)((f, a) => f.asInstanceOf[B => C](a))
-  }
-
-  /** Add `B` as an unused dependency of this Functoid */
-  def addDependency[B: Tag]: Functoid[A] = addDependency(DIKey[B])
-  def addDependency[B: Tag](name: Identifier): Functoid[A] = addDependency(DIKey[B](name))
-  def addDependency(key: DIKey): Functoid[A] = addDependencies(key :: Nil)
-  def addDependencies(keys: Iterable[DIKey]): Functoid[A] = copy[A](get = get.addUnused(keys))
-
-  /**
-    * Add an `@Id` annotation to an unannotated parameter `P`, e.g.
-    * for .annotateParameter("x"), transform lambda `(p: P) => x(p)`
-    * into `(p: P @Id("x")) => x(p)`
-    */
-  def annotateParameter[P: Tag](name: Identifier): Functoid[A] = {
-    val paramTpe = SafeType.get[P]
-    annotateParameterWhen(name) {
-      case DIKey.TypeKey(tpe, _) => tpe == paramTpe
-      case _: DIKey.IdKey[?] => false
-    }
-  }
-  /** Add an `@Id(name)` annotation to all unannotated parameters */
-  def annotateAllParameters(name: Identifier): Functoid[A] = {
-    annotateParameterWhen(name) {
-      case _: DIKey.TypeKey => true
-      case _: DIKey.IdKey[?] => false
-    }
-  }
-  /** Add an `@Id(name)` annotation to all parameters matching `predicate` */
-  def annotateParameterWhen(name: Identifier)(predicate: DIKey.BasicKey => Boolean): Functoid[A] = {
-    val newProvider = this.get.replaceKeys {
-      case k: DIKey.BasicKey if predicate(k) =>
-        DIKey.IdKey(k.tpe, name.id, k.mutatorIndex)(name.idContract)
-      case k => k
-    }
-    Functoid(newProvider)
-  }
-
-  @inline private def getRetTag: Tag[A @uncheckedVariance] = Tag(get.ret.closestClass, get.ret.tag)
+final case class Functoid[+A](get: Provider) extends AbstractFunctoid[A, Functoid] {
+  override protected def create[B](provider: Provider): Functoid[B] = copy(get = provider)
 }
 
 object Functoid extends FunctoidMacroMethods with FunctoidLifecycleAdapters with FunctoidConstructors with SimpleFunctoids {
 
   implicit final class SyntaxMapSame[A](private val functoid: Functoid[A]) extends AnyVal {
-    def mapSame(f: A => A): Functoid[A] = functoid.map(f)(functoid.getRetTag)
+    def mapSame(f: A => A): Functoid[A] = functoid.map(f)(functoid.returnTypeTag)
   }
 
   def pure[A: Tag](a: A): Functoid[A] = lift(a)
---
+++ fundamentals/fundamentals-platform/src/main/scala/izumi/distage/model/providers/AbstractFunctoid.scala
@@ -0,0 +1,95 @@
+package izumi.distage.model.providers
+
+import izumi.distage.model.definition.Identifier
+import izumi.distage.model.reflection.{DIKey, Provider, SafeType}
+import izumi.fundamentals.platform.language.Quirks.Discarder
+import izumi.reflect.Tag
+
+import scala.annotation.unchecked.uncheckedVariance
+import scala.annotation.unused
+
+trait AbstractFunctoid[+A, Self[+K] <: AbstractFunctoid[K, Self]] {
+
+  def get: Provider
+
+  protected def create[B](provider: Provider): Self[B]
+
+  def map[B: Tag](f: A => B): Self[B] = {
+    create[B](get.unsafeMap(SafeType.get[B], (any: Any) => f(any.asInstanceOf[A])))
+  }
+
+  def zip[B](that: Self[B]): Self[(A, B)] = {
+    implicit val tagA: Tag[A] = this.returnTypeTag
+    implicit val tagB: Tag[B] = that.returnTypeTag
+    tagA.discard() // used for assembling Tag[(A, B)] below
+    tagB.discard()
+    create[(A, B)](get.unsafeZip(SafeType.get[(A, B)], that.get))
+  }
+
+  def map2[B, C: Tag](that: Self[B])(f: (A, B) => C): Self[C] = {
+    zip(that).map[C](f.tupled)
+  }
+
+  /**
+    * Applicative's `ap` method - can be used to chain transformations like `flatMap`.
+    *
+    * Apply a function produced by `that` Functoid to the value produced by `this` Functoid.
+    *
+    * Same as
+    * {{{
+    *   this.map2(that)((a, f) => f(a))
+    * }}}
+    */
+  def flatAp[B: Tag](that: Self[A => B]): Self[B] = {
+    map2(that)((a, f) => f(a))
+  }
+
+  /**
+    * Apply a function produced by `this` Functoid to the argument produced by `that` Functoid.
+    *
+    * Same as
+    * {{{
+    *   this.map2(that)((f, a) => f(a))
+    * }}}
+    */
+  def ap[B, C](that: Self[B])(implicit @unused ev: A <:< (B => C), tag: Tag[C]): Self[C] = {
+    map2(that)((f, a) => f.asInstanceOf[B => C](a))
+  }
+
+  /** Add `B` as an unused dependency of this Functoid */
+  def addDependency[B: Tag]: Self[A] = addDependency(DIKey[B])
+  def addDependency[B: Tag](name: Identifier): Self[A] = addDependency(DIKey[B](name))
+  def addDependency(key: DIKey): Self[A] = addDependencies(key :: Nil)
+  def addDependencies(keys: Iterable[DIKey]): Self[A] = create[A](get.addUnused(keys))
+
+  /**
+    * Add an `@Id` annotation to an unannotated parameter `P`, e.g.
+    * for .annotateParameter("x"), transform lambda `(p: P) => x(p)`
+    * into `(p: P @Id("x")) => x(p)`
+    */
+  def annotateParameter[P: Tag](name: Identifier): Self[A] = {
+    val paramTpe = SafeType.get[P]
+    annotateParameterWhen(name) {
+      case DIKey.TypeKey(tpe, _) => tpe == paramTpe
+      case _: DIKey.IdKey[?] => false
+    }
+  }
+  /** Add an `@Id(name)` annotation to all unannotated parameters */
+  def annotateAllParameters(name: Identifier): Self[A] = {
+    annotateParameterWhen(name) {
+      case _: DIKey.TypeKey => true
+      case _: DIKey.IdKey[?] => false
+    }
+  }
+  /** Add an `@Id(name)` annotation to all parameters matching `predicate` */
+  def annotateParameterWhen(name: Identifier)(predicate: DIKey.BasicKey => Boolean): Self[A] = {
+    val newProvider = this.get.replaceKeys {
+      case k: DIKey.BasicKey if predicate(k) =>
+        DIKey.IdKey(k.tpe, name.id, k.mutatorIndex)(name.idContract)
+      case k => k
+    }
+    create[A](newProvider)
+  }
+
+  def returnTypeTag: Tag[A @uncheckedVariance] = Tag(get.ret.closestClass, get.ret.tag)
+}
