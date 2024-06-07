c4fde73e975985ad11cb7c7e9190ed43a6f28871
Kai <450507+neko-kai@users.noreply.github.com>
2024-06-07 17:02:07 +0100

    LifecycleTagMacro: add Try just in case

--- distage/distage-core-api/src/main/scala-2/izumi/distage/model/definition/dsl/LifecycleTagMacro.scala
+++ distage/distage-core-api/src/main/scala-2/izumi/distage/model/definition/dsl/LifecycleTagMacro.scala
@@ -4,11 +4,12 @@ import izumi.distage.model.definition.Lifecycle
 import izumi.reflect.TagMacro
 
 import scala.reflect.macros.blackbox
+import scala.util.Try
 
 object LifecycleTagMacro {
   def fakeResourceTagMacroIntellijWorkaroundImpl[R <: Lifecycle[Any, Any]: c.WeakTypeTag](c: blackbox.Context): c.Expr[Nothing] = {
     val tagMacro = new TagMacro(c)
-    val _ = tagMacro.makeWeakTag[R] // run the macro AGAIN, to get a fresh error message
+    val _ = Try(tagMacro.makeWeakTag[R]) // run the macro AGAIN, to get a fresh error message
     val tagTrace = tagMacro.getImplicitError()
 
     c.abort(c.enclosingPosition, s"could not find implicit ResourceTag for ${c.universe.weakTypeOf[R]}!\n$tagTrace")
