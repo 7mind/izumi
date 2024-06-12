1da40b827c2b67c6585d4aedad3722630e65a674
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-06-12 15:05:05 +0200

    Update pureconfig-core, ... to 0.17.7 (#2132)

--- project/Versions.scala
+++ project/Versions.scala
@@ -23,7 +23,7 @@ object V {
 
   val circe = "0.14.7"
   val circe_derivation = "0.13.0-M5"
-  val pureconfig = "0.17.6"
+  val pureconfig = "0.17.7"
   val magnolia = "1.1.10"
   val jawn = "1.6.0"
 
