afe2c0d60da98d2fb014985ce67ccc5abde48362
Kai <450507+neko-kai@users.noreply.github.com>
2024-06-12 13:52:13 +0200

    Allow adding custom compile-time checks in distage-framework

--- distage/distage-framework/.jvm/src/main/scala/izumi/distage/framework/PlanCheck.scala
+++ distage/distage-framework/.jvm/src/main/scala/izumi/distage/framework/PlanCheck.scala
@@ -94,7 +94,7 @@ object PlanCheck {
       planVerifier: PlanVerifier = PlanVerifier(),
       logger: TrivialLogger = defaultLogger(),
     ): PlanCheckResult = {
-      val chosenRoles = parseRoles(cfg.roles)
+      val chosenRoles = RoleSelection.parseRoles(cfg.roles)
       val chosenActivations = parseActivations(cfg.excludeActivations)
       val chosenConfig = if (cfg.config == "*") None else Some(cfg.config)
 
@@ -124,13 +124,13 @@ object PlanCheck {
       logger: TrivialLogger = defaultLogger(),
     ): PlanCheckResult = {
 
-      var effectiveRoleNames = "unknown, failed too early"
-      var effectiveRoots = "unknown, failed too early"
-      var effectiveConfig = "unknown, failed too early"
-      var effectiveBsPlugins = LoadedPlugins.empty
-      var effectiveAppPlugins = LoadedPlugins.empty
-      var effectiveModule = ModuleBase.empty
-      var effectivePlugins = LoadedPlugins.empty
+      var reportingEffectiveRoleNames = "unknown, failed too early"
+      var reportingEffectiveRoots = "unknown, failed too early"
+      var reportingEffectiveConfig = "unknown, failed too early"
+      var reportingEffectiveBsPlugins = LoadedPlugins.empty
+      var reportingEffectiveAppPlugins = LoadedPlugins.empty
+      var reportingEffectiveModule = ModuleBase.empty
+      var reportingEffectivePlugins = LoadedPlugins.empty
 
       def renderPlugins(loadedPlugins: LoadedPlugins): String = {
         val plugins = loadedPlugins.loaded
@@ -162,16 +162,16 @@ object PlanCheck {
         val errorMsg = cause.fold("\n" + _.stacktraceString, _.issues.fromNESet.map(_.render + "\n").niceList())
         val message = {
           val configStr = if (checkConfig) {
-            s"\n  config              = ${chosenConfig.fold("*")(c => s"resource:$c")} (effective: $effectiveConfig)"
+            s"\n  config              = ${chosenConfig.fold("*")(c => s"resource:$c")} (effective: $reportingEffectiveConfig)"
           } else {
             ""
           }
-          val bindings = effectiveModule
-          val bsPlugins = effectiveBsPlugins.result
-          val appPlugins = effectiveAppPlugins.result
+          val bindings = reportingEffectiveModule
+          val bsPlugins = reportingEffectiveBsPlugins.result
+          val appPlugins = reportingEffectiveAppPlugins.result
           // fixme missing DefaultModule bindings !!!
-          val bsPluginsStr = renderPlugins(effectiveBsPlugins)
-          val appPluginsStr = renderPlugins(effectiveAppPlugins)
+          val bsPluginsStr = renderPlugins(reportingEffectiveBsPlugins)
+          val appPluginsStr = renderPlugins(reportingEffectiveAppPlugins)
           val printedBindings = if (printBindings) {
             (if (bsPlugins.nonEmpty)
                s"""
@@ -200,7 +200,7 @@ object PlanCheck {
 
           s"""Found a problem with your DI wiring, when checking application=${app.getClass.getName.split('.').last.split('$').last}, with parameters:
              |
-             |  roles               = $chosenRoles (effective roles: $effectiveRoleNames) (all effective roots: $effectiveRoots)
+             |  roles               = $chosenRoles (effective roles: $reportingEffectiveRoleNames) (all effective roots: $reportingEffectiveRoots)
              |  excludedActivations = ${NESet.from(excludedActivations).fold("ø")(_.map(_.mkString(" ")).mkString(" | "))}
              |  bootstrapPlugins    = $bsPluginsStr
              |  plugins             = $appPluginsStr
@@ -214,26 +214,29 @@ object PlanCheck {
              |""".stripMargin
         }
 
-        PlanCheckResult.Incorrect(effectivePlugins, visitedKeys, message, cause)
+        PlanCheckResult.Incorrect(reportingEffectivePlugins, visitedKeys, message, cause)
       }
 
       logger.log(s"Checking with roles=`$chosenRoles` excludedActivations=`$excludedActivations` chosenConfig=`$chosenConfig`")
 
       try {
         val input = app.preparePlanCheckInput(chosenRoles, chosenConfig)
+        val loadedPlugins = input.appPlugins ++ input.bsPlugins
 
-        effectiveRoleNames = input.roleNames.mkString(", ")
-        effectiveRoots = input.roots match {
+        reportingEffectiveRoleNames = input.roleNames.mkString(", ")
+        reportingEffectiveRoots = input.roots match {
           case Roots.Of(roots) => roots.mkString(", ")
           case Roots.Everything => "<Roots.Everything>"
         }
-        effectiveModule = input.module
-        effectiveAppPlugins = input.appPlugins
-        effectiveBsPlugins = input.bsPlugins
-        val loadedPlugins = input.appPlugins ++ input.bsPlugins
-        effectivePlugins = loadedPlugins
+        reportingEffectiveModule = input.module
+        reportingEffectiveAppPlugins = input.appPlugins
+        reportingEffectiveBsPlugins = input.bsPlugins
+        reportingEffectivePlugins = loadedPlugins
 
-        checkAnyApp[F](planVerifier, excludedActivations, checkConfig, effectiveConfig = _)(input) match {
+        val primaryCheckResult = checkAnyApp[F](planVerifier, excludedActivations, checkConfig, { reportingEffectiveConfig = _ }, input)
+        val additionalCheckResult = app.customCheck(planVerifier, excludedActivations, checkConfig, input)
+
+        primaryCheckResult.combine(additionalCheckResult) match {
           case incorrect: PlanVerifierResult.Incorrect => returnPlanCheckError(Right(incorrect))
           case PlanVerifierResult.Correct(visitedKeys, _) => PlanCheckResult.Correct(loadedPlugins, visitedKeys)
         }
@@ -244,12 +247,12 @@ object PlanCheck {
       }
     }
 
-    private[this] def checkAnyApp[F[_]](
+    def checkAnyApp[F[_]](
       planVerifier: PlanVerifier,
       excludedActivations: Set[NESet[AxisPoint]],
       checkConfig: Boolean,
       reportEffectiveConfig: String => Unit,
-    )(planCheckInput: PlanCheckInput[F]
+      planCheckInput: PlanCheckInput[F],
     ): PlanVerifierResult = {
       val PlanCheckInput(effectType, module, roots, _, providedKeys, configLoader, _, _) = planCheckInput
 
@@ -296,20 +299,42 @@ object PlanCheck {
       }
     }
 
-    sealed trait RoleSelection {
-      override final def toString: String = this match {
-        case RoleSelection.Everything => "*"
-        case RoleSelection.OnlySelected(selection) => selection.mkString(" ")
-        case RoleSelection.AllExcluding(excluded) => excluded.map("-" + _).mkString(" ")
-      }
-    }
-    object RoleSelection {
-      case object Everything extends RoleSelection
-      final case class OnlySelected(selection: Set[String]) extends RoleSelection
-      final case class AllExcluding(excluded: Set[String]) extends RoleSelection
+    private def parseActivations(s: String): Set[NESet[AxisPoint]] = {
+      s.split("\\|").iterator.filter(_.nonEmpty).flatMap {
+          NESet `from` _.split(" ").iterator.filter(_.nonEmpty).map(AxisPoint.parseAxisPoint).toSet
+        }.toSet
     }
 
-    private[this] def parseRoles(s: String): RoleSelection = {
+    private def defaultLogger(): TrivialLogger = {
+      TrivialLogger.make[this.type](DebugProperties.`izumi.debug.macro.distage.plancheck`.name)
+    }
+
+    @tailrec private def cutoffMacroTrace(t: Throwable): Unit = {
+      val trace = t.getStackTrace
+      val cutoffIdx = Some(trace.indexWhere(_.getClassName contains "scala.reflect.macros.runtime.JavaReflectionRuntimes$JavaReflectionResolvers")).filter(_ > 0)
+      t.setStackTrace(cutoffIdx.fold(trace)(trace.take))
+      val suppressed = t.getSuppressed
+      suppressed.foreach(cutSuppressed)
+      if (t.getCause ne null) cutoffMacroTrace(t.getCause)
+    }
+    // indirection for tailrec
+    private def cutSuppressed(t: Throwable): Unit = cutoffMacroTrace(t)
+
+  }
+
+  sealed trait RoleSelection {
+    override final def toString: String = this match {
+      case RoleSelection.Everything => "*"
+      case RoleSelection.OnlySelected(selection) => selection.mkString(" ")
+      case RoleSelection.AllExcluding(excluded) => excluded.map("-" + _).mkString(" ")
+    }
+  }
+  object RoleSelection {
+    case object Everything extends RoleSelection
+    final case class OnlySelected(selection: Set[String]) extends RoleSelection
+    final case class AllExcluding(excluded: Set[String]) extends RoleSelection
+
+    def parseRoles(s: String): RoleSelection = {
       val tokens = s.split(" ").iterator.filter(_.nonEmpty).toList
       tokens match {
         case "*" :: Nil =>
@@ -323,7 +348,7 @@ object PlanCheck {
       }
     }
 
-    private[this] def throwInvalidRoleSelectionError(s: String): Nothing = {
+    private def throwInvalidRoleSelectionError(s: String): Nothing = {
       throw new IllegalArgumentException(
         s"""Invalid role selection syntax in `$s`.
            |
@@ -335,28 +360,6 @@ object PlanCheck {
            |""".stripMargin
       )
     }
-
-    private[this] def parseActivations(s: String): Set[NESet[AxisPoint]] = {
-      s.split("\\|").iterator.filter(_.nonEmpty).flatMap {
-          NESet `from` _.split(" ").iterator.filter(_.nonEmpty).map(AxisPoint.parseAxisPoint).toSet
-        }.toSet
-    }
-
-    private[this] def defaultLogger(): TrivialLogger = {
-      TrivialLogger.make[this.type](DebugProperties.`izumi.debug.macro.distage.plancheck`.name)
-    }
-
-    @tailrec private[this] def cutoffMacroTrace(t: Throwable): Unit = {
-      val trace = t.getStackTrace
-      val cutoffIdx = Some(trace.indexWhere(_.getClassName contains "scala.reflect.macros.runtime.JavaReflectionRuntimes$JavaReflectionResolvers")).filter(_ > 0)
-      t.setStackTrace(cutoffIdx.fold(trace)(trace.take))
-      val suppressed = t.getSuppressed
-      suppressed.foreach(cutSuppressed)
-      if (t.getCause ne null) cutoffMacroTrace(t.getCause)
-    }
-    // indirection for tailrec
-    private[this] def cutSuppressed(t: Throwable): Unit = cutoffMacroTrace(t)
-
   }
 
 }
