818e69e17aeaf50ee7b766f401102cfc0e5dd6f8
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-08-18 01:21:10 +0200

    Update byte-buddy to 1.14.19 (#2171)

--- project/Versions.scala
+++ project/Versions.scala
@@ -36,7 +36,7 @@ object V {
   val typesafe_config = "1.4.0"
 
   // good to drop - java
-  val bytebuddy = "1.14.18"
+  val bytebuddy = "1.14.19"
   val docker_java = "3.4.0"
 
   // microsite-only
