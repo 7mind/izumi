90d5cd297fdf84afe5f9359c5332fa3839e5bcd5
Pavel Shirshov <pshirshov@eml.cc>
2024-09-20 17:26:06 +0100

    wip

--- logstage/logstage-core/src/test/scala/izumi/logstage/api/LoggerLevelAcceptanceTest.scala
+++ logstage/logstage-core/src/test/scala/izumi/logstage/api/LoggerLevelAcceptanceTest.scala
@@ -16,6 +16,7 @@ class LoggerLevelAcceptanceTest extends AnyWordSpec {
 
       assert(logger.router.acceptable(Log.LoggerId("org.test"), Log.Level.Debug))
       assert(logger.router.acceptable(Log.LoggerId("org.test.other"), Log.Level.Debug))
+      assert(logger.router.acceptable(Log.LoggerId("org.test.other.sub"), Log.Level.Debug))
       assert(logger.router.acceptable(Log.LoggerId("org.test2.FullName"), Log.Level.Debug))
     }
   }
