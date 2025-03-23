ec91e9000f657d0712b5001ce87ba7ea2fc864da
Scala Steward <scala_steward@virtuslab.com>
2025-03-23 14:07:17 +0000

    Update slf4j-api to 2.0.17

--- project/Versions.scala
+++ project/Versions.scala
@@ -32,7 +32,7 @@ object V {
 
   // java-only dependencies below
   val classgraph = "4.8.179"
-  val slf4j = "2.0.16"
+  val slf4j = "2.0.17"
   val typesafe_config = "1.4.0"
 
   // good to drop - java
