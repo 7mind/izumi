83bbdf8a93f92869b4537342a078a6d9fd00718e
Scala Steward <scala_steward@virtuslab.com>
2025-05-20 14:18:30 +0000

    Update zio-managed to 2.1.18

--- fundamentals/fundamentals-bio/src/main/scala/izumi/functional/lifecycle/Lifecycle.scala
+++ fundamentals/fundamentals-bio/src/main/scala/izumi/functional/lifecycle/Lifecycle.scala
@@ -938,7 +938,7 @@ object Lifecycle extends LifecycleInstances {
   }
   // Another workaround for a Scala 3 bincompat failure:
   // java.lang.NoClassDefFoundError: zio/CanFail.
-  // Appeared in an update from zio 2.1.14 to 2.1.16
+  // Appeared in an update from zio 2.1.14 to 2.1.18
   private implicit def zioCanFailWorkaround[F[x] >: zio.CanFail[x], E]: F[E] = null
 }
 
