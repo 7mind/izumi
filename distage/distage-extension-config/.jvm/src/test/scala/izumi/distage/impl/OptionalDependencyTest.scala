69c7f9767b764df0fa3cc9dcc78457fa4aa612cb
Kai <450507+neko-kai@users.noreply.github.com>
2025-09-19 22:53:36 +0100

    fix Scala 3 build

--- distage/distage-extension-config/.jvm/src/test/scala/izumi/distage/impl/OptionalDependencyTest.scala
+++ distage/distage-extension-config/.jvm/src/test/scala/izumi/distage/impl/OptionalDependencyTest.scala
@@ -217,7 +217,7 @@ class OptionalDependencyTest extends AnyWordSpec with GivenWhenThen {
     Exit.Trace.discard()
     // Exit.ZIOExit fails, but it's zio-specific
     intercept[java.lang.NoClassDefFoundError] {
-      Exit.ZIOExit.discard()
+      Exit.ZIOExit
     }
     Exit.CatsExit.discard() // CatsExit succeeds, even though it's cats-specific
     Exit.Trace.ThrowableTrace.discard()
