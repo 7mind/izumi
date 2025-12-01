f61dc2f23a078d73ff48b6d708b51bf53a621a92
Kai <450507+neko-kai@users.noreply.github.com>
2025-12-01 21:53:28 +0000

    fix bio test

--- fundamentals/fundamentals-bio/src/test/scala/izumi/functional/bio/test/BIOAsyncExpectedBehaviorTest.scala
+++ fundamentals/fundamentals-bio/src/test/scala/izumi/functional/bio/test/BIOAsyncExpectedBehaviorTest.scala
@@ -5,7 +5,6 @@ import izumi.reflect.TagKK
 import org.scalatest.Assertion
 import org.scalatest.wordspec.AsyncWordSpec
 
-import java.util.concurrent.CompletableFuture
 import scala.concurrent.{ExecutionContext, Promise}
 
 final class BIOAsyncExpectedBehaviorTestZIO extends BIOAsyncExpectedBehaviorTest[zio.IO](ec => UnsafeRun2.createZIO(Some(zio.Executor.fromExecutionContext(ec))))
@@ -21,19 +20,6 @@ abstract class BIOAsyncExpectedBehaviorTest[F[+_, +_]](
   val runner: UnsafeRun2[F] = mkRunner(this.executionContext)
 
   s"implementor ${TagKK[F].tag} of {Async2,Primitives2,Fork2}" should {
-
-    "have fromFutureJava call cancel when interrupted" in {
-      val cs = new CompletableFuture[Unit]()
-      val test: F[Throwable, Assertion] = for {
-        l1 <- F.sync(Promise[Unit]())
-        fiber = F.fromFutureJava { l1.success(()); cs }
-        _ <- F.fork(fiber).flatMap(F.fromFuture(_ => l1.future) *> _.interrupt)
-        canceled <- F.sync(cs.isCancelled)
-      } yield {
-        assert(canceled)
-      }
-      runner.unsafeRunAsyncAsFuture(test).map(_.toThrowableEither.toTry.get)
-    }
 
     "have interruptible fromFuture" in {
       val test: F[Throwable, Assertion] = for {
