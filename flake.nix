82e6d89f93cc1d24265aec6915cefd021cbace98
Paul S. <pshirshov@eml.cc>
2025-12-02 21:40:15 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -18,7 +18,10 @@
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
       in
       {
         devShells.default = pkgs.mkShell {
