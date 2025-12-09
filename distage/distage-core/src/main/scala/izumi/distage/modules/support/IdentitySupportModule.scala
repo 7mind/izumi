986321a0f3e82c57ea4e85107460c6171d7c8910
Kai <450507+neko-kai@users.noreply.github.com>
2025-12-09 19:33:57 +0000

    remove IdentityPlatformDependentSupportModule

--- distage/distage-core/src/main/scala/izumi/distage/modules/support/IdentitySupportModule.scala
+++ distage/distage-core/src/main/scala/izumi/distage/modules/support/IdentitySupportModule.scala
@@ -1,7 +1,6 @@
 package izumi.distage.modules.support
 
 import izumi.distage.model.definition.ModuleDef
-import izumi.distage.modules.platform.IdentityPlatformDependentSupportModule
 import izumi.functional.bio.{Clock1, Entropy1}
 import izumi.functional.quasi.*
 import izumi.fundamentals.platform.functional.Identity
@@ -15,14 +14,13 @@ object IdentitySupportModule extends IdentitySupportModule
   * Adds [[izumi.functional.quasi.QuasiIO]] instances to support running without an effect type in `Injector`, `distage-framework` & `distage-testkit-scalatest`
   */
 trait IdentitySupportModule extends ModuleDef {
-  include(IdentityPlatformDependentSupportModule)
-
   addImplicit[TagK[Identity]]
 
   addImplicit[QuasiFunctor[Identity]]
   addImplicit[QuasiApplicative[Identity]]
   addImplicit[QuasiPrimitives[Identity]]
   addImplicit[QuasiIO[Identity]]
+  addImplicit[QuasiAsync[Identity]]
   addImplicit[QuasiTemporal[Identity]]
   addImplicit[QuasiIORunner[Identity]]
   make[Clock1[Identity]].fromValue(Clock1.Standard)
