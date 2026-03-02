a6044f32f61a98c5254bdaff588dfaec8b2b1155
Kai <450507+neko-kai@users.noreply.github.com>
2026-03-02 22:14:05 +0000

    Remove unused izumi.fundamentals.platform.properties.EnvVars

--- fundamentals/fundamentals-platform/src/main/scala/izumi/fundamentals/platform/properties/EnvVars.scala
+++
@@ -1,8 +0,0 @@
-package izumi.fundamentals.platform.properties
-
-trait EnvVars {
-  case class EnvVar protected (name: String) {
-    def get(): Option[String] = Option(System.getenv().get(name))
-    def isDefined(): Boolean = System.getenv().containsKey(name)
-  }
-}
