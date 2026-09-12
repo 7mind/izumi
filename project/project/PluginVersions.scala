06bff7cc708cc3c8a2d5a0a44ecb8dc16a537d6a
Scala Steward <scala_steward@virtuslab.com>
2026-09-12 13:01:50 +0000

    Update sbt-ghpages to 0.10.0

--- project/project/PluginVersions.scala
+++ project/project/PluginVersions.scala
@@ -4,7 +4,7 @@ object PV {
   val sbt_mdoc = "2.7.2"
   val sbt_paradox_material_theme = "0.7.0"
   val sbt_paradox = "0.10.7"
-  val sbt_ghpages = "0.8.0"
+  val sbt_ghpages = "0.10.0"
   val sbt_site = "1.7.0"
   val sbt_unidoc = "0.6.1"
   val sbt_scoverage = "2.4.4"
