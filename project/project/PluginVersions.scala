2a7cec2987c4ffe8d116c3a1210a51e9727bde5b
Scala Steward <scala_steward@virtuslab.com>
2026-09-12 13:02:02 +0000

    Update sbt-paradox to 0.11.0

--- project/project/PluginVersions.scala
+++ project/project/PluginVersions.scala
@@ -3,7 +3,7 @@ object PV {
 
   val sbt_mdoc = "2.7.2"
   val sbt_paradox_material_theme = "0.7.0"
-  val sbt_paradox = "0.10.7"
+  val sbt_paradox = "0.11.0"
   val sbt_ghpages = "0.10.0"
   val sbt_site = "1.8.0"
   val sbt_unidoc = "0.6.1"
